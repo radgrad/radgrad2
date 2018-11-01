9f0d91f3e645f4eddb8b0226b8f992729b43fbb4
Cam Moore <cammymoore07@gmail.com>
2018-11-01 08:59:31 -1000

    Checkpoint

--- app/imports/ui/components/guidedtour/guidedtour-style.ts
+++ app/imports/ui/components/guidedtour/guidedtour-style.ts
@@ -1,43 +1,41 @@
 export default {
-  '@global': {
-    '.slick-dots li.slick-active button:before': {
-      color: 'white',
-    },
-    '.slick-dots li button:before': {
-      color: 'white',
-      fontSize: '8px !important',
-    },
-    '.slick-dots': {
-      bottom: -60,
-    },
-    '.guided-tour-background': {
-      backgroundColor: '#333333',
-    },
-    '.guided-tour-background:first-child': {
-      padding: '2.5rem 0',
-    },
-    '.guided-tour-description > h1': {
-      color: 'white',
-      fontSize: 36,
-      textAlign: 'left',
-    },
-    '.guided-tour-description > p': {
-      color: 'rgba(255,255,255,.7)',
-      fontSize: 16,
-      textIndent: '1rem',
-      textAlign: 'left',
-    },
-    '.guided-tour-description ul > li': {
-      color: 'rgba(255,255,255,.7)',
-      fontSize: 16,
-      textAlign: 'left',
-    },
-    '.guided-tour-description > p strong, .guided-tour-description ul > li strong': {
-      color: '#70BE44',
-    },
-    '.guided-tour-background .container > a': {
-      color: 'white',
-      textDecoration: 'underline',
-    },
+  '.slick-dots li.slick-active button:before': {
+    color: 'white',
+  },
+  '.slick-dots li button:before': {
+    color: 'white',
+    fontSize: '8px !important',
+  },
+  '.slick-dots': {
+    bottom: -60,
+  },
+  '.guided-tour-background': {
+    backgroundColor: '#333333',
+  },
+  '.guided-tour-background:first-child': {
+    padding: '2.5rem 0',
+  },
+  '.guided-tour-description > h1': {
+    color: 'white',
+    fontSize: 36,
+    textAlign: 'left',
+  },
+  '.guided-tour-description > p': {
+    color: 'rgba(255,255,255,.7)',
+    fontSize: 16,
+    textIndent: '1rem',
+    textAlign: 'left',
+  },
+  '.guided-tour-description ul > li': {
+    color: 'rgba(255,255,255,.7)',
+    fontSize: 16,
+    textAlign: 'left',
+  },
+  '.guided-tour-description > p strong, .guided-tour-description ul > li strong': {
+    color: '#70BE44',
+  },
+  '.guided-tour-background .container > a': {
+    color: 'white',
+    textDecoration: 'underline',
   },
 };
