3fe61611f713b895067df3e4ba67be247ada463d
Cam Moore <cammymoore07@gmail.com>
2019-04-03 11:34:27 -1000

    pagination action types

--- app/imports/redux/actions/paginationActions.ts
+++ app/imports/redux/actions/paginationActions.ts
@@ -7,11 +7,19 @@ import {
   SET_ACADEMIC_YEARS_SHOW_INDEX,
   SET_ADVISOR_LOGS_SHOW_COUNT,
   SET_ADVISOR_LOGS_SHOW_INDEX,
+  SET_CAREER_GOALS_SHOW_COUNT,
+  SET_COURSE_INSTANCES_SHOW_COUNT,
+  SET_COURSES_SHOW_COUNT,
+  SET_DESIRED_DEGREES_SHOW_COUNT,
 } from './paginationActionTypes';
 import { AcademicYearInstances } from '../../api/degree-plan/AcademicYearInstanceCollection';
 import { AcademicPlans } from '../../api/degree-plan/AcademicPlanCollection';
 import { AcademicTerms } from '../../api/academic-term/AcademicTermCollection';
 import { AdvisorLogs } from '../../api/log/AdvisorLogCollection';
+import { CareerGoals } from '../../api/career/CareerGoalCollection';
+import { CourseInstances } from '../../api/course/CourseInstanceCollection';
+import { Courses } from '../../api/course/CourseCollection';
+import { DesiredDegrees } from '../../api/degree-plan/DesiredDegreeCollection';
 
 export const setCollectionShowCount = (collectionName: string, count: number) => {
   const retval: {type?: string, payload?: number} = {};
@@ -28,6 +36,18 @@ export const setCollectionShowCount = (collectionName: string, count: number) =>
       break;
     case AdvisorLogs.getCollectionName():
       retval.type = SET_ADVISOR_LOGS_SHOW_COUNT;
+      break;
+    case CareerGoals.getCollectionName():
+      retval.type = SET_CAREER_GOALS_SHOW_COUNT;
+      break;
+    case CourseInstances.getCollectionName():
+      retval.type = SET_COURSE_INSTANCES_SHOW_COUNT;
+      break;
+    case Courses.getCollectionName():
+      retval.type = SET_COURSES_SHOW_COUNT;
+      break;
+    case DesiredDegrees.getCollectionName():
+      retval.type = SET_DESIRED_DEGREES_SHOW_COUNT;
       break;
     default:
       // do nothing
