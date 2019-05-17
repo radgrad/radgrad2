ed2f89cfe3ad044e894d8ce529de6da210f4d2a7
Mercedez Castro <mercedez@hawaii.edu>
2019-05-17 10:17:22 -1000

    Base UI and Question Form Completed
    Added page: StudentMentorSpacePage, Updated and added to StudentMentorSpaceQuestionForm.tsx

--- app/imports/ui/pages/student/StudentMentorSpacePage.tsx
+++ app/imports/ui/pages/student/StudentMentorSpacePage.tsx
@@ -1,23 +1,41 @@
 import * as React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Segment, Header } from 'semantic-ui-react';
 import StudentPageMenuWidget from '../../components/student/StudentPageMenuWidget';
+import StudentMentorSpaceQuestionForm from '../../components/student/StudentMentorSpaceQuestionForm';
 import withGlobalSubscription from '../../layouts/shared/GlobalSubscriptionsHOC';
 import withInstanceSubscriptions from '../../layouts/shared/InstanceSubscriptionsHOC';
 
 /** A simple static component to render some text for the landing page. */
 class StudentMentorSpacePage extends React.Component {
   public render() {
+    const marginStyle = {
+      marginTop: 5,
+    };
     return (
       <div>
         <StudentPageMenuWidget/>
-        <Grid verticalAlign="middle" textAlign="center" container={true}>
+        <Grid container={true} stackable={true} style={marginStyle}>
+          <Grid.Row>
+            <h3>Help Panel Widget goes here.</h3>
+          </Grid.Row>
 
-          <Grid.Column width={4}>
-            <Image size="small" circular={true} src="/images/radgrad_logo.png"/>
+          <Grid.Column width={11}>
+            <Segment padded={true}>
+              <Header dividing={true}>
+                <h4>QUESTIONS</h4>
+              </Header>
+              questionsList function goes here.
+            </Segment>
+              <StudentMentorSpaceQuestionForm/>
           </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Student Mentor Space</h1>
+          <Grid.Column width={5}>
+            <Segment>
+              <Header dividing={true}>
+                <h4>MENTOR DIRECTORY</h4>
+              </Header>
+              mentorFunction goes here.
+            </Segment>
           </Grid.Column>
 
         </Grid>
