079fb4543898d4854ae8f53367fd41dc2a5c38cf
gcalica <gcalica@hawaii.edu>
2019-05-17 10:20:19 -1000

    Created StudentHomeMenu and StudentFeedWidget #30

---
+++ app/imports/ui/components/student/StudentHomeWidget.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Grid } from 'semantic-ui-react';
+import StudentFeedWidget from './StudentFeedWidget';
+
+interface IStudentHomeWidget {
+  type?: string;
+}
+
+class StudentHomeWidget extends React.Component<IStudentHomeWidget> {
+  constructor(props) {
+    super(props);
+  }
+
+  public render(): React.ReactElement<any> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+    return (
+        <Grid>
+          <Grid.Column width={10}>
+            TODO: StudentOfInterestWidget, type=opportunities
+            TODO: StudentOfInterestWidget, type=courses
+          </Grid.Column>
+
+          <Grid.Column width={6}>
+            <StudentFeedWidget/>
+            TODO: StudentTeaserWidget
+          </Grid.Column>
+        </Grid>
+    );
+  }
+}
+
+export default StudentHomeWidget;
