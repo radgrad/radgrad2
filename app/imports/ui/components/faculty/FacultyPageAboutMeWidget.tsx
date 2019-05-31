e88b2b327316e878e2ad0111adc71a59936d3afe
Quinne Uchida <qauchida@hawaii.edu>
2019-05-31 12:43:52 -1000

    fixed most eslint errors

--- app/imports/ui/components/faculty/FacultyPageAboutMeWidget.tsx
+++ app/imports/ui/components/faculty/FacultyPageAboutMeWidget.tsx
@@ -1,4 +1,5 @@
-/**@qauchida
+/**
+ * @qauchida
  * 05/20/19
  * Faculty Widget that shows About Me information
  */
@@ -11,9 +12,6 @@ import {Users} from "../../../api/user/UserCollection";
 import {Interests} from "../../../api/interest/InterestCollection";
 import {CareerGoals} from "../../../api/career/CareerGoalCollection";
 import Swal from "sweetalert2";
-import { ValidatedMethod } from 'meteor/mdg:validated-method';
-import {RadGrad} from "../../../api/radgrad/RadGrad";
-import {CallPromiseMixin} from "meteor/didericis:callpromise-mixin";
 import {updateMethod} from "../../../api/base/BaseCollection.methods";
 
 interface IFacultyPageAboutMeWidgetProps {
@@ -22,94 +20,52 @@ interface IFacultyPageAboutMeWidgetProps {
       id: string;
       username: string;
       url: string;
-    },
-    state: {
-      id: 'not changed';
-      website: string;
-      picture: string;
     }
+
   }
 }
 
-class FacultyPageAboutMeWidget extends React.Component<IFacultyPageAboutMeWidgetProps> {
+interface IFacultyProfileState {
+  id: string;
+  website: string;
+  picture: string;
+}
+
+
+class FacultyPageAboutMeWidget extends React.Component<IFacultyPageAboutMeWidgetProps, IFacultyProfileState> {
   //call the props constructor
   constructor(props: any) {
     super(props);
+    this.state = {id: '', website: '', picture: ''}
   }
 
+  /**
+   * Function to record the changes in the form.
+   * @param event
+   * @param name Name of the value being changed
+   * @param value Value of the attribute
+   */
   private handleChange = (event, {name, value}) => {
     console.log('handle change');
     console.log(event, {name, value});
     console.log(FacultyProfiles.findDoc(this.props.match.params.username).userID);
-    this.setState({[name]: value});
+    const changeIntermediate = {};
+    changeIntermediate[name] = value;
+    this.setState(changeIntermediate);
   };
 
   /**
-   * Handle Update from AdminDataModelUsersPage.tsx
-  private handleUpdate = (doc) => {
-    const updateData = doc; // create the updateData object from the doc.
-    updateData.id = doc._id;
-    let collectionName;
-    if (StudentProfiles.isDefined(updateData.id)) {
-      collectionName = StudentProfiles.getCollectionName();
-    }
-    if (FacultyProfiles.isDefined(updateData.id)) {
-      collectionName = FacultyProfiles.getCollectionName();
-    }
-    if (MentorProfiles.isDefined(updateData.id)) {
-      collectionName = MentorProfiles.getCollectionName();
-    }
-    if (AdvisorProfiles.isDefined(updateData.id)) {
-      collectionName = AdvisorProfiles.getCollectionName();
-    }
-    updateData.interests = _.map(doc.interests, (interest) => interestSlugFromName(interest));
-    updateData.careerGoals = _.map(doc.careerGoals, (goal) => careerGoalSlugFromName(goal));
-    if (!_.isNil(doc.academicPlan)) {
-      updateData.academicPlan = academicPlanSlugFromName(doc.academicPlan);
-    }
-    if (!_.isNil(doc.declaredAcademicTerm)) {
-      updateData.declaredAcademicTerm = declaredAcademicTermSlugFromName(doc.declaredAcademicTerm);
-    }
-    console.log(collectionName, updateData);
-    updateMethod.call({ collectionName, updateData }, (error) => {
-      if (error) {
-        Swal.fire({
-          title: 'Update failed',
-          text: error.message,
-          type: 'error',
-        });
-        console.error('Error in updating. %o', error);
-      } else {
-        Swal.fire({
-          title: 'Update succeeded',
-          type: 'success',
-          showConfirmButton: false,
-          timer: 1500,
-        });
-        this.setState({ showUpdateForm: false, id: '' });
-      }
-    });
-  };
+   * Function to handle the submission.
    */
-
-  //reference the handleUpdate from AdminDataModelUsersPage.tsx
-  private handleSubmitWebsite = (event, info) => {
-    console.log('handle update');
-    let updateData = this.state;  // create the updateData object from the doc.
-    console.log(updateData);
-
-    console.log(FacultyProfiles.findDoc(this.props.match.params.username).userID);
-
+  private handleSubmitWebsite = () => {
+    const updateData: any = this.state;  // create the updateData object from the doc.
     updateData.id = (FacultyProfiles.findDoc(this.props.match.params.username)._id);
-    console.log('updataData:', updateData);
-    let collectionName = FacultyProfiles.getCollectionName();
-    console.log(event, info);
-    console.log(FacultyProfiles.isDefined(this.props.match.params.username));
-    console.log(collectionName);
+    const collectionName = FacultyProfiles.getCollectionName();
+    /**
+     * borrowed from AdminDataModelUsersPage.tsx
+     */
 
-
-
-    updateMethod.call({ collectionName, updateData }, (error) => {
+    updateMethod.call({collectionName, updateData}, (error) => {
       if (error) {
         Swal.fire({
           title: 'Update failed',
@@ -124,38 +80,23 @@ class FacultyPageAboutMeWidget extends React.Component<IFacultyPageAboutMeWidget
           showConfirmButton: false,
           timer: 1500,
         });
-        this.setState({ showUpdateForm: false, id: '' });
-        console.log(FacultyProfiles.findDoc(this.props.match.params.username).website);
       }
     });
   };
 
-
-
-
-  private handleSubmitPhoto = (event) => {
-    const username = this.props.match.params.username;
-    //gets the doc object containing information on desired profile based on username
-    const facultyDoc = FacultyProfiles.findDoc(username);
-    console.log('handle Submit Photo');
-    this.setState({id: 'changed'});
-    console.log(this.state);
-    console.log(FacultyProfiles.findDoc(this.props.match.params.username).picture);
-    //updates faculty profile's website entry
-    FacultyProfiles.update(facultyDoc._id, this.state);
-    console.log(Users.getProfile(facultyDoc.userID));
-    //need to alert userif their update was sucessful
-    //also need to update the placeholder text
-    alert(event);
-
-  };
-
-  //private method to call the picture uploader
+  /**
+   * Function to handle picture uploads.
+   * This is a placeholder until we get cloudniary working.
+   * @param event (may delete later)
+   */
   private fileSelectedHandler = (event) => {
     console.log(event.target.files[0]);
   };
 
-  //private method to generate route for slugs for interests
+  /**
+   * Puts together the slug for specific interest route.
+   * @param label
+   */
   private generateInterestRoute = (label) => {
     const facultyDoc = FacultyProfiles.findDoc(this.props.match.params.username);
     const facultyUserID = facultyDoc.userID;
@@ -167,10 +108,13 @@ class FacultyPageAboutMeWidget extends React.Component<IFacultyPageAboutMeWidget
       'explorer', 'interests', label];
     let exploreRoute = explorePath.join('/');
     exploreRoute = `/${exploreRoute}`;
-    return(exploreRoute);
+    return (exploreRoute);
   };
 
-  //private method to generate route for slugs for career goals
+  /**
+   * Puts together the slug for the specific career goals route.
+   * @param label
+   */
   private generateCareerGoalsRoute = (label) => {
     const facultyDoc = FacultyProfiles.findDoc(this.props.match.params.username);
     const facultyUserID = facultyDoc.userID;
@@ -186,27 +130,27 @@ class FacultyPageAboutMeWidget extends React.Component<IFacultyPageAboutMeWidget
 
   public render() {
     const username = this.props.match.params.username;
-//gets the doc object containing information on desired profile based on username
+    //gets the doc object containing information on desired profile based on username
     const facultyDoc = FacultyProfiles.findDoc(username);
-//gets the user ID based on the username
+    //gets the user ID based on the username
     const facultyUserID = facultyDoc.userID;
-//gets the user profile based on the user ID
+    //gets the user profile based on the user ID
     const facultyUserProfile = Users.getProfile(facultyUserID);
-//gets the username based on the user ID
+    //gets the username based on the user ID
     const facultyUserUsername = facultyUserProfile.username;
-//gets the faculty website address
+    //gets the faculty website address
     const facultyWebsite = facultyUserProfile.website;
     //get the career goal IDs based on the userID
     const facultyCareerGoalsIDs = facultyUserProfile.careerGoalIDs;
-//map the career goal IDs to their names
+    //map the career goal IDs to their names
     const facultyCareerGoals = _.map(facultyCareerGoalsIDs, (id) => CareerGoals.findDoc(id).name);
-//get the interest goal IDs based on the User ID
+    //get the interest goal IDs based on the User ID
     const facultyInterestIDs = facultyUserProfile.interestIDs;
-//map the interests IDs to their names
+    //map the interests IDs to their names
     const facultyInterests = _.map(facultyInterestIDs, (id) => Interests.findDoc(id).name);
-//M: should make it so that you reference the doc and then the name rather than the doc directly
-//gets the url from the faculty profile's information
-//url is made up of: role/username/explorer/CareerOrInterests
+    //M: should make it so that you reference the doc and then the name rather than the doc directly
+    // gets the url from the faculty profile's information
+    // url is made up of: role/username/explorer/CareerOrInterests
     let explorePath = [facultyUserProfile.role.toLowerCase(), facultyUserUsername, 'explorer', 'interests'];
     let exploreRoute = explorePath.join('/');
     exploreRoute = `/${exploreRoute}`;
@@ -214,104 +158,106 @@ class FacultyPageAboutMeWidget extends React.Component<IFacultyPageAboutMeWidget
     let careerPath = [facultyUserProfile.role.toLowerCase(), facultyUserUsername, 'explorer', 'career-goals'];
     let careerRoute = careerPath.join('/');
     careerRoute = `/${careerRoute}`;
-//handles the submit for website changes
-
-// for the picture upload, reference https://cloudinary.com/documentation/upload_widget from RadGrad1
-// here is the react way: https://www.npmjs.com/package/react-images-upload
-
-// React Image Uploader https://www.npmjs.com/package/react-images-upload
+    //handles the submit for website changes
+    // for the picture upload, reference https://cloudinary.com/documentation/upload_widget from RadGrad1
+    // here is the react way: https://www.npmjs.com/package/react-images-upload
+    // React Image Uploader https://www.npmjs.com/package/react-images-upload
 
     return (
-      <Container>
-        <div className="ui padded container segment">
-          <Grid>
-            <Grid.Row>
-              <Grid.Column>
-                <Header as='h3' dividing textAlign='left'>Profile</Header>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column floated='left' width={2}>
-                <Header as='h5' textAlign='left'>Name</Header>
-              </Grid.Column>
-              <Grid.Column floated='left' width={6}>
-                <Header as='h5' textAlign='left'>{Users.getFullName(facultyDoc)}</Header>
-              </Grid.Column>
-              <Grid.Column floated='left' width={2}>
-                <Header as='h5' textAlign='left'>Email</Header>
-              </Grid.Column>
-              <Grid.Column floated='left' width={6}>
-                <Header as='h5' textAlign='left'>{facultyUserUsername}</Header>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column floated='left' width={2}>
-                <Header as='h5' textAlign='left'>Interests</Header>
-              </Grid.Column>
-              <Grid.Column floated='left' width={6}>
-                <Grid>
-                  <Grid.Row divided textAlign='left'>
-                    <Label.Group>
-                      {_.map(facultyInterests, (interests, index) =>
-                        <Label size='small' key={index} as={NavLink} exact={true} to={this.generateInterestRoute(interests)}><Icon
-                          name='star'>{interests}</Icon></Label>
-                          )}
-                    </Label.Group>
-                  </Grid.Row>
-                  <Link to={exploreRoute}>Edit in Interest Explorer</Link>
-                </Grid>
-              </Grid.Column>
-              <Grid.Column floated='left' width={2}>
-                <Header as='h5' textAlign='left'>Career Goals</Header>
-              </Grid.Column>
-              <Grid.Column floated='left' width={6}>
-                <Grid>
-                  <Grid.Row divided textAlign='left'>
-                    <Label.Group>
-                      {_.map(facultyCareerGoals, (careerGoals, index) =>
-                        <Label size='small' key={index} as={NavLink} exact={true} to={this.generateCareerGoalsRoute(careerGoals)}><Icon
-                          name='suitcase'>{careerGoals}</Icon></Label>
-                      )}
-                    </Label.Group>
-                  </Grid.Row>
-                  <Link to={careerRoute}>Edit in Career Goal Explorer</Link>
-                </Grid>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column floated='left' width={2}>
-                <Header as='h5' textAlign='left'>Website</Header>
-              </Grid.Column>
-              <Grid.Column floated='left' width={6}>
-                <Form onSubmit={this.handleSubmitWebsite} success>
-                  <Form.Group>
-                    <Form.Input width={10}
-                                name='website'
-                                onChange={this.handleChange}
-                                placeholder={facultyWebsite}/>
-                    <Form.Button content='Update'/>
-                  </Form.Group>
-                </Form>
-              </Grid.Column>
-              <Grid.Column floated='left' width={2}>
-                <Header as='h5' textAlign='left'>Picture</Header>
-              </Grid.Column>
-              <Grid.Column floated='left' width={6}>
-                <Form onSubmit={this.handleSubmitPhoto} success>
-                  <Form.Group>
-                    <Form.Input
-                      onChange={this.handleChange} width={10}
-                      name='picture'
-                      placeholder={facultyUserProfile.picture}/>
-                    <Form.Button content='Update'/>
-                  </Form.Group>
-                </Form>
-                <input type='file' onChange={this.fileSelectedHandler}/>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </div>
-      </Container>
+
+        <Container>
+          <div className="ui padded container segment">
+            <Grid>
+              <Grid.Row>
+                <Grid.Column>
+                  <Header as='h3' dividing textAlign='left'>Profile</Header>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column floated='left' width={2}>
+                  <Header as='h5' textAlign='left'>Name</Header>
+                </Grid.Column>
+                <Grid.Column floated='left' width={6}>
+                  <Header as='h5' textAlign='left'>{Users.getFullName(facultyDoc)}</Header>
+                </Grid.Column>
+                <Grid.Column floated='left' width={2}>
+                  <Header as='h5' textAlign='left'>Email</Header>
+                </Grid.Column>
+                <Grid.Column floated='left' width={6}>
+                  <Header as='h5' textAlign='left'>{facultyUserUsername}</Header>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column floated='left' width={2}>
+                  <Header as='h5' textAlign='left'>Interests</Header>
+                </Grid.Column>
+                <Grid.Column floated='left' width={6}>
+                  <Grid>
+                    <Grid.Row divided textAlign='left'>
+                      <Label.Group>
+                        {_.map(facultyInterests, (interests, index) =>
+                          <Label size='small' key={index} as={NavLink} exact={true}
+                                 to={this.generateInterestRoute(interests)}><Icon
+                            name='star'>{interests}</Icon></Label>
+                        )}
+                      </Label.Group>
+                    </Grid.Row>
+                    <Link to={exploreRoute}>Edit in Interest Explorer</Link>
+                  </Grid>
+                </Grid.Column>
+                <Grid.Column floated='left' width={2}>
+                  <Header as='h5' textAlign='left'>Career Goals</Header>
+                </Grid.Column>
+                <Grid.Column floated='left' width={6}>
+                  <Grid>
+                    <Grid.Row divided textAlign='left'>
+                      <Label.Group>
+                        {_.map(facultyCareerGoals, (careerGoals, index) =>
+                          <Label size='small' key={index} as={NavLink} exact={true}
+                                 to={this.generateCareerGoalsRoute(careerGoals)}><Icon
+                            name='suitcase'>{careerGoals}</Icon></Label>
+                        )}
+                      </Label.Group>
+                    </Grid.Row>
+                    <Link to={careerRoute}>Edit in Career Goal Explorer</Link>
+                  </Grid>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column floated='left' width={2}>
+                  <Header as='h5' textAlign='left'>Website</Header>
+                </Grid.Column>
+                <Grid.Column floated='left' width={6}>
+                  <Form onSubmit={this.handleSubmitWebsite} success>
+                    <Form.Group>
+                      <Form.Input width={10}
+                                  name='website'
+                                  onChange={this.handleChange}
+                                  placeholder={facultyWebsite}/>
+                      <Form.Button content='Update'/>
+                    </Form.Group>
+                  </Form>
+                </Grid.Column>
+                <Grid.Column floated='left' width={2}>
+                  <Header as='h5' textAlign='left'>Picture</Header>
+                </Grid.Column>
+                <Grid.Column floated='left' width={6}>
+                  <Form onSubmit={this.handleSubmitWebsite} success>
+                    <Form.Group>
+                      <Form.Input
+                        onChange={this.handleChange} width={10}
+                        name='picture'
+                        placeholder={facultyUserProfile.picture}/>
+                      <Form.Button content='Update'/>
+                    </Form.Group>
+                  </Form>
+                  <input type='file' onChange={this.fileSelectedHandler}/>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </div>
+        </Container>
+
     );
 
   }
@@ -320,7 +266,3 @@ class FacultyPageAboutMeWidget extends React.Component<IFacultyPageAboutMeWidget
 }
 
 export default withRouter(FacultyPageAboutMeWidget);
-
-//update button on click should update the appropriate field with information inputted from the user
-// may have to make quality checks and what not
-
