d64c888e7a072156d2fc430682b584de71115019
qauchida <qauchida@hawaii.edu>
2019-06-05 19:57:38 -1000

    got title and description to show up for Explorer Interest

--- app/imports/ui/components/shared/ExplorerInterestsWidget.tsx
+++ app/imports/ui/components/shared/ExplorerInterestsWidget.tsx
@@ -4,17 +4,14 @@ import {Container, Header} from 'semantic-ui-react';
 import StudentPageMenuWidget from '../../components/student/StudentPageMenuWidget';
 import MentorPageMenuWidget from '../../components/mentor/MentorPageMenuWidget';
 import FacultyPageMenuWidget from '../../components/faculty/FacultyPageMenuWidget';
-import {Interests} from "../../../api/interest/InterestCollection";
+import { Interests } from "../../../api/interest/InterestCollection";
+import BaseSlugCollection from '../../../api/base/BaseSlugCollection'
 
 interface IExplorerInterestsWidgetProps {
   match: {
-    isExact: boolean,
-    path: string,
-    url: string,
     params: {
-      username: string;
       interest: string;
-      _id: string;
+      id: string;
     }
   }
 };
@@ -30,20 +27,49 @@ class ExplorerInterestsWidget extends React.Component <IExplorerInterestsWidgetP
     this.state = {};
   }
 
-
-  private getInterest = () => {
-    console.log(this.props.match);
+  /**
+   * returns the doc of interest
+   * @constructor
+   */
+  private GetInterestDoc = () => {
+    console.log('get interest method',this.props.match);
+    //quick fix need to make method that converts this.props.match.params.interest to appropriate casing
+    console.log(Interests.findDoc(this.ConvertedInterestName(this.props.match.params.interest)));
+    return (Interests.findDoc(this.ConvertedInterestName(this.props.match.params.interest)));
+    //Interests.findIdBySlug(slug)
   }
+
+  /**
+   *
+   * reference from: https://stackoverflow.com/questions/32589197/capitalize-first-letter-of-each-word-in-a-string-javascript/32589256
+   * should try to get rid of the for loop later
+   * converts lower case name to
+   * @param interestName
+   * @constructor
+   */
+  private ConvertedInterestName = (interestName) =>{
+  var convertedName = interestName.toString().split('-').join(' ');
+  convertedName = convertedName.split(' ');
+  for(var i = 0; i<convertedName.length;i++){
+    convertedName[i] = convertedName[i].charAt(0).toUpperCase() + convertedName[i].substring(1);
+  }
+  return convertedName.join(' ');
+};
+
 
 
   public render() {
+    const interestName = this.GetInterestDoc().name;
+    const interestDescription = this.GetInterestDoc().description;
+    const interstSlugID = this.GetInterestDoc().slugID;
     return (
       <div className='ui paded container'>
         <div className="ui segments">
           <div className='ui padded segment container'>
-            <Header>The Interest Name should go here</Header>
-            {this.getInterest()}
+            <Header>{interestName}</Header>
+            {interestDescription}
           </div>
+
         </div>
       </div>
     );
