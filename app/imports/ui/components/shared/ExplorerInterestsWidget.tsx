b45362ff5c14b49501a8c2258f6ca34a9836490d
qauchida <qauchida@hawaii.edu>
2019-06-07 08:31:07 -1000

    removed unused method "ConvertInterestNames"

--- app/imports/ui/components/shared/ExplorerInterestsWidget.tsx
+++ app/imports/ui/components/shared/ExplorerInterestsWidget.tsx
@@ -49,30 +49,12 @@ class ExplorerInterestsWidget extends React.Component <IExplorerInterestsWidgetP
     return (Interests.findDocBySlug(splitSlug));
     //Interests.findIdBySlug(slug)
   }
-
-
-  /**
-   * reference from: https://stackoverflow.com/questions/32589197/capitalize-first-letter-of-each-word-in-a-string-javascript/32589256
-   * should try to get rid of the for loop later
-   * converts lower case name to
-   * @param interestName
-   * @constructor
-   */
-  private ConvertedInterestName = (interestName) =>{
-  var convertedName = interestName.toString().split('-').join(' ');
-  convertedName = convertedName.split(' ');
-  for(var i = 0; i<convertedName.length;i++){
-    convertedName[i] = convertedName[i].charAt(0).toUpperCase() + convertedName[i].substring(1);
-  }
-  return convertedName.join(' ');
-};
-
+  
 
 
   public render() {
     const interestName = this.GetInterestDoc().name;
     const interestDescription = this.GetInterestDoc().description;
-    const interestSlugID = this.GetInterestDoc()._id;
     return (
       <div className='ui paded container'>
         <div className="ui segments">
