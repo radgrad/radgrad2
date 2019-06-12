8b0ba8895de508cf1a18478bbb800d331e1a6f58
qauchida <qauchida@hawaii.edu>
2019-06-11 19:23:41 -1000

    put in conditional if the role is not student for getting courses and opportunities

--- app/imports/ui/components/shared/ExplorerInterestsWidget.tsx
+++ app/imports/ui/components/shared/ExplorerInterestsWidget.tsx
@@ -7,12 +7,8 @@ import {StudentProfiles} from "../../../api/user/StudentProfileCollection";
 import {CourseInstances} from "../../../api/course/CourseInstanceCollection";
 import {FacultyProfiles} from "../../../api/user/FacultyProfileCollection";
 import {MentorProfiles} from "../../../api/user/MentorProfileCollection";
-import {Doughnut} from 'react-chartjs-2';
 import {Courses} from "../../../api/course/CourseCollection"
 import {Opportunities} from "../../../api/opportunity/OpportunityCollection";
-import {interestIdToName} from "./AdminDataModelHelperFunctions";
-import {userInteractionDefineMethod} from "../../../api/analytic/UserInteractionCollection.methods";
-import {Users} from "../../../api/user/UserCollection";
 import {OpportunityInstances} from "../../../api/opportunity/OpportunityInstanceCollection";
 
 interface IExplorerInterestsWidgetProps {
@@ -26,7 +22,7 @@ interface IExplorerInterestsWidgetProps {
       interest: string;
     }
   }
-};
+}
 
 // don't know if we'll need this because state may not change
 interface IExplorerInterestsWidgetState {
@@ -58,13 +54,6 @@ class ExplorerInterestsWidget extends React.Component <IExplorerInterestsWidgetP
     let interested = [];
     switch (role) {
       case 'student':
-        //StudentProfiles.getInterestIDs(all the students);
-        // use _.map?
-        // need to return all students who have this interest in their profiles
-        // call this.GetInterestDoc().ID to get the interest ID
-        // go through the StudentProfiles Collection and compare the interest IDs to this.GetInterestDoc().interestID
-        // if there is a match, interested.push(theStudent);
-        // then assign howManyInterested = interested.length+1 to get the amount of students interested
         const students = StudentProfiles.findNonRetired();
         for (let a = 0; a < students.length; a++) {
           for (let i = 0; i < students[a].interestIDs.length; i++) {
@@ -122,42 +111,56 @@ class ExplorerInterestsWidget extends React.Component <IExplorerInterestsWidgetP
   };
 
 
-  private GetAssociationRelatedCourses = (courses) => {
+  /**
+   *
+   * ToDo need to add conditionals for other roles
+   * @param courses
+   * @constructor
+   */
+  private GetAssociationRelatedCourses = (courses, role) => {
     let inPlanIDs = [];
     let completedIDs = [];
 
-    const inPlanInstance = CourseInstances.findNonRetired({
-      'studentID':
-      StudentProfiles.findDoc(this.props.match.params.username).userID, 'verified': false,
-    });
-    _.map(inPlanInstance, (value) => {
-      inPlanIDs.push(value.courseID);
-    });
+    if (role != 'student') {
+      let relatedCourses: { completed: []; inPlan: []; notInPlan: []; } = {
+        completed: [],
+        inPlan: [],
+        notInPlan: []
+      }
+      return relatedCourses;
+    } else {
+      const inPlanInstance = CourseInstances.findNonRetired({
+        'studentID':
+        StudentProfiles.findDoc(this.props.match.params.username).userID, 'verified': false,
+      });
+      _.map(inPlanInstance, (value) => {
+        inPlanIDs.push(value.courseID);
+      });
 
-    const completedInstance = CourseInstances.findNonRetired({
-      'studentID':
-      StudentProfiles.findDoc(this.props.match.params.username).userID, 'verified': true
-    });
-    _.map(completedInstance, (value) => {
-      completedIDs.push(value.courseID);
-    });
+      const completedInstance = CourseInstances.findNonRetired({
+        'studentID':
+        StudentProfiles.findDoc(this.props.match.params.username).userID, 'verified': true
+      });
+      _.map(completedInstance, (value) => {
+        completedIDs.push(value.courseID);
+      });
 
-    let relatedIDs = [];
-    //shows all ids for related courses
-    _.map(courses, (value) => {
-      relatedIDs.push(value._id)
-    });
-    const relatedInPlanIDs = _.intersection(relatedIDs, inPlanIDs);
-    const relatedCompletedIDs = _.intersection(relatedIDs, completedIDs);
-    const relatedNotInPlanIDs = _.difference(relatedIDs, relatedInPlanIDs, relatedCompletedIDs);
+      let relatedIDs = [];
+      //shows all ids for related courses
+      _.map(courses, (value) => {
+        relatedIDs.push(value._id)
+      });
+      const relatedInPlanIDs = _.intersection(relatedIDs, inPlanIDs);
+      const relatedCompletedIDs = _.intersection(relatedIDs, completedIDs);
+      const relatedNotInPlanIDs = _.difference(relatedIDs, relatedInPlanIDs, relatedCompletedIDs);
 
-    let relatedCourses: { completed: []; inPlan: []; notInPlan: []; } = {
-      completed: relatedCompletedIDs,
-      inPlan: relatedInPlanIDs,
-      notInPlan: relatedNotInPlanIDs
-    };
-    return relatedCourses;
-
+      let relatedCourses: { completed: []; inPlan: []; notInPlan: []; } = {
+        completed: relatedCompletedIDs,
+        inPlan: relatedInPlanIDs,
+        notInPlan: relatedNotInPlanIDs
+      };
+      return relatedCourses;
+    }
   };
 
   private GetRelatedOpportunities = () => {
@@ -173,42 +176,50 @@ class ExplorerInterestsWidget extends React.Component <IExplorerInterestsWidgetP
     return opportunities;
   };
 
-  private GetAssociationRelatedOpportunities = (opportunities) => {
+  private GetAssociationRelatedOpportunities = (opportunities, role) => {
     let inPlanIDs = [];
     let completedIDs = [];
 
-    const inPlanInstance = OpportunityInstances.findNonRetired({
-      'studentID':
-      StudentProfiles.findDoc(this.props.match.params.username).userID, 'verified': false,
-    });
-    _.map(inPlanInstance, (value) => {
-      inPlanIDs.push(value.opportunityID);
-    });
+    if (role != 'student') {
+      let relatedOpportunities: { completed: []; inPlan: []; notInPlan: []; } = {
+        completed: [],
+        inPlan: [],
+        notInPlan: []
+      }
+      return relatedOpportunities;
+    } else {
+      const inPlanInstance = OpportunityInstances.findNonRetired({
+        'studentID':
+        StudentProfiles.findDoc(this.props.match.params.username).userID, 'verified': false,
+      });
+      _.map(inPlanInstance, (value) => {
+        inPlanIDs.push(value.opportunityID);
+      });
 
-    const completedInstance = OpportunityInstances.findNonRetired({
-      'studentID':
-      StudentProfiles.findDoc(this.props.match.params.username).userID, 'verified': true
-    });
-    _.map(completedInstance, (value) => {
-      completedIDs.push(value.opportunityID);
-    });
-    let relatedIDs = [];
-    //shows all ids for related courses
-    _.map(opportunities, (value) => {
-      relatedIDs.push(value._id)
-    });
+      const completedInstance = OpportunityInstances.findNonRetired({
+        'studentID':
+        StudentProfiles.findDoc(this.props.match.params.username).userID, 'verified': true
+      });
+      _.map(completedInstance, (value) => {
+        completedIDs.push(value.opportunityID);
+      });
+      let relatedIDs = [];
+      //shows all ids for related courses
+      _.map(opportunities, (value) => {
+        relatedIDs.push(value._id)
+      });
 
-    const relatedInPlanIDs = _.intersection(relatedIDs, inPlanIDs);
-    const relatedCompletedIDs = _.intersection(relatedIDs, completedIDs);
-    const relatedNotInPlanIDs = _.difference(relatedIDs, relatedInPlanIDs, relatedCompletedIDs);
+      const relatedInPlanIDs = _.intersection(relatedIDs, inPlanIDs);
+      const relatedCompletedIDs = _.intersection(relatedIDs, completedIDs);
+      const relatedNotInPlanIDs = _.difference(relatedIDs, relatedInPlanIDs, relatedCompletedIDs);
 
-    let relatedOpportunities: { completed: []; inPlan: []; notInPlan: []; } = {
-      completed: relatedCompletedIDs,
-      inPlan: relatedInPlanIDs,
-      notInPlan: relatedNotInPlanIDs
-    };
-    return relatedOpportunities;
-
+      let relatedOpportunities: { completed: []; inPlan: []; notInPlan: []; } = {
+        completed: relatedCompletedIDs,
+        inPlan: relatedInPlanIDs,
+        notInPlan: relatedNotInPlanIDs
+      };
+      return relatedOpportunities;
+    }
   };
 
 
@@ -245,13 +256,16 @@ class ExplorerInterestsWidget extends React.Component <IExplorerInterestsWidgetP
     const interestDoc = this.GetInterestDoc();
     const interestName = interestDoc.name;
     const interestDescription = interestDoc.description;
-    const relatedCourses = this.GetAssociationRelatedCourses(this.GetRelatedCourses());
-    const relatedOpportunities = this.GetAssociationRelatedOpportunities(this.GetRelatedOpportunities());
+    const relatedCourses = this.GetAssociationRelatedCourses(this.GetRelatedCourses(), this.props.match.url.split('/')[1]);
+    const relatedOpportunities = this.GetAssociationRelatedOpportunities(this.GetRelatedOpportunities(), this.props.match.url.split('/')[1]);
     const interestedStudents = this.Participation('student');
     const interestedFaculty = this.Participation('faculty');
     const interestedAlumni = this.Participation('alumni');
     const interestedMentor = this.Participation('mentor');
 
+    /**
+     * ToDo polish this UI
+     */
     return (
       <div className='ui paded container'>
         <div className="ui segments">
@@ -313,7 +327,8 @@ class ExplorerInterestsWidget extends React.Component <IExplorerInterestsWidgetP
                 {
                   _.map(relatedOpportunities.completed, (value) =>
                     <div>
-                      <Link to={this.GenerateOpportunityRoute(Opportunities.findDoc(value))}>{Opportunities.findDoc(value).name}</Link>
+                      <Link
+                        to={this.GenerateOpportunityRoute(Opportunities.findDoc(value))}>{Opportunities.findDoc(value).name}</Link>
                     </div>)
                 }</div>
               <div>
@@ -321,7 +336,8 @@ class ExplorerInterestsWidget extends React.Component <IExplorerInterestsWidgetP
                 {
                   _.map(relatedOpportunities.inPlan, (value) =>
                     <div>
-                      <Link to={this.GenerateOpportunityRoute(Opportunities.findDoc(value))}>{Opportunities.findDoc(value).name}</Link>
+                      <Link
+                        to={this.GenerateOpportunityRoute(Opportunities.findDoc(value))}>{Opportunities.findDoc(value).name}</Link>
                     </div>)
                 }</div>
               <div>
@@ -329,7 +345,8 @@ class ExplorerInterestsWidget extends React.Component <IExplorerInterestsWidgetP
                 {
                   _.map(relatedOpportunities.notInPlan, (value) =>
                     <div>
-                      <Link to={this.GenerateOpportunityRoute(Opportunities.findDoc(value))}>{Opportunities.findDoc(value).name}</Link>
+                      <Link
+                        to={this.GenerateOpportunityRoute(Opportunities.findDoc(value))}>{Opportunities.findDoc(value).name}</Link>
                     </div>)
                 }</div>
             </Container>
