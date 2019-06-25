cf839fe8587af4efcdd667b6c91f5d23a563c4cc
qauchida <qauchida@hawaii.edu>
2019-06-24 15:24:49 -1000

    :triangular_flag_on_post: won't let me ammend last commit, have to make new one

--- app/imports/ui/components/admin/AdminAnalyticsMenuWidget.tsx
+++ app/imports/ui/components/admin/AdminAnalyticsMenuWidget.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter, Link, NavLink } from 'react-router-dom';
 import { Segment, Menu } from 'semantic-ui-react';
 
 interface IAdminAnalyticsMenuWidget {
@@ -23,7 +23,7 @@ class AdminAnalyticsMenuWidget extends React.Component<IAdminAnalyticsMenuWidget
     console.log('menu widget url', this.props.match.url);
     let helper: any = this.props.match.url.split('/');
     console.log(helper);
-    const urlList = [helper[1], helper[2], helper[3],slug];
+    const urlList = [helper[1], helper[2], helper[3], slug];
     helper = urlList.join('/');
     return `/${helper}`;
   }
@@ -35,11 +35,12 @@ class AdminAnalyticsMenuWidget extends React.Component<IAdminAnalyticsMenuWidget
     return (
       <div>
         <Menu text vertical>
-          <Menu.Item><Link to={this.buildRoute('')}>Logged In Students</Link></Menu.Item>
-          <Menu.Item><Link to={this.buildRoute('newsletter')}>Newsletter</Link></Menu.Item>
-          <Menu.Item><Link to={this.buildRoute('overhead-analysis')}>Overhead Analysis</Link></Menu.Item>
-          <Menu.Item><Link to={this.buildRoute('student-summary')}>Student Summary</Link></Menu.Item>
-          <Menu.Item><Link to={this.buildRoute('user-interactions')}>User Interactions</Link></Menu.Item>
+          <Menu.Item as={NavLink} to={this.buildRoute('')} active={false}>Logged In Users</Menu.Item>
+          <Menu.Item as={NavLink} to={this.buildRoute('newsletter')}>Newsletter</Menu.Item>
+          <Menu.Item as={NavLink} to={this.buildRoute('overhead-analysis')}>Overhead Analysis</Menu.Item>
+          <Menu.Item as={NavLink} to={this.buildRoute('student-summary')}>Student Summary</Menu.Item>
+          <Menu.Item as={NavLink} to={this.buildRoute('user-interactions')}>User Interactions</Menu.Item>
+
         </Menu>
       </div>
     )
--- app/imports/ui/components/admin/AdminAnalyticsNewsletterWidget.tsx
+++ app/imports/ui/components/admin/AdminAnalyticsNewsletterWidget.tsx
@@ -1,24 +1,84 @@
 import * as React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Segment, Header } from "semantic-ui-react";
+import { Segment, Header, Grid, Form, Button, Label } from "semantic-ui-react";
 
-interface IAdminAnalyticsNewsletterWidget{
+interface IAdminAnalyticsNewsletterWidget {
 
 }
 
-class AdminAnalyticsNewsletterWidget extends React.Component<IAdminAnalyticsNewsletterWidget>{
-  constructor(props){
+interface IAdminAnalyticsNewsletterWidgetState {
+  inputMessage: string,
+  bcc: string,
+  subjectLine: string,
+  studentEmails: string,
+  sendToStudentsToo: boolean,
+  level: number,
+  confirmSendLevel: boolean,
+  confirmSendAll: boolean,
+}
+
+class AdminAnalyticsNewsletterWidget extends React.Component<IAdminAnalyticsNewsletterWidget, IAdminAnalyticsNewsletterWidgetState> {
+  constructor(props) {
     super(props)
+    this.state = {
+      inputMessage: '',
+      bcc: '',
+      subjectLine: '',
+      studentEmails: '',
+      sendToStudentsToo: false,
+      level: 0,
+      confirmSendLevel: false,
+      confirmSendAll: false
+    }
   }
 
-  public render(){
-    return(
+  private handleChange = (e, {name, value}) => {
+    console.log(name, value);
+    this.setState({ [name]: value });
+    console.log(this.state);
+  }
+
+  public render() {
+    console.log(this.state);
+    return (
       <div>
         <Segment>
           <Header as='h4' dividing>NEWSLETTER OPTIONS</Header>
+          <Grid padded>
+
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <Form>
+                  <Form.TextArea required label='Input admin message in markdown format'
+                                 input={this.state.inputMessage} onChange={this.handleChange} name='inputMessage'/>
+                  <Button color='green' basic>Preview And Save</Button>
+                </Form>
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <Form>
+                  <Form.Field label='Message Preview'/>
+                  <p>Aloha Student!</p>
+                    <div className='markdown'>
+
+                    </div>
+                  <p>- The RadGrad Team</p>
+                </Form>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <Form>
+                <Form.Input label='BCC recipients' placeholder ='Input comma separated list of bcc recipients' onChange={this.handleChange} name='bcc'/>
+                <Form.Input label='Subject line' placeholder='Input email subject line' onChange={this.handleChange} name='subjectLine' required/>
+                </Form>
+              </Grid.Column>
+            </Grid.Row>
+
+          </Grid>
         </Segment>
       </div>
     )
   }
 }
+
 export default withRouter(AdminAnalyticsNewsletterWidget);
