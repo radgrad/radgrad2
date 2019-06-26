b350a9d6caafddfea80a4cf123e3140f5f32a879
gbarcelo <gbarcelo@users.noreply.github.com>
2019-06-25 14:38:50 -1000

    :sparkles: Completed FacultyVerificationPage (issue-23)
     - Changed some formatting from radgrad1
     - Added functionality: faculty can now see Completed Verifications if they have permissions (e.g. they are the owner)

--- app/imports/ui/pages/faculty/FacultyVerificationPage.tsx
+++ app/imports/ui/pages/faculty/FacultyVerificationPage.tsx
@@ -1,39 +1,102 @@
 import * as React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Container, Grid, Menu } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { withRouter } from 'react-router';
 import FacultyPageMenuWidget from '../../components/faculty/FacultyPageMenuWidget';
 import withGlobalSubscription from '../../layouts/shared/GlobalSubscriptionsHOC';
 import withInstanceSubscriptions from '../../layouts/shared/InstanceSubscriptionsHOC';
 import HelpPanelWidgetContainer from '../../components/shared/HelpPanelWidget';
+import PendingVerificationsWidget from '../../components/shared/PendingVerificationsWidget';
+import { VerificationRequests } from '../../../api/verification/VerificationRequestCollection';
+import EventVerificationsWidget from '../../components/shared/EventVerificationsWidget';
+// eslint-disable-next-line no-unused-vars
+import { IOpportunity, IVerificationRequest } from '../../../typings/radgrad';
+import { Opportunities } from '../../../api/opportunity/OpportunityCollection';
+import { Users } from '../../../api/user/UserCollection';
+import { OpportunityInstances } from '../../../api/opportunity/OpportunityInstanceCollection';
+import CompletedVerificationsWidget from '../../components/shared/CompletedVerificationsWidget';
+
+interface FacultyVerificationPageProps {
+  verificationRequests: IVerificationRequest[];
+  eventOpportunities: IOpportunity[];
+  match: {
+    params: {
+      username: string;
+    }
+  }
+}
 
 /** A simple static component to render some text for the landing page. */
-class FacultyVerificationPage extends React.Component {
+class FacultyVerificationPage extends React.Component<FacultyVerificationPageProps> {
+  state = { activeItem: 'pending' };
+
+  handleMenu = (e, { name }) => this.setState({ activeItem: name });
+
   public render() {
-    const moveDownStyle = {
-      marginTop: 10,
-    };
+    const { activeItem } = this.state;
     return (
       <div>
         <FacultyPageMenuWidget/>
-        <Grid verticalAlign="middle" container={true} style={moveDownStyle}>
-          <Grid.Row>
-            <Grid.Column width={16}><HelpPanelWidgetContainer/></Grid.Column>
-          </Grid.Row>
-
-          <Grid.Column width={4}>
-            <Image size="small" circular={true} src="/images/radgrad_logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Faculty Verification</h1>
-          </Grid.Column>
-
-        </Grid>
+        <Container fluid={false}>
+          <Grid stackable={true}>
+            <Grid.Column width={14}>
+              <Grid.Row style={{ paddingBottom: '0px', paddingTop: '14px' }}>
+                <HelpPanelWidgetContainer/>
+              </Grid.Row>
+            </Grid.Column>
+            <Grid.Row style={{ paddingTop: '0px' }}>
+              <Grid.Column width={3}>
+                <Menu vertical={true} text={true}>
+                  <Menu.Item name={'pending'}
+                             active={activeItem === 'pending'}
+                             onClick={this.handleMenu}>
+                    Pending Verifications
+                  </Menu.Item>
+                  <Menu.Item name={'event'}
+                             active={activeItem === 'event'}
+                             onClick={this.handleMenu}>
+                    Event Verifications
+                  </Menu.Item>
+                  <Menu.Item name={'completed'}
+                             active={activeItem === 'completed'}
+                             onClick={this.handleMenu}>
+                    Completed Verifications
+                  </Menu.Item>
+                </Menu>
+              </Grid.Column>
+              <Grid.Column width={11}>
+                {activeItem === 'pending' ?
+                  <PendingVerificationsWidget
+                    pendingVerifications={this.props.verificationRequests.filter(ele => ele.status === VerificationRequests.OPEN)}/>
+                  : undefined}
+                {activeItem === 'event' ?
+                  <EventVerificationsWidget eventOpportunities={this.props.eventOpportunities}/>
+                  : undefined}
+                {activeItem === 'completed' ?
+                  <CompletedVerificationsWidget username={this.props.match.params.username}
+                                                completedVerifications={this.props.verificationRequests.filter(ele => VerificationRequests.ACCEPTED === ele.status || ele.status === VerificationRequests.REJECTED)}/>
+                  : undefined}
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
       </div>
     );
   }
 }
 
-const FacultyVerificationPageCon = withGlobalSubscription(FacultyVerificationPage);
-const FacultyVerificationPageContainer = withInstanceSubscriptions(FacultyVerificationPageCon);
+const FacultyVerificationPageWithGSub = withGlobalSubscription(FacultyVerificationPage);
+const FacultyVerificationPageWithISub = withInstanceSubscriptions(FacultyVerificationPageWithGSub);
+const FacultyVerificationPageWithTracker = withTracker((props) => {
+  const userID = Users.getID(props.match.params.username);
+  const linkedOppInstances = OpportunityInstances.findNonRetired({ sponsorID: userID });
+  const isLinkedReq = (verReq: IVerificationRequest) => !!linkedOppInstances.find(oppI => verReq.opportunityInstanceID === oppI._id);
+  return {
+    verificationRequests: VerificationRequests.findNonRetired().filter(ele => isLinkedReq(ele)),
+    eventOpportunities: Opportunities.find({ eventDate: { $exists: true } }).fetch(),
+  };
+})(FacultyVerificationPageWithISub);
+const FacultyVerificationPageWithRouter = withRouter(FacultyVerificationPageWithTracker);
+const FacultyVerificationPageContainer = FacultyVerificationPageWithRouter;
 
 export default FacultyVerificationPageContainer;
