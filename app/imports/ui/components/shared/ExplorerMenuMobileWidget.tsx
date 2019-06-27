9ed5bdaacede444948a722097f617ebab887857a
Mercedez Castro <mercedez@hawaii.edu>
2019-06-26 15:04:13 -1000

    :triangular_flag_on_post: Checkpoint - MobileItem added

--- app/imports/ui/components/shared/ExplorerMenuMobileWidget.tsx
+++ app/imports/ui/components/shared/ExplorerMenuMobileWidget.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Dropdown, Responsive } from 'semantic-ui-react';
-import { NavLink, withRouter } from 'react-router-dom';
-import * as _ from 'lodash';
+import { withRouter } from 'react-router-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 import {
   IAcademicPlan, //eslint-disable-line
@@ -13,11 +12,10 @@ import {
   IProfile, // eslint-disable-line
 } from '../../../typings/radgrad';
 import { Users } from '../../../api/user/UserCollection';
-import { CourseInstances } from '../../../api/course/CourseInstanceCollection';
-import { OpportunityInstances } from '../../../api/opportunity/OpportunityInstanceCollection';
-import { Slugs } from '../../../api/slug/SlugCollection';
 import * as Router from './RouterHelperFunctions';
 import { EXPLORER_TYPE } from '../../../startup/client/routes-config';
+import ExplorerMenuMobileItem from './ExplorerMenuMobileItem';
+
 
 type explorerInterfaces = IAcademicPlan | ICareerGoal | ICourse | IDesiredDegree | IInterest | IOpportunity;
 
@@ -44,161 +42,15 @@ class ExplorerMenuMobileWidget extends React.Component<IExplorerMenuMobileWidget
   }
 
   /* ####################################### GENERAL HELPER FUNCTIONS ############################################ */
-  private getUsername = (): string => Router.getUsername(this.props.match);
-
-  private getUserIdFromRoute = (): string => Router.getUserIdFromRoute(this.props.match);
 
   private isRoleStudent = (): boolean => Router.isUrlRoleStudent(this.props.match);
-
-  private getTypeName = (): string => {
-    const { type } = this.props;
-    const names = ['Academic Plans', 'Career Goals', 'Courses', 'Degrees', 'Interests', 'Opportunities', 'Users'];
-    switch (type) {
-      case EXPLORER_TYPE.ACADEMICPLANS:
-        return names[0];
-      case EXPLORER_TYPE.CAREERGOALS:
-        return names[1];
-      case EXPLORER_TYPE.COURSES:
-        return names[2];
-      case EXPLORER_TYPE.DEGREES:
-        return names[3];
-      case EXPLORER_TYPE.INTERESTS:
-        return names[4];
-      case EXPLORER_TYPE.OPPORTUNITIES:
-        return names[5];
-      case EXPLORER_TYPE.USERS:
-        return names[6];
-      default:
-        return '';
-    }
-  }
 
   private isType = (typeToCheck: string): boolean => {
     const { type } = this.props;
     return type === typeToCheck;
   }
 
-  // Determines whether or not we show a "check green circle outline icon" for an item
-  private getItemStatus = (item: explorerInterfaces): string => {
-    const { type } = this.props;
-    switch (type) {
-      case EXPLORER_TYPE.ACADEMICPLANS:
-        return this.userPlans(item as IAcademicPlan);
-      case EXPLORER_TYPE.CAREERGOALS:
-        return this.userCareerGoals(item as ICareerGoal);
-      case EXPLORER_TYPE.COURSES:
-        return this.userCourses(item as ICourse);
-      // case 'degrees': users currently cannot add a desired degree to their profile
-      //   return this.userDegrees(item.item as DesiredDegree);
-      case EXPLORER_TYPE.INTERESTS:
-        return this.userInterests(item as IInterest);
-      case EXPLORER_TYPE.OPPORTUNITIES:
-        return this.userOpportunities(item as IOpportunity);
-      case EXPLORER_TYPE.USERS: // do nothing
-        return '';
-      default:
-        return '';
-    }
-  }
-
-  private itemName = (item: { item: explorerInterfaces, count: number }): string => {
-    const countStr = `x${item.count}`;
-    if (item.count > 1) {
-      return `${item.item.name} ${countStr}`;
-    }
-    return `${item.item.name}`;
-  }
-
-  private slugName = (item: { [key: string]: any }): string => Slugs.findDoc(item.slugID).name;
-
-  /* ####################################### ACADEMIC PLANS HELPER FUNCTIONS ####################################### */
-  private userPlans = (plan: IAcademicPlan): string => {
-    let ret = '';
-    const profile = Users.getProfile(this.getUsername());
-    if (_.includes(profile.academicPlanID, plan._id)) {
-      ret = 'check green circle outline icon';
-    }
-    return ret;
-  }
-
-  /* ####################################### CAREER GOALS HELPER FUNCTIONS ######################################### */
-  private userCareerGoals = (careerGoal: ICareerGoal): string => {
-    let ret = '';
-    const profile = Users.getProfile(this.getUsername());
-    if (_.includes(profile.careerGoalIDs, careerGoal._id)) {
-      ret = 'check green circle outline icon';
-    }
-    return ret;
-  }
-
-  /* ####################################### COURSES HELPER FUNCTIONS ############################################## */
-  private userCourses = (course: ICourse): string => {
-    let ret = '';
-    const ci = CourseInstances.find({
-      studentID: this.getUserIdFromRoute(),
-      courseID: course._id,
-    }).fetch();
-    if (ci.length > 0) {
-      ret = 'check green circle outline icon';
-
-    }
-    return ret;
-  }
-
-  private courseName = (course: { item: ICourse, count: number }): string => {
-    const countStr = `x${course.count}`;
-    if (course.count > 1) {
-      return `${course.item.shortName} ${countStr}`;
-    }
-    return `${course.item.shortName}`;
-  }
-
-  /* ####################################### INTERESTS HELPER FUNCTIONS ############################################ */
-  private userInterests = (interest: IInterest): string => {
-    let ret = '';
-    const profile = Users.getProfile(this.getUsername());
-    if (_.includes(Users.getInterestIDs(profile.userID), interest._id)) {
-      ret = 'check green circle outline icon';
-    }
-    return ret;
-  }
-
-  /* ####################################### OPPORTUNITIES HELPER FUNCTIONS ######################################## */
-  private userOpportunities = (opportunity: IOpportunity): string => {
-    let ret = '';
-    const oi = OpportunityInstances.find({
-      studentID: this.getUserIdFromRoute(),
-      opportunityID: opportunity._id,
-    }).fetch();
-    if (oi.length > 0) {
-      ret = 'check green circle outline icon';
-
-    }
-    return ret;
-  }
-
-  private opportunityItemName = (item: { item: IOpportunity, count: number }): string => {
-    const countStr = `x${item.count}`;
-    const iceString = `(${item.item.ice.i}/${item.item.ice.c}/${item.item.ice.e})`;
-    if (item.count > 1) {
-      return `${item.item.name} ${iceString} ${countStr}`;
-    }
-    return `${item.item.name} ${iceString}`;
-  }
-
-  /* ####################################### MOBILE HELPER FUNCTIONS ############################################### */
-
-  // These are functions to help build the Dropdown for mobile
   public render(): React.ReactElement<any> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-    const iconStyle: React.CSSProperties = {
-      position: 'absolute',
-      marginLeft: '-20px',
-    };
-
-    const baseUrl = this.props.match.url;
-    const username = this.getUsername();
-    const baseIndex = baseUrl.indexOf(username);
-    const baseRoute = `${baseUrl.substring(0, baseIndex)}${username}`;
 
     const { menuAddedList, menuCareerList } = this.props;
     const isStudent = this.isRoleStudent();
@@ -216,14 +68,7 @@ class ExplorerMenuMobileWidget extends React.Component<IExplorerMenuMobileWidget
                         <Dropdown.Divider/>
                         {
                           menuAddedList.map((listItem, index) => (
-                            <Dropdown.Item as={NavLink} key={index} exact={true}
-                                           to={`${baseRoute}/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.ACADEMICPLANS}/${this.slugName(listItem.item)}`}
-                                           text={(
-                                             <React.Fragment>
-                                               <i className={this.getItemStatus(listItem.item)} style={iconStyle}/>
-                                               {this.itemName(listItem)}
-                                             </React.Fragment>
-                                           )}/>
+                            <ExplorerMenuMobileItem type={EXPLORER_TYPE.ACADEMICPLANS} listItem={listItem} key={index} match={this.props.match}/>
                           ))
                         }
                       </Dropdown.Menu>
@@ -245,14 +90,7 @@ class ExplorerMenuMobileWidget extends React.Component<IExplorerMenuMobileWidget
                         <Dropdown.Divider/>
                         {
                           menuAddedList.map((listItem, index) => (
-                            <Dropdown.Item as={NavLink} key={index} exact={true}
-                                           to={`${baseRoute}/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.COURSES}/${this.slugName(listItem.item)}`}
-                                           text={(
-                                             <React.Fragment>
-                                               <i className={this.getItemStatus(listItem.item)} style={iconStyle}/>
-                                               {this.courseName(listItem as { item: ICourse, count: number })}
-                                             </React.Fragment>
-                                           )}/>
+                            <ExplorerMenuMobileItem type={EXPLORER_TYPE.COURSES} listItem={listItem} key={index} match={this.props.match}/>
                           ))
                         }
                       </Dropdown.Menu>
@@ -274,14 +112,7 @@ class ExplorerMenuMobileWidget extends React.Component<IExplorerMenuMobileWidget
                         <Dropdown.Divider/>
                         {
                           menuAddedList.map((listItem, index) => (
-                            <Dropdown.Item as={NavLink} key={index} exact={true}
-                                           to={`${baseRoute}/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.OPPORTUNITIES}/${this.slugName(listItem.item)}`}
-                                           text={(
-                                             <React.Fragment>
-                                               <i className={this.getItemStatus(listItem.item)} style={iconStyle}/>
-                                               {this.itemName(listItem)}
-                                             </React.Fragment>
-                                           )}/>
+                            <ExplorerMenuMobileItem type={EXPLORER_TYPE.OPPORTUNITIES} listItem={listItem} key={index} match={this.props.match}/>
                           ))
                         }
                       </Dropdown.Menu>
@@ -301,14 +132,7 @@ class ExplorerMenuMobileWidget extends React.Component<IExplorerMenuMobileWidget
                   <Dropdown.Divider/>
                   {
                     menuAddedList.map((listItem, index) => (
-                      <Dropdown.Item as={NavLink} key={index} exact={true}
-                                     to={`${baseRoute}/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.INTERESTS}/${this.slugName(listItem.item)}`}
-                                     text={(
-                                       <React.Fragment>
-                                         <i className={this.getItemStatus(listItem.item)} style={iconStyle}/>
-                                         {this.itemName(listItem)}
-                                       </React.Fragment>
-                                     )}/>
+                      <ExplorerMenuMobileItem type={EXPLORER_TYPE.INTERESTS} listItem={listItem} key={index} match={this.props.match}/>
                     ))
                   }
 
@@ -316,14 +140,7 @@ class ExplorerMenuMobileWidget extends React.Component<IExplorerMenuMobileWidget
                   <Dropdown.Divider/>
                   {
                     menuCareerList.map((listItem, index) => (
-                      <Dropdown.Item as={NavLink} key={index} exact={true}
-                                     to={`${baseRoute}/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.INTERESTS}/${this.slugName(listItem.item)}`}
-                                     text={(
-                                       <React.Fragment>
-                                         <i className={this.getItemStatus(listItem.item)} style={iconStyle}/>
-                                         {this.itemName(listItem)}
-                                       </React.Fragment>
-                                     )}/>
+                      <ExplorerMenuMobileItem type={EXPLORER_TYPE.INTERESTS} listItem={listItem} key={index} match={this.props.match}/>
                     ))
                   }
                 </Dropdown.Menu>
@@ -339,14 +156,7 @@ class ExplorerMenuMobileWidget extends React.Component<IExplorerMenuMobileWidget
                   <Dropdown.Divider/>
                   {
                     menuAddedList.map((listItem, index) => (
-                      <Dropdown.Item as={NavLink} key={index} exact={true}
-                                     to={`${baseRoute}/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.CAREERGOALS}/${this.slugName(listItem.item)}`}
-                                     text={(
-                                       <React.Fragment>
-                                         <i className={this.getItemStatus(listItem.item)} style={iconStyle}/>
-                                         {this.itemName(listItem)}
-                                       </React.Fragment>
-                                     )}/>
+                      <ExplorerMenuMobileItem type={EXPLORER_TYPE.CAREERGOALS} listItem={listItem} key={index} match={this.props.match}/>
                     ))
                   }
                 </Dropdown.Menu>
