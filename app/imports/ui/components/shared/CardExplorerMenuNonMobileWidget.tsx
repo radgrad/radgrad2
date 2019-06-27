9730ac9423761830a92b40386d667a9040baa40d
Mercedez Castro <mercedez@hawaii.edu>
2019-06-26 14:34:04 -1000

    :triangular_flag_on_post: Checkpoint - Card NonMobileItem "Widget" added

--- app/imports/ui/components/shared/CardExplorerMenuNonMobileWidget.tsx
+++ app/imports/ui/components/shared/CardExplorerMenuNonMobileWidget.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import * as _ from 'lodash';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Menu, Header, Responsive } from 'semantic-ui-react';
-import { NavLink, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import * as Router from './RouterHelperFunctions';
 import {
   IAcademicPlan, //eslint-disable-line
@@ -13,10 +12,8 @@ import {
   IOpportunity, //eslint-disable-line
 } from '../../../typings/radgrad';
 import { Users } from '../../../api/user/UserCollection';
-import { CourseInstances } from '../../../api/course/CourseInstanceCollection';
-import { OpportunityInstances } from '../../../api/opportunity/OpportunityInstanceCollection';
-import { Slugs } from '../../../api/slug/SlugCollection';
 import { EXPLORER_TYPE } from '../../../startup/client/routes-config';
+import ExplorerMenuNonMobileItem from './ExplorerMenuNonMobileItem';
 
 type explorerInterfaces = IAcademicPlan | ICareerGoal | ICourse | IDesiredDegree | IInterest | IOpportunity;
 
@@ -42,9 +39,6 @@ class CardExplorerMenuNonMobileWidget extends React.Component<ICardExplorerMenuN
   }
 
   /* ####################################### GENERAL HELPER FUNCTIONS ############################################ */
-  private getUsername = (): string => Router.getUsername(this.props.match);
-
-  private getUserIdFromRoute = (): string => Router.getUserIdFromRoute(this.props.match);
 
   private isRoleStudent = (): boolean => Router.isUrlRoleStudent(this.props.match);
 
@@ -53,125 +47,7 @@ class CardExplorerMenuNonMobileWidget extends React.Component<ICardExplorerMenuN
     return type === typeToCheck;
   }
 
-  // Determines whether or not we show a "check green circle outline icon" for an item
-  private getItemStatus = (item: explorerInterfaces): string => {
-    const { type } = this.props;
-    switch (type) {
-      case EXPLORER_TYPE.ACADEMICPLANS:
-        return this.userPlans(item as IAcademicPlan);
-      case EXPLORER_TYPE.CAREERGOALS:
-        return this.userCareerGoals(item as ICareerGoal);
-      case EXPLORER_TYPE.COURSES:
-        return this.userCourses(item as ICourse);
-      // case 'degrees': users currently cannot add a desired degree to their profile
-      //   return this.userDegrees(item.item as DesiredDegree);
-      case EXPLORER_TYPE.INTERESTS:
-        return this.userInterests(item as IInterest);
-      case EXPLORER_TYPE.OPPORTUNITIES:
-        return this.userOpportunities(item as IOpportunity);
-      case EXPLORER_TYPE.USERS: // do nothing
-        return '';
-      default:
-        return '';
-    }
-  }
-
-  private itemName = (item: { item: explorerInterfaces, count: number }): string => {
-    const countStr = `x${item.count}`;
-    if (item.count > 1) {
-      return `${item.item.name} ${countStr}`;
-    }
-    return `${item.item.name}`;
-  }
-
-  private slugName = (item: { [key: string]: any }): string => Slugs.findDoc(item.slugID).name;
-
-  /* ####################################### ACADEMIC PLANS HELPER FUNCTIONS ####################################### */
-  private userPlans = (plan: IAcademicPlan): string => {
-    let ret = '';
-    const profile = Users.getProfile(this.getUsername());
-    if (_.includes(profile.academicPlanID, plan._id)) {
-      ret = 'check green circle outline icon';
-    }
-    return ret;
-  }
-
-  /* ####################################### CAREER GOALS HELPER FUNCTIONS ######################################### */
-  private userCareerGoals = (careerGoal: ICareerGoal): string => {
-    let ret = '';
-    const profile = Users.getProfile(this.getUsername());
-    if (_.includes(profile.careerGoalIDs, careerGoal._id)) {
-      ret = 'check green circle outline icon';
-    }
-    return ret;
-  }
-
-  /* ####################################### COURSES HELPER FUNCTIONS ############################################## */
-  private userCourses = (course: ICourse): string => {
-    let ret = '';
-    const ci = CourseInstances.find({
-      studentID: this.getUserIdFromRoute(),
-      courseID: course._id,
-    }).fetch();
-    if (ci.length > 0) {
-      ret = 'check green circle outline icon';
-
-    }
-    return ret;
-  }
-
-  private courseName = (course: { item: ICourse, count: number }): string => {
-    const countStr = `x${course.count}`;
-    if (course.count > 1) {
-      return `${course.item.shortName} ${countStr}`;
-    }
-    return `${course.item.shortName}`;
-  }
-
-  /* ####################################### INTERESTS HELPER FUNCTIONS ############################################ */
-  private userInterests = (interest: IInterest): string => {
-    let ret = '';
-    const profile = Users.getProfile(this.getUsername());
-    if (_.includes(Users.getInterestIDs(profile.userID), interest._id)) {
-      ret = 'check green circle outline icon';
-    }
-    return ret;
-  }
-
-  /* ####################################### OPPORTUNITIES HELPER FUNCTIONS ######################################## */
-  private userOpportunities = (opportunity: IOpportunity): string => {
-    let ret = '';
-    const oi = OpportunityInstances.find({
-      studentID: this.getUserIdFromRoute(),
-      opportunityID: opportunity._id,
-    }).fetch();
-    if (oi.length > 0) {
-      ret = 'check green circle outline icon';
-
-    }
-    return ret;
-  }
-
-  private opportunityItemName = (item: { item: IOpportunity, count: number }): string => {
-    const countStr = `x${item.count}`;
-    const iceString = `(${item.item.ice.i}/${item.item.ice.c}/${item.item.ice.e})`;
-    if (item.count > 1) {
-      return `${item.item.name} ${iceString} ${countStr}`;
-    }
-    return `${item.item.name} ${iceString}`;
-  }
-
   public render(): React.ReactElement<any> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-    const iconStyle: React.CSSProperties = {
-      position: 'absolute',
-      marginLeft: '-20px',
-    };
-
-    const baseUrl = this.props.match.url;
-    const username = this.getUsername();
-    const baseIndex = baseUrl.indexOf(username);
-    const baseRoute = `${baseUrl.substring(0, baseIndex)}${username}`;
-
     const { menuAddedList, menuCareerList } = this.props;
     const isStudent = this.isRoleStudent();
     const adminEmail = 'radgrad@hawaii.edu';
@@ -190,11 +66,8 @@ class CardExplorerMenuNonMobileWidget extends React.Component<ICardExplorerMenuN
                       <Header as="h4" dividing={true}>MY ACADEMIC PLAN</Header>
                       {
                         menuAddedList.map((listItem, index) => (
-                          <Menu.Item as={NavLink} key={index} exact={true}
-                                     to={`${baseRoute}/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.ACADEMICPLANS}/${this.slugName(listItem.item)}`}>
-                            <i className={this.getItemStatus(listItem.item)} style={iconStyle}/>
-                            {this.itemName(listItem)}
-                          </Menu.Item>
+                          <ExplorerMenuNonMobileItem listItem={listItem} type={EXPLORER_TYPE.ACADEMICPLANS} key={index}
+                                                     match={this.props.match}/>
                         ))
                       }
                     </Menu>
@@ -213,11 +86,8 @@ class CardExplorerMenuNonMobileWidget extends React.Component<ICardExplorerMenuN
                       <Header as="h4" dividing={true}>COURSES IN MY PLAN</Header>
                       {
                         menuAddedList.map((listItem, index) => (
-                          <Menu.Item as={NavLink} key={index} exact={true}
-                                     to={`${baseRoute}/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.COURSES}/${this.slugName(listItem.item)}`}>
-                            <i className={this.getItemStatus(listItem.item)} style={iconStyle}/>
-                            {this.courseName(listItem as { item: ICourse, count: number })}
-                          </Menu.Item>
+                          <ExplorerMenuNonMobileItem listItem={listItem} type={EXPLORER_TYPE.COURSES} key={index}
+                                                     match={this.props.match}/>
                         ))
                       }
                     </Menu>
@@ -237,11 +107,8 @@ class CardExplorerMenuNonMobileWidget extends React.Component<ICardExplorerMenuN
                       <Header as="h4" dividing={true}>OPPORTUNITIES IN MY PLAN</Header>
                       {
                         menuAddedList.map((listItem, index) => (
-                          <Menu.Item as={NavLink} key={index} exact={true}
-                                     to={`${baseRoute}/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.OPPORTUNITIES}/${this.slugName(listItem.item)}`}>
-                            <i className={this.getItemStatus(listItem.item)} style={iconStyle}/>
-                            {this.opportunityItemName(listItem as { item: IOpportunity, count: number })}
-                          </Menu.Item>
+                          <ExplorerMenuNonMobileItem listItem={listItem} type={EXPLORER_TYPE.OPPORTUNITIES} key={index}
+                                                     match={this.props.match}/>
                         ))
                       }
                     </Menu>
@@ -260,22 +127,16 @@ class CardExplorerMenuNonMobileWidget extends React.Component<ICardExplorerMenuN
                 <Header as="h4" dividing={true}>MY INTERESTS</Header>
                 {
                   menuAddedList.map((listItem, index) => (
-                    <Menu.Item as={NavLink} key={index} exact={true}
-                               to={`${baseRoute}/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.INTERESTS}/${this.slugName(listItem.item)}`}>
-                      <i className={this.getItemStatus(listItem.item)} style={iconStyle}/>
-                      {this.itemName(listItem)}
-                    </Menu.Item>
+                    <ExplorerMenuNonMobileItem listItem={listItem} type={EXPLORER_TYPE.INTERESTS} key={index}
+                                               match={this.props.match}/>
                   ))
                 }
 
                 <Header as="h4" dividing={true}>CAREER GOAL INTERESTS</Header>
                 {
                   menuCareerList.map((listItem, index) => (
-                    <Menu.Item as={NavLink} key={index} exact={true}
-                               to={`${baseRoute}/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.INTERESTS}/${this.slugName(listItem.item)}`}>
-                      <i className={this.getItemStatus(listItem.item)} style={iconStyle}/>
-                      {this.itemName(listItem)}
-                    </Menu.Item>
+                    <ExplorerMenuNonMobileItem listItem={listItem} type={EXPLORER_TYPE.INTERESTS} key={index}
+                                               match={this.props.match}/>
                   ))
                 }
               </Menu>
@@ -289,11 +150,8 @@ class CardExplorerMenuNonMobileWidget extends React.Component<ICardExplorerMenuN
                 <Header as="h4" dividing={true}>MY CAREER GOALS</Header>
                 {
                   menuAddedList.map((listItem, index) => (
-                    <Menu.Item as={NavLink} key={index} exact={true}
-                               to={`${baseRoute}/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.CAREERGOALS}/${this.slugName(listItem.item)}`}>
-                      <i className={this.getItemStatus(listItem.item)} style={iconStyle}/>
-                      {this.itemName(listItem)}
-                    </Menu.Item>
+                    <ExplorerMenuNonMobileItem listItem={listItem} type={EXPLORER_TYPE.CAREERGOALS} key={index}
+                                               match={this.props.match}/>
                   ))
                 }
               </Menu>
