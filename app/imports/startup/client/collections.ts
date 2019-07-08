86536c6eadc8b995aace7f69e8c2787b19632674
Cam Moore <cammymoore07@gmail.com>
2019-07-08 08:10:06 -1000

    Added client side OpportunityScoreboard collection

--- app/imports/startup/client/collections.ts
+++ app/imports/startup/client/collections.ts
@@ -1,3 +1,4 @@
 import { Mongo } from 'meteor/mongo';
 
 export const CourseScoreboard = new Mongo.Collection('CourseScoreboard');
+export const OpportunityScoreboard = new Mongo.Collection('OpportunityScoreboard');
