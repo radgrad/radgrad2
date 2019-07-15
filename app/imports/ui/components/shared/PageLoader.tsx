53405bbcf35a4f00913270eddc8511831bb278c2
Mercedez Castro <mercedez@hawaii.edu>
2019-07-15 12:04:03 -1000

    Loader Update

--- app/imports/ui/components/shared/PageLoader.tsx
+++ app/imports/ui/components/shared/PageLoader.tsx
@@ -3,22 +3,24 @@ import ContentLoader from 'react-content-loader';
 
 const PageLoader = () => (
   <ContentLoader
-    height={360}
+    height={460}
     width={790}
     speed={0}
     primaryColor="#f3f3f3"
     secondaryColor="#737375"
   >
-    <rect x="584" y="13" rx="0" ry="0" width="1" height="38" />
-    <rect x="598" y="25" rx="0" ry="0" width="31" height="22" />
+    <rect x="600" y="20" rx="0" ry="0" width="1" height="30" />
+    <rect x="612" y="24" rx="0" ry="0" width="31" height="22" />
     <rect x="152" y="84" rx="0" ry="0" width="0" height="0" />
-    <circle cx="558" cy="35" r="15" />
-    <rect x="-20" y="62" rx="0" ry="0" width="807" height="22" />
-    {/* <rect x="50" y="96" rx="0" ry="0" width="677" height="34" /> */} // Help panel widget
-    <circle cx="527" cy="35" r="15" />
-    <circle cx="497" cy="35" r="15" />
-    <circle cx="462" cy="35" r="15" />
-    <circle cx="27" cy="32" r="15" />
+    <circle cx="572" cy="34" r="15" />
+    <rect x="-19" y="60" rx="0" ry="0" width="807" height="22" />
+    <rect x="50" y="96" rx="0" ry="0" width="677" height="34" />
+    <circle cx="541" cy="34" r="15" />
+    <circle cx="511" cy="34" r="15" />
+    <circle cx="476" cy="34" r="15" />
+    <circle cx="33" cy="28" r="15" />
+    <rect x="52" y="155" rx="0" ry="0" width="94" height="114" />
+    <rect x="163" y="153" rx="0" ry="0" width="560" height="328" />
   </ContentLoader>
 );
 
