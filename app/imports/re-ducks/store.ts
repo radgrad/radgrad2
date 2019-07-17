a5d4de2c77bad0ecc6deb9350b0faf1f67baa846
gbarcelo <gbarcelo@users.noreply.github.com>
2019-07-17 12:36:35 -1000

    :hammer: Refactor Student Home to re-ducks

--- app/imports/re-ducks/store.ts
+++ app/imports/re-ducks/store.ts
@@ -1,8 +1,10 @@
 import { createStore, combineReducers } from 'redux';
 import advisor from './advisor';
+import student from './student';
 
 const rootReducer = combineReducers({
   advisor,
+  student,
 });
 
 const store = createStore(rootReducer);
---
+++ app/imports/re-ducks/student/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import home from './home';
+
+const student = combineReducers({
+  home,
+});
+
+export default student;
