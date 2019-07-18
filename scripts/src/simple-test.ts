468734ec051876d9b5700bd3a510cfb3a3b4df74
Cam Moore <cammymoore07@gmail.com>
2019-07-18 11:34:22 -1000

    Checkpoint

---
+++ scripts/src/simple-test.ts
@@ -0,0 +1,5 @@
+import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2'; // eslint-disable-line
+import SimpleSchema from 'simpl-schema';
+
+const schema = new SimpleSchema(); // eslint-disable-line
+// const bridge = new SimpleSchema2Bridge(schema);
