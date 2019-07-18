7a5d60e1b6c9648cb568bbae6b294de86d94bbca
Cam Moore <cammymoore07@gmail.com>
2019-07-18 07:08:44 -1000

    Update DesiredDegreeCollection.test.ts

--- app/imports/api/degree-plan/DesiredDegreeCollection.test.ts
+++ app/imports/api/degree-plan/DesiredDegreeCollection.test.ts
@@ -21,12 +21,14 @@ if (Meteor.isServer) {
       removeAllEntities();
     });
 
-    it('#define, #isDefined, #update, #removeIt, #dumpOne, #restoreOne', function test() {
+    it('#define, #isDefined, #update, #checkIntegrity, #removeIt, #dumpOne, #restoreOne', function test() {
       const docID = DesiredDegrees.define({ name, shortName, slug, description });
       expect(DesiredDegrees.isDefined(slug)).to.be.true;
       const dumpObject = DesiredDegrees.dumpOne(docID);
       expect(DesiredDegrees.countNonRetired()).to.equal(1);
       DesiredDegrees.update(docID, { retired: true });
+      const errors = DesiredDegrees.checkIntegrity();
+      expect(errors.length).to.equal(0);
       expect(DesiredDegrees.countNonRetired()).to.equal(0);
       DesiredDegrees.removeIt(slug);
       expect(DesiredDegrees.isDefined(slug)).to.be.false;
