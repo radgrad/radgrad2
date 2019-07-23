7f040975b082414b1a3123d24dbc558d5df58488
gbarcelo <gbarcelo@users.noreply.github.com>
2019-07-23 13:22:22 -1000

    :hammer: Changed action types to have the form app/role/feature/ACTION_TYPE

--- app/imports/redux/admin/database/types.ts
+++ app/imports/redux/admin/database/types.ts
@@ -1,4 +1,4 @@
-export const CHECK_INTEGRITY_WORKING = 'CHECK_INTEGRITY_WORKING';
-export const CHECK_INTEGRITY_DONE = 'CHECK_INTEGRITY_DONE';
-export const DUMP_DATABASE_WORKING = 'DUMP_DATABASE_WORKING';
-export const DUMP_DATABASE_DONE = 'DUMP_DATABASE_DONE';
+export const CHECK_INTEGRITY_WORKING = 'radgrad/admin/database/CHECK_INTEGRITY_WORKING';
+export const CHECK_INTEGRITY_DONE = 'radgrad/admin/database/CHECK_INTEGRITY_DONE';
+export const DUMP_DATABASE_WORKING = 'radgrad/admin/database/DUMP_DATABASE_WORKING';
+export const DUMP_DATABASE_DONE = 'radgrad/admin/database/DUMP_DATABASE_DONE';
--- app/imports/redux/student/degree-planner/types.ts
+++ app/imports/redux/student/degree-planner/types.ts
@@ -1,7 +1,7 @@
-export const SELECT_COURSE = 'SELECT_COURSE';
-export const SELECT_COURSE_INSTANCE = 'SELECT_COURSE_INSTANCE';
-export const SELECT_OPPORTUNITY = 'SELECT_OPPORTUNITY';
-export const SELECT_OPPORTUNITY_INSTANCE = 'SELECT_OPPORTUNITY_INSTANCE';
+export const SELECT_COURSE = 'radgrad/student/degree-planner/SELECT_COURSE';
+export const SELECT_COURSE_INSTANCE = 'radgrad/student/degree-planner/SELECT_COURSE_INSTANCE';
+export const SELECT_OPPORTUNITY = 'radgrad/student/degree-planner/SELECT_OPPORTUNITY';
+export const SELECT_OPPORTUNITY_INSTANCE = 'radgrad/student/degree-planner/SELECT_OPPORTUNITY_INSTANCE';
 
-export const SELECT_PLAN = 'PLAN';
-export const SELECT_INSPECTOR = 'INSPECTOR';
+export const SELECT_PLAN = 'radgrad/student/degree-planner/PLAN';
+export const SELECT_INSPECTOR = 'radgrad/student/degree-planner/INSPECTOR';
