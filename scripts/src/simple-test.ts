d2841f9b32f4c4609b43c1a91407bd6f269281c0
Cam Moore <cammymoore07@gmail.com>
2019-07-24 09:57:47 -0700

    Just do the import of uniforms-bridge-simple-schema-2

--- scripts/src/simple-test.ts
+++ scripts/src/simple-test.ts
@@ -1,4 +1,4 @@
-import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2'; // eslint-disable-line
+import 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
 
 const schema = new SimpleSchema(); // eslint-disable-line
