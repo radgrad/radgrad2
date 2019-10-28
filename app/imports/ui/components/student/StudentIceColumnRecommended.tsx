c60ac8766286fdc134292fb94af8d52927a65b24
Cam Moore <cammymoore07@gmail.com>
2019-10-27 16:03:41 -1000

    Checkpoint

--- app/imports/ui/components/student/StudentIceColumnRecommended.tsx
+++ app/imports/ui/components/student/StudentIceColumnRecommended.tsx
@@ -29,70 +29,85 @@ interface IStudentIceColumnRecommendedProps {
   };
 }
 
+const hasNoInterests = (props: IStudentIceColumnRecommendedProps): boolean => {
+  const user = Users.getProfile(getUsername(props.match));
+  return user.interestIDs.length === 0;
+};
+
+const recommendedEvents = (projectedPoints: number, props: IStudentIceColumnRecommendedProps): any[] => {
+  const { type } = props;
+  if (getUserIdFromRoute(props.match)) {
+    let allInstances = [];
+    const recommendedInstances = [];
+    let totalIce = 0;
+    const remainder = 100 - projectedPoints;
+    if (type === 'Competency') {
+      allInstances = this.matchingCourses();
+    } else {
+      allInstances = this.matchingOpportunities();
+    }
+
+    if (type === 'Innovation') {
+      allInstances.forEach((instance) => {
+        if (totalIce < remainder) {
+          if (instance.ice.i > 0) {
+            totalIce += instance.ice.i;
+            recommendedInstances.push(instance);
+          }
+        }
+      });
+    } else if (type === 'Competency') {
+      allInstances.forEach((instance) => {
+        if (totalIce < remainder) {
+          totalIce += 9; // assume A grade
+          recommendedInstances.push(instance);
+        }
+      });
+    } else if (type === 'Experience') {
+      allInstances.forEach((instance) => {
+        if (totalIce < remainder) {
+          if (instance.ice.e > 0) {
+            totalIce += instance.ice.e;
+            recommendedInstances.push(instance);
+          }
+        }
+      });
+    } else {
+      return null;
+    }
+    return recommendedInstances;
+  }
+  return null;
+};
+
+const availableCourses = (): ICourse[] => {
+  const courses = Courses.findNonRetired({});
+  if (courses.length > 0) {
+    const filtered = _.filter(courses, (course) => {
+      if (course.number === 'ICS 499') { // TODO: hardcoded ICS string
+        return true;
+      }
+      const ci = CourseInstances.findNonRetired({
+        studentID: getUserIdFromRoute(this.props.match),
+        courseID: course._id,
+      });
+      return ci.length === 0;
+    });
+    return filtered;
+  }
+  return [];
+};
+
+
 class StudentIceColumnRecommended extends React.Component<IStudentIceColumnRecommendedProps> {
   constructor(props) {
     super(props);
   }
 
-  private getUsername = (): string => getUsername(this.props.match);
-
-  private getUserIdFromRoute = (): string => getUserIdFromRoute(this.props.match);
-
-  private hasNoInterests = (): boolean => {
-    const user = Users.getProfile(this.getUsername());
-    return user.interestIDs.length === 0;
-  }
-
-  private recommendedEvents(projectedPoints: number): any[] {
-    const { type } = this.props;
-    if (this.getUserIdFromRoute()) {
-      let allInstances = [];
-      const recommendedInstances = [];
-      let totalIce = 0;
-      const remainder = 100 - projectedPoints;
-      if (type === 'Competency') {
-        allInstances = this.matchingCourses();
-      } else {
-        allInstances = this.matchingOpportunities();
-      }
-
-      if (type === 'Innovation') {
-        allInstances.forEach((instance) => {
-          if (totalIce < remainder) {
-            if (instance.ice.i > 0) {
-              totalIce += instance.ice.i;
-              recommendedInstances.push(instance);
-            }
-          }
-        });
-      } else if (type === 'Competency') {
-        allInstances.forEach((instance) => {
-          if (totalIce < remainder) {
-            totalIce += 9; // assume A grade
-            recommendedInstances.push(instance);
-          }
-        });
-      } else if (type === 'Experience') {
-        allInstances.forEach((instance) => {
-          if (totalIce < remainder) {
-            if (instance.ice.e > 0) {
-              totalIce += instance.ice.e;
-              recommendedInstances.push(instance);
-            }
-          }
-        });
-      } else {
-        return null;
-      }
-      return recommendedInstances;
-    }
-    return null;
-  }
-
   private matchingOpportunities = (): IOpportunity[] => {
     const allOpportunities = Opportunities.findNonRetired();
     const matching = [];
-    const profile = Users.getProfile(this.getUsername());
+    const profile = Users.getProfile(getUsername(this.props.match));
     const userInterests = [];
     let opportunityInterests = [];
     _.forEach(Users.getInterestIDs(profile.userID), (id) => {
@@ -117,9 +132,9 @@ class StudentIceColumnRecommended extends React.Component<IStudentIceColumnRecom
   }
 
   private matchingCourses = (): ICourse[] => {
-    const allCourses = this.availableCourses();
+    const allCourses = availableCourses();
     const matching = [];
-    const profile = Users.getProfile(this.getUsername());
+    const profile = Users.getProfile(getUsername(this.props.match));
     const userInterests = [];
     let courseInterests = [];
     _.forEach(Users.getInterestIDs(profile.userID), (id) => {
@@ -143,27 +158,8 @@ class StudentIceColumnRecommended extends React.Component<IStudentIceColumnRecom
     return matching;
   }
 
-  private availableCourses = (): ICourse[] => {
-    const courses = Courses.findNonRetired({});
-    if (courses.length > 0) {
-      const filtered = _.filter(courses, (course) => {
-        if (course.number === 'ICS 499') { // TODO: hardcoded ICS string
-          return true;
-        }
-        const ci = CourseInstances.findNonRetired({
-          studentID: this.getUserIdFromRoute(),
-          courseID: course._id,
-        });
-        return ci.length === 0;
-      });
-      return filtered;
-    }
-    return [];
-  };
-
   public render(): React.ReactElement<any> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
     const { type, earnedICEPoints, projectedICEPoints, matchingPoints, getCourseSlug, getOpportunitySlug, icePoints, match } = this.props;
-    const recommendedEvents = this.recommendedEvents(projectedICEPoints);
 
     return (
       <React.Fragment>
@@ -173,13 +169,13 @@ class StudentIceColumnRecommended extends React.Component<IStudentIceColumnRecom
           matchingPoints(100, projectedICEPoints) ?
             <p>You already have at least 100 verified or unverified {type} points.</p>
             :
-            this.hasNoInterests() ?
+            hasNoInterests(this.props) ?
               <p>Consider adding interests to see recommendations here.</p>
               :
               <React.Fragment>
                 <p>Consider the following to acquire 100 {type} points.</p>
                 <List>
-                  {recommendedEvents.map((event) => {
+                  {recommendedEvents(projectedICEPoints, this.props).map((event) => {
                     const courseSlug = getCourseSlug(event);
                     const courseRoute = buildRouteName(match, `/${EXPLORER_TYPE.HOME}/${EXPLORER_TYPE.COURSES}/${courseSlug}`);
                     const opportunitySlug = getOpportunitySlug(event);
