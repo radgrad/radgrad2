f2e47b0c0ccdb5de978df4dd98ce98a0db6882e5
Cam Moore <cammymoore07@gmail.com>
2019-10-30 11:50:40 -1000

    Checkpoint

--- app/imports/ui/components/student/StudentPageMenuWidget.tsx
+++ app/imports/ui/components/student/StudentPageMenuWidget.tsx
@@ -1,36 +1,21 @@
 import * as React from 'react';
-import { withRouter } from 'react-router-dom';
 import FirstMenuContainer from '../../pages/shared/FirstMenu';
 import SecondMenu from '../../pages/shared/SecondMenu';
 
-interface IStudentPageMenuWidgetProps {
-  match?: {
-    isExact: boolean;
-    path: string;
-    url: string;
-    params: {
-      username: string;
-    }
-  };
-}
+const StudentPageMenuWidget = () => {
+  const divStyle = { marginBottom: 30 };
+  const menuItems = [
+    { label: 'Home', route: 'home' },
+    { label: 'Explorer', route: 'explorer' },
+    { label: 'Degree Planner', route: 'degree-planner' },
+    { label: 'Mentor Space', route: 'mentor-space' },
+  ];
+  return (
+    <div style={divStyle}>
+      <FirstMenuContainer/>
+      <SecondMenu menuItems={menuItems} numItems={menuItems.length}/>
+    </div>
+  );
+};
 
-class StudentPageMenuWidget extends React.Component<IStudentPageMenuWidgetProps> {
-  public render() {
-    const divStyle = { marginBottom: 30 };
-    const menuItems = [
-      { label: 'Home', route: 'home' },
-      { label: 'Explorer', route: 'explorer' },
-      { label: 'Degree Planner', route: 'degree-planner' },
-      { label: 'Mentor Space', route: 'mentor-space' },
-    ];
-    return (
-      <div style={divStyle}>
-        <FirstMenuContainer/>
-        <SecondMenu menuItems={menuItems} numItems={menuItems.length}/>
-      </div>
-    );
-  }
-}
-
-/** Enable ReactRouter for this component. https://reacttraining.com/react-router/web/api/withRouter */
-export default withRouter(StudentPageMenuWidget);
+export default StudentPageMenuWidget;
