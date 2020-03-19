71f2538fb1b49c2eeb19ca2bfeb202c8be7773dc
Cam Moore <cammymoore07@gmail.com>
2020-03-19 11:58:51 -1000

    MentorAnswers

--- app/imports/api/mentor/MentorAnswerCollection.test.ts
+++ app/imports/api/mentor/MentorAnswerCollection.test.ts
@@ -1,11 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import { expect } from 'chai';
-import {} from 'mocha';
+import 'mocha';
+import fc from 'fast-check';
+import faker from 'faker';
 import { removeAllEntities } from '../base/BaseUtilities';
 import { MentorAnswers } from './MentorAnswerCollection';
 import { MentorQuestions } from './MentorQuestionCollection';
 import { makeSampleUser } from '../user/SampleUsers';
 import { ROLE } from '../role/Role';
+import { makeSampleMentorQuestion } from './SampleMentorQuestionAndAnswer';
 
 /* eslint prefer-arrow-callback: "off",  @typescript-eslint/no-unused-expressions: "off" */
 /* eslint-env mocha */
@@ -14,7 +17,7 @@ if (Meteor.isServer) {
   describe('MentorAnswerCollection', function testSuite() {
     // Define course data.
     const questionSlug = 'hiring-expectations';
-    const text = 'Test answer.';
+    let text = 'Test answer.';
 
     before(function setup() {
       removeAllEntities();
@@ -24,19 +27,62 @@ if (Meteor.isServer) {
       removeAllEntities();
     });
 
-    it('#define, #isDefined, #removeIt', function test() {
-      // Define mentor and the question.
-      const mentor = makeSampleUser(ROLE.MENTOR);
-      const student = makeSampleUser(ROLE.STUDENT);
-      MentorQuestions.define({ question: 'Sample Question', slug: questionSlug, student });
-      // Now define an answer, passing the defined question and the defined mentor.
-      const instanceID = MentorAnswers.define({ question: questionSlug, mentor, text });
-      expect(MentorAnswers.isDefined(instanceID)).to.be.true;
-      MentorAnswers.removeIt(instanceID);
-      expect(MentorAnswers.isDefined(instanceID)).to.be.false;
+    it('Can define and removeIt', function test1(done) {
+      this.timeout(20000);
+      fc.assert(
+        fc.property(fc.lorem(10), fc.boolean(), (fcText, fcRetired) => {
+          const mentor = makeSampleUser(ROLE.MENTOR);
+          const question = makeSampleMentorQuestion();
+          const docID = MentorAnswers.define({ question, mentor, text: fcText, retired: fcRetired });
+          expect(MentorAnswers.isDefined(docID)).to.be.true;
+          const doc = MentorAnswers.findDoc(docID);
+          expect(doc.questionID).to.equal(question);
+          expect(doc.mentorID).to.equal(mentor);
+          expect(doc.text).to.equal(fcText);
+          expect(doc.retired).to.equal(fcRetired);
+          MentorAnswers.removeIt(docID);
+          expect(MentorAnswers.isDefined(docID)).to.be.false;
+        }),
+      );
+      done();
     });
 
-    it('#removeQuestion, #removeUser', function test() {
+    it('Can update', function test2(done) {
+      this.timeout(5000);
+      text = faker.lorem.words();
+      const mentor = makeSampleUser(ROLE.MENTOR);
+      const question = makeSampleMentorQuestion();
+      const docID = MentorAnswers.define({ question, mentor, text });
+      let doc = MentorAnswers.findDoc(docID);
+      fc.assert(
+        fc.property(fc.lorem(24), fc.boolean(), (fcText, fcRetired) => {
+          MentorAnswers.update(docID, { text: fcText, retired: fcRetired });
+          doc = MentorAnswers.findDoc(docID);
+          expect(doc.text).to.equal(fcText);
+          expect(doc.retired).to.equal(fcRetired);
+        }),
+      );
+      done();
+    });
+
+    it('Can dumpOne, removeIt, and restoreOne', function test3() {
+      let doc = MentorAnswers.findOne({});
+      let docID = doc._id;
+      const dumpObject = MentorAnswers.dumpOne(docID);
+      MentorAnswers.removeIt(docID);
+      expect(MentorAnswers.isDefined(docID)).to.be.false;
+      docID = MentorAnswers.restoreOne(dumpObject);
+      doc = MentorAnswers.findDoc(docID);
+      expect(doc.retired).to.equal(dumpObject.retired);
+      expect(doc.text).to.equal(dumpObject.text);
+    });
+
+    it('Can check integrity no errors', function test4() {
+      const problems = MentorAnswers.checkIntegrity();
+      expect(problems.length).to.equal(0);
+    });
+
+    it('Can removeQuestion and removeUser', function test() {
       // Define mentor and the question.
       const mentor1 = makeSampleUser(ROLE.MENTOR);
       const mentor2 = makeSampleUser(ROLE.MENTOR);
