3cd9ccba797aa84945bc8f833dff1d34a27a0237
Cam Moore <cammymoore07@gmail.com>
2020-05-20 12:57:35 -1000

    AdvisorProfiles

--- app/imports/api/user/AdvisorProfileCollection.test.ts
+++ app/imports/api/user/AdvisorProfileCollection.test.ts
@@ -1,8 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import { expect } from 'chai';
-import {} from 'mocha';
+import fc from 'fast-check';
+import faker from 'faker';
 import { removeAllEntities } from '../base/BaseUtilities';
 import { AdvisorProfiles } from './AdvisorProfileCollection';
+import { makeSampleInterestArray, makeSampleInterestSlugArray } from '../interest/SampleInterests';
+import { makeSampleCareerGoalSlugArray } from '../career/SampleCareerGoals';
+import { FavoriteInterests } from '../favorite/FavoriteInterestCollection';
+import { FavoriteCareerGoals } from '../favorite/FavoriteCareerGoalCollection';
 
 /* eslint prefer-arrow-callback: "off",  @typescript-eslint/no-unused-expressions: "off" */
 /* eslint-env mocha */
@@ -17,23 +22,80 @@ if (Meteor.isServer) {
       removeAllEntities();
     });
 
-    it('#define, #isDefined, #update, #removeIt, #dumpOne, #restoreOne', function test() {
-      const username = 'glau@hawaii.edu';
-      const firstName = 'Gerald';
-      const lastName = 'Lau';
-      const picture = 'glau.jpg';
-      const website = 'http://glau.github.io';
-      const interests = [];
-      const careerGoals = [];
+    it('Can define and removeIt', function test1(done) { // Test the define and removeIt methods
+      this.timeout(15000);
+      fc.assert(
+        fc.property(fc.lorem(1), fc.lorem(1), fc.lorem(1), fc.lorem(1), fc.lorem(1), (username, firstName, lastName, picture, website) => {
+          const interests = makeSampleInterestArray();
+          const careerGoals = makeSampleCareerGoalSlugArray();
+          const docID = AdvisorProfiles.define({ username, firstName, lastName, picture, website, interests, careerGoals });
+          expect(AdvisorProfiles.isDefined(docID)).to.be.true;
+          expect(FavoriteInterests.count()).to.equal(interests.length);
+          expect(FavoriteCareerGoals.count()).to.equal(careerGoals.length);
+          AdvisorProfiles.removeIt(docID);
+          expect(AdvisorProfiles.isDefined(docID)).to.be.false;
+          expect(FavoriteInterests.count()).to.equal(0);
+          expect(FavoriteCareerGoals.count()).to.equal(0);
+        }),
+      );
+      done();
+    });
+
+    it('Cannot define duplicates', function test2() { // Test if duplicate documents can be defined
+      const username = faker.internet.userName();
+      const firstName = faker.name.firstName();
+      const lastName = faker.name.lastName();
+      const picture = faker.image.imageUrl();
+      const website = faker.internet.url();
+      const interests = makeSampleInterestSlugArray();
+      const careerGoals = makeSampleCareerGoalSlugArray();
       const docID = AdvisorProfiles.define({ username, firstName, lastName, picture, website, interests, careerGoals });
       expect(AdvisorProfiles.isDefined(docID)).to.be.true;
+      expect(() => AdvisorProfiles.define({ username, firstName, lastName, picture, website, interests, careerGoals })).to.throw(Error);
+    });
+
+    it('Can update', function test3(done) { // Test updating documents
+      this.timeout(15000);
+      let doc = AdvisorProfiles.findOne({});
+      const docID = doc._id;
+      fc.assert(
+        fc.property(fc.lorem(1), fc.lorem(1), fc.lorem(1), fc.boolean(), (firstName, lastName, picture, retired) => {
+          const website = faker.internet.url();
+          const interests = makeSampleInterestArray();
+          const careerGoals = makeSampleCareerGoalSlugArray();
+          AdvisorProfiles.update(docID, { firstName, lastName, picture, retired, website, interests, careerGoals });
+          doc = AdvisorProfiles.findDoc(docID);
+          expect(doc.firstName).to.equal(firstName);
+          expect(doc.lastName).to.equal(lastName);
+          expect(doc.picture).to.equal(picture);
+          expect(doc.website).to.equal(website);
+          expect(FavoriteInterests.count()).to.equal(interests.length);
+          expect(FavoriteCareerGoals.count()).to.equal(careerGoals.length);
+        }),
+      );
+      done();
+    });
+
+    it('Can dumpOne, removeIt, and restoreOne', function test4() { // Tests dumpOne and restoreOne
+      const origDoc = AdvisorProfiles.findOne({});
+      let docID = origDoc._id;
       const dumpObject = AdvisorProfiles.dumpOne(docID);
       AdvisorProfiles.removeIt(docID);
       expect(AdvisorProfiles.isDefined(docID)).to.be.false;
-      AdvisorProfiles.restoreOne(dumpObject);
-      const id = AdvisorProfiles.getID(username);
-      expect(AdvisorProfiles.isDefined(id)).to.be.true;
-      AdvisorProfiles.removeIt(id);
+      expect(FavoriteInterests.count()).to.equal(0);
+      expect(FavoriteCareerGoals.count()).to.equal(0);
+      docID = AdvisorProfiles.restoreOne(dumpObject);
+      expect(AdvisorProfiles.isDefined(docID)).to.be.true;
+      const doc = AdvisorProfiles.findDoc(docID);
+      expect(doc.firstName).to.equal(origDoc.firstName);
+      expect(doc.lastName).to.equal(origDoc.lastName);
+      expect(doc.picture).to.equal(origDoc.picture);
+      expect(doc.website).to.equal(origDoc.website);
+    });
+
+    it('Can checkIntegrity no errors', function test5() { // Tests checkIntegrity
+      const problems = AdvisorProfiles.checkIntegrity();
+      expect(problems).to.have.lengthOf(0);
     });
   });
 }
