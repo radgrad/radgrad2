1b1435a2b70cf72ab836856c9ae672be6b4c2a7c
Cam Moore <cammymoore07@gmail.com>
2020-05-20 09:13:24 -1000

    Checkpoint

--- app/imports/api/career/SampleCareerGoals.ts
+++ app/imports/api/career/SampleCareerGoals.ts
@@ -1,10 +1,12 @@
 import faker from 'faker';
+import moment from 'moment';
 import { CareerGoals } from './CareerGoalCollection';
 import { makeSampleInterest } from '../interest/SampleInterests';
+import slugify from '../slug/SlugCollection';
 
 export function makeSampleCareerGoal() {
   const name = faker.lorem.word();
-  const slug = `career-goal-${name}`;
+  const slug = slugify(`career-goal-${name}-${moment().format('YYYY-MM-DD-HH-mm-ss-SSSSS')}`);
   const description = faker.lorem.paragraph();
   const interests = [makeSampleInterest()];
   return CareerGoals.define({ name, slug, description, interests });
