c0a90e2913c03720d6773623255b03cae45db041
Cam Moore <cammymoore07@gmail.com>
2020-05-20 15:25:29 -1000

    Update MentorProfileCollection.test.ts
    Checkpoint

--- app/imports/api/user/MentorProfileCollection.test.ts
+++ app/imports/api/user/MentorProfileCollection.test.ts
@@ -1,8 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import { expect } from 'chai';
-import {} from 'mocha';
+import fc from 'fast-check';
+import faker from 'faker';
 import { removeAllEntities } from '../base/BaseUtilities';
 import { MentorProfiles } from './MentorProfileCollection';
+import { makeSampleInterestArray } from '../interest/SampleInterests';
+import { makeSampleCareerGoalSlugArray } from '../career/SampleCareerGoals';
+import { FavoriteInterests } from '../favorite/FavoriteInterestCollection';
+import { FavoriteCareerGoals } from '../favorite/FavoriteCareerGoalCollection';
 
 /* eslint prefer-arrow-callback: "off",  @typescript-eslint/no-unused-expressions: "off" */
 /* eslint-env mocha */
@@ -16,6 +21,30 @@ if (Meteor.isServer) {
     after(function tearDown() {
       removeAllEntities();
     });
+
+    it('Can define and removeIt', function test1(done) { // Test the define and removeIt methods
+      this.timeout(25000);
+      fc.assert(
+        fc.property(fc.lorem(1), fc.lorem(1), fc.lorem(1), fc.lorem(1), fc.lorem(1), fc.lorem(3), fc.lorem(2), fc.lorem(2), fc.lorem(1), fc.lorem(10),
+          (username, firstName, lastName, picture, website, company, career, location, linkedin, motivation) => {
+          const interests = makeSampleInterestArray();
+          const careerGoals = makeSampleCareerGoalSlugArray();
+          const docID = MentorProfiles.define({
+            username, firstName, lastName, picture, website, interests,
+            careerGoals, company, career, location, linkedin, motivation,
+          });
+            expect(MentorProfiles.isDefined(docID)).to.be.true;
+            expect(FavoriteInterests.count()).to.equal(interests.length);
+            expect(FavoriteCareerGoals.count()).to.equal(careerGoals.length);
+            MentorProfiles.removeIt(docID);
+            expect(MentorProfiles.isDefined(docID)).to.be.false;
+            expect(FavoriteInterests.count()).to.equal(0);
+            expect(FavoriteCareerGoals.count()).to.equal(0);
+          }),
+      );
+      done();
+    });
+
 
     it('#define, #isDefined, #update, #removeIt, #dumpOne, #restoreOne', function test() {
       const username = 'rbrewer@tableau.com';
