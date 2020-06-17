74e6ec9baba81647061ea496d48f67f767aee9bb
Gian Calica <calicagian7@gmail.com>
2020-06-16 18:57:47 -1000

    :pencil2: Finished moving withHistoryListen to PageTrackerHOC.tsx and refactoring it such that it's a pure function component rather than a class component :art: Login Button text is now a <Link> rather than <NavLink>

--- app/imports/ui/layouts/App.tsx
+++ app/imports/ui/layouts/App.tsx
@@ -1,9 +1,8 @@
 import { Roles } from 'meteor/alanning:roles';
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import { HashRouter as Router, Redirect, Route, Switch, withRouter } from 'react-router-dom';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import '/public/semantic.min.css';
-import { connect } from 'react-redux';
 import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signout from '../pages/Signout';
@@ -11,29 +10,12 @@ import { ROLE } from '../../api/role/Role';
 import { routes } from '../../startup/client/routes-config';
 import withGlobalSubscription from './shared/GlobalSubscriptionsHOC';
 import withInstanceSubscriptions from './shared/InstanceSubscriptionsHOC';
-import { userInteractionDefineMethod } from '../../api/analytic/UserInteractionCollection.methods';
 import {
-  getAllUrlParamsByLocationObject,
   getUsername,
-  ILocationProps,
 } from '../components/shared/RouterHelperFunctions';
 import { Users } from '../../api/user/UserCollection';
-import { UserInteractionsTypes } from '../../api/analytic/UserInteractionsTypes';
-import { EXPLORER_TYPE } from '../../startup/client/route-constants';
-import { IPageInterest, IPageInterestDefine, ISlug } from '../../typings/radgrad';
-import {
-  IPageInterestsCategoryTypes,
-  PageInterestsCategoryTypes,
-} from '../../api/page-tracking/PageInterestsCategoryTypes';
-import { pageInterestDefineMethod } from '../../api/page-tracking/PageInterestCollection.methods';
-import { Slugs } from '../../api/slug/SlugCollection';
-import { RootState } from '../../redux/types';
 import NotAuthorized from '../pages/NotAuthorized';
-import { PageInterests } from '../../api/page-tracking/PageInterestCollection';
-import { CareerGoals } from '../../api/career/CareerGoalCollection';
-import { Courses } from '../../api/course/CourseCollection';
-import { Interests } from '../../api/interest/InterestCollection';
-import { Opportunities } from '../../api/opportunity/OpportunityCollection';
+import withPageTracker from './PageTrackerHOC';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.tsx. */
 const App = () => (
@@ -125,7 +107,6 @@ const AdvisorProtectedRoute = ({ component: Component, ...rest }) => {
   );
 };
 
-
 const FacultyProtectedRoute = ({ component: Component, ...rest }) => {
   const WrappedComponent = withInstanceSubscriptions(withGlobalSubscription(Component));
   return (
@@ -160,161 +141,13 @@ const MentorProtectedRoute = ({ component: Component, ...rest }) => {
   );
 };
 
-// TODO: Turn this class into a separate pure function and move this function to another file
-function withHistoryListen(WrappedComponent) {
-  interface IHistoryListenProps {
-    history: {
-      listen: (...args) => any;
-    };
-    match: {
-      isExact: boolean;
-      path: string;
-      url: string;
-      params: {
-        username: string;
-      }
-    };
-    router: {
-      location: ILocationProps;
-      action: string;
-    };
-  }
-
-  const mapStateToProps = (state: RootState): object => ({
-    router: state.router,
-  });
-
-  const getDescriptionText = (type, slugName) => {
-    const id = Slugs.findOne({ name: slugName })._id;
-    switch (type) {
-      case 'CareerGoal':
-        return CareerGoals.findOne({ slugID: id }).description;
-      case 'Course':
-        return Courses.findOne({ slugID: id }).description;
-      case 'Interest':
-        return Interests.findOne({ slugID: id }).description;
-      case 'Opportunity':
-        return Opportunities.findOne({ slugID: id }).description;
-      default:
-        console.error(`Bad entityName: ${type}`);
-        break;
-    }
-    return undefined;
-  };
-
-  // Calculates how long to wait (since the time the student has opened the page) before they're considered "interested" in that item
-  const calculateEngagedInterestTime = (slugName: string): number => {
-    const slug: ISlug = Slugs.findOne({ name: slugName });
-    const type = slug.entityName;
-    const descriptionText = getDescriptionText(type, slugName);
-
-    const descriptionTextStrings: string[] = descriptionText.split(' ');
-    const removedHttpLinks: string[] = descriptionTextStrings.filter((str) => !str.includes('http')); // Remove the Markdown http links
-    const validWordLength: number = 3; // Number of characters for a string to be considered a "word"
-    const validDescriptionTextStrings: string[] = removedHttpLinks.filter((str) => str.length >= validWordLength); // Remove strings that aren't "valid words"
-    const isLongDescriptionText: boolean = validDescriptionTextStrings.length > 50; // If there are more than 50 words, it's considered a long description
-
-    const wpm = isLongDescriptionText ? 275 : 250; // Words per minute. We expect that the longer a description is, the faster a english-literate college student reads
-    // We expect that in order for a student to have been considered "interested" in the item they're reading
-    // that they have read around half of the words in the description
-    const expectedNumberOfWords = validDescriptionTextStrings.length / 2;
-    const estimatedReadingTime = (expectedNumberOfWords / wpm) * 60 * 1000; // Based on the WPM, the estimated time it would take to read half of the description (in milliseconds)
-    // As reading experience and time varies between students (in the sense that they might not read everything word by word),
-    // we only expect that they have spent a minimum of half of the estimated reading time for reading half of the description
-    // to be considered "interested" in that item
-    const expectedReadingTime = estimatedReadingTime / 2;
-
-    // Students may not necessarily start reading the description as soon as they enter the page.
-    // If it is a longer description, they might spend some time skimming the page first before reading. (in milliseconds)
-    const initiationTime = isLongDescriptionText ? 1500 : 1000;
-
-    return expectedReadingTime + initiationTime;
-  };
-
-  class HistoryListen extends React.Component<IHistoryListenProps> {
-    private timeoutHandle;
-
-    componentDidUpdate(prevProps: Readonly<IHistoryListenProps>, prevState: Readonly<{}>, snapshot?: any): void {
-      const { match, router } = this.props;
-      if (prevProps.router.location !== router.location) {
-        // Defining User Interactions
-        const parameters = getAllUrlParamsByLocationObject(match, router.location);
-        const typeData = parameters.join('/');
-        const username = Meteor.user().username;
-        const type = UserInteractionsTypes.PAGEVIEW;
-        const interactionData = { username, type, typeData };
-        userInteractionDefineMethod.call(interactionData, (userInteractionError) => {
-          if (userInteractionError) {
-            console.error('Error creating UserInteraction.', userInteractionError);
-          }
-        });
-
-        const isValidParameter = (p) => p === EXPLORER_TYPE.CAREERGOALS || p === EXPLORER_TYPE.COURSES || p === EXPLORER_TYPE.INTERESTS || p === EXPLORER_TYPE.OPPORTUNITIES;
-        const urlCategory = parameters[1];
-        const urlSlug = parameters[2];
-        // Defining Page Interests
-        if (parameters[0] === EXPLORER_TYPE.HOME && urlSlug && isValidParameter(urlCategory)) {
-          // Don't create a PageInterest in case we ever change how the URL of Explorer Pages are structured
-          // since we're directly accessing parameters for data
-          if (parameters.length > 3) {
-            console.error('Invalid length of parameters: %o', parameters);
-            return;
-          }
-          // OR the user is trying to visit a specific page that no longer exists.
-          if (!Slugs.isDefined(parameters[2])) {
-            return;
-          }
-          let category: IPageInterestsCategoryTypes;
-          switch (urlCategory) {
-            case EXPLORER_TYPE.CAREERGOALS:
-              category = PageInterestsCategoryTypes.CAREERGOAL;
-              break;
-            case EXPLORER_TYPE.COURSES:
-              category = PageInterestsCategoryTypes.COURSE;
-              break;
-            case EXPLORER_TYPE.INTERESTS:
-              category = PageInterestsCategoryTypes.INTEREST;
-              break;
-            case EXPLORER_TYPE.OPPORTUNITIES:
-              category = PageInterestsCategoryTypes.OPPORTUNITY;
-              break;
-          }
-          const pageInterest: IPageInterest = PageInterests.findOne({ name: urlSlug });
-          // Only define a PageInterest if there hasn't been one defined for the past 24 hours.
-          if (!pageInterest) {
-            const engagedInterestTime = calculateEngagedInterestTime(urlSlug);
-            const pageInterestData: IPageInterestDefine = { username, category, name: urlSlug };
-            this.timeoutHandle = Meteor.setTimeout(() => {
-              pageInterestDefineMethod.call(pageInterestData, (pageInterestError) => {
-                if (pageInterestError) {
-                  console.error('Error creating PageInterest.', pageInterestError);
-                }
-              });
-            }, engagedInterestTime);
-          }
-        }
-      }
-    }
-
-    componentWillUnmount() {
-      Meteor.clearTimeout(this.timeoutHandle);
-    }
-
-    public render(): React.ReactElement<any> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-      return <WrappedComponent {...this.props} />;
-    }
-  }
-
-  return withRouter(connect(mapStateToProps)(HistoryListen));
-}
-
 const StudentProtectedRoute = ({ component: Component, ...rest }) => {
   const ComponentWithSubscriptions = withInstanceSubscriptions(withGlobalSubscription(Component));
   const isStudent = Roles.userIsInRole(Meteor.userId(), ROLE.STUDENT);
   // Because ROLE.ADMIN and ROLE.ADVISOR are allowed to go to StudentProtectedRoutes, they can trigger the
   // userInteractionDefineMethod.call() inside of withHistoryListen. Since we only want to track the pageViews of
   // STUDENTS, we should only use withHistoryListen if LOGGED IN user is a student.
-  const WrappedComponent = isStudent ? withHistoryListen(ComponentWithSubscriptions) : ComponentWithSubscriptions;
+  const WrappedComponent = isStudent ? withPageTracker(ComponentWithSubscriptions) : ComponentWithSubscriptions;
   return (
     <Route
       {...rest}
--- app/imports/ui/layouts/PageTrackerHOC.tsx
+++ app/imports/ui/layouts/PageTrackerHOC.tsx
@@ -1,45 +1,211 @@
-// import React, { useEffect } from 'react';
-// import { Meteor } from 'meteor/meteor';
-// import { withRouter } from 'react-router-dom';
-// import { connect } from 'react-redux';
-// import {
-//   getAllUrlParamsByLocationObject,
-//   ILocationProps,
-//   IMatchProps,
-// } from '../components/shared/RouterHelperFunctions';
-// import { RootState } from '../../redux/types';
-// import { Slugs } from '../../api/slug/SlugCollection';
-// import { CareerGoals } from '../../api/career/CareerGoalCollection';
-// import { Courses } from '../../api/course/CourseCollection';
-// import { Interests } from '../../api/interest/InterestCollection';
-// import { Opportunities } from '../../api/opportunity/OpportunityCollection';
-// import { IPageInterest, IPageInterestDefine, ISlug } from '../../typings/radgrad';
-// import { UserInteractionsTypes } from '../../api/analytic/UserInteractionsTypes';
-// import { userInteractionDefineMethod } from '../../api/analytic/UserInteractionCollection.methods';
-// import { EXPLORER_TYPE } from '../../startup/client/route-constants';
-// import {
-//   IPageInterestsCategoryTypes,
-//   PageInterestsCategoryTypes,
-// } from '../../api/page-tracking/PageInterestsCategoryTypes';
-// import { PageInterests } from '../../api/page-tracking/PageInterestCollection';
-// import { pageInterestDefineMethod } from '../../api/page-tracking/PageInterestCollection.methods';
+import React, { useEffect, useRef } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {
+  getAllUrlParamsByLocationObject,
+  ILocationProps,
+  IMatchProps,
+} from '../components/shared/RouterHelperFunctions';
+import { RootState } from '../../redux/types';
+import { Slugs } from '../../api/slug/SlugCollection';
+import { CareerGoals } from '../../api/career/CareerGoalCollection';
+import { Courses } from '../../api/course/CourseCollection';
+import { Interests } from '../../api/interest/InterestCollection';
+import { Opportunities } from '../../api/opportunity/OpportunityCollection';
+import { IPageInterest, IPageInterestDefine, ISlug } from '../../typings/radgrad';
+import { UserInteractionsTypes } from '../../api/analytic/UserInteractionsTypes';
+import { userInteractionDefineMethod } from '../../api/analytic/UserInteractionCollection.methods';
+import { EXPLORER_TYPE } from '../../startup/client/route-constants';
+import {
+  IPageInterestsCategoryTypes,
+  PageInterestsCategoryTypes,
+} from '../../api/page-tracking/PageInterestsCategoryTypes';
+import { PageInterests } from '../../api/page-tracking/PageInterestCollection';
+import { pageInterestDefineMethod } from '../../api/page-tracking/PageInterestCollection.methods';
+
+interface IPageTrackerProps {
+  history: {
+    listen: (...args) => any;
+  };
+  match: IMatchProps
+  router: {
+    location: ILocationProps;
+    action: string;
+  };
+}
+
+const mapStateToProps = (state: RootState): object => ({
+  router: state.router,
+});
+
+/* Page Interest Views Tracking Helper Functions */
+// Checks if the URL parameter is one of the explorer types that we're tracking
+// Valid explorer types that we track are Career Goals, Courses, Interests, and Opportunities
+const isValidParameter = (parameter) => (parameter === EXPLORER_TYPE.CAREERGOALS || parameter === EXPLORER_TYPE.COURSES || parameter === EXPLORER_TYPE.INTERESTS || parameter === EXPLORER_TYPE.OPPORTUNITIES);
+
+const getDescriptionText = (type, slugName) => {
+  const id = Slugs.findOne({ name: slugName })._id;
+  switch (type) { // entityNames are the types
+    case CareerGoals.getType():
+      return CareerGoals.findOne({ slugID: id }).description;
+    case Courses.getType():
+      return Courses.findOne({ slugID: id }).description;
+    case Interests.getType():
+      return Interests.findOne({ slugID: id }).description;
+    case Opportunities.getType():
+      return Opportunities.findOne({ slugID: id }).description;
+    default:
+      console.error(`Bad entityName: ${type}`);
+      break;
+  }
+  return undefined;
+};
+
+// Calculates how long to wait (since the time the student has opened the page) before they're considered "interested" in that item
+const calculateEngagedInterestTime = (slugName: string): number => {
+  const slug: ISlug = Slugs.findOne({ name: slugName });
+  const type = slug.entityName;
+  const descriptionText = getDescriptionText(type, slugName);
+
+  const descriptionTextStrings: string[] = descriptionText.split(' ');
+  const removedHttpLinks: string[] = descriptionTextStrings.filter((str) => !str.includes('http')); // Remove the Markdown http links
+  const validWordLength: number = 3; // Number of characters for a string to be considered a "word"
+  const validDescriptionTextStrings: string[] = removedHttpLinks.filter((str) => str.length >= validWordLength); // Remove strings that aren't "valid words"
+  const isLongDescriptionText: boolean = validDescriptionTextStrings.length > 50; // If there are more than 50 words, it's considered a long description
+
+  const wpm = isLongDescriptionText ? 275 : 250; // Words per minute. We expect that the longer a description is, the faster a english-literate college student reads
+  // We expect that in order for a student to have been considered "interested" in the item they're reading
+  // that they have read around half of the words in the description
+  const expectedNumberOfWords = validDescriptionTextStrings.length / 2;
+  const estimatedReadingTime = (expectedNumberOfWords / wpm) * 60 * 1000; // Based on the WPM, the estimated time it would take to read half of the description (in milliseconds)
+  // As reading experience and time varies between students (in the sense that they might not read everything word by word),
+  // we only expect that they have spent a minimum of half of the estimated reading time for reading half of the description
+  // to be considered "interested" in that item
+  const expectedReadingTime = estimatedReadingTime / 2;
+
+  // Students may not necessarily start reading the description as soon as they enter the page.
+  // If it is a longer description, they might spend some time skimming the page first before reading. (in milliseconds)
+  const initiationTime = isLongDescriptionText ? 1500 : 500;
+  return expectedReadingTime + initiationTime;
+};
+
+/* ################################### */
+function usePreviousProps(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  return ref.current;
+}
+
+function withPageTracker(WrappedComponent) {
+  const PageTracker = (props: IPageTrackerProps) => {
+    const prevProps = usePreviousProps(props);
+    const { match, router } = props;
+    useEffect(() => {
+      // @ts-ignore
+      if (prevProps && prevProps.router.location !== router.location) {
+        let timeoutHandle; // Page Interest Views Tracking
+
+        /* ########## User Interactions ########## */
+        const parameters = getAllUrlParamsByLocationObject(match, router.location);
+        const typeData = parameters.join('/');
+        const username = Meteor.user().username;
+        const type = UserInteractionsTypes.PAGEVIEW;
+        const interactionData = { username, type, typeData };
+        userInteractionDefineMethod.call(interactionData, (userInteractionError) => {
+          if (userInteractionError) {
+            console.error('Error creating UserInteraction.', userInteractionError);
+          }
+        });
+
+        /* ########## Page Interest Views ########## */
+        const urlCategory = parameters[1];
+        const urlSlug = parameters[2];
+        // Defining Page Interests
+        if (parameters[0] === EXPLORER_TYPE.HOME && urlSlug && isValidParameter(urlCategory)) {
+          // Don't create a PageInterest in case we ever change how the URL of Explorer Pages are structured
+          // since we're directly accessing parameters for data
+          if (parameters.length > 3) {
+            console.error('Invalid length of parameters: %o', parameters);
+            return;
+          }
+          // OR the user is trying to visit a specific page that no longer exists.
+          if (!Slugs.isDefined(parameters[2])) {
+            return;
+          }
+          let category: IPageInterestsCategoryTypes;
+          switch (urlCategory) {
+            case EXPLORER_TYPE.CAREERGOALS:
+              category = PageInterestsCategoryTypes.CAREERGOAL;
+              break;
+            case EXPLORER_TYPE.COURSES:
+              category = PageInterestsCategoryTypes.COURSE;
+              break;
+            case EXPLORER_TYPE.INTERESTS:
+              category = PageInterestsCategoryTypes.INTEREST;
+              break;
+            case EXPLORER_TYPE.OPPORTUNITIES:
+              category = PageInterestsCategoryTypes.OPPORTUNITY;
+              break;
+          }
+          const pageInterest: IPageInterest = PageInterests.findOne({ name: urlSlug });
+          // Only define a PageInterest if there hasn't been one defined for the past 24 hours.
+          if (!pageInterest) {
+            const engagedInterestTime = calculateEngagedInterestTime(urlSlug);
+            const pageInterestData: IPageInterestDefine = { username, category, name: urlSlug };
+            timeoutHandle = Meteor.setTimeout(() => {
+              pageInterestDefineMethod.call(pageInterestData, (pageInterestError) => {
+                if (pageInterestError) {
+                  console.error('Error creating PageInterest.', pageInterestError);
+                }
+              });
+            }, engagedInterestTime);
+          }
+        }
+        // componentWillUnmount
+        // eslint-disable-next-line consistent-return
+        return () => {
+          Meteor.clearTimeout(timeoutHandle);
+        };
+      }
+    }, [match, prevProps, router.location]);
+
+    return (
+      <WrappedComponent {...props} />
+    );
+  };
+
+  return withRouter(connect(mapStateToProps)(PageTracker));
+}
+
+export default withPageTracker;
+
+// Same exact functionality but using Class instead of pure function; leaving this here just in case
+// function withHistoryListen(WrappedComponent) {
+//   interface IHistoryListenProps {
+//     history: {
+//       listen: (...args) => any;
+//     };
+//     match: {
+//       isExact: boolean;
+//       path: string;
+//       url: string;
+//       params: {
+//         username: string;
+//       }
+//     };
+//     router: {
+//       location: ILocationProps;
+//       action: string;
+//     };
+//   }
 //
-// interface IPageTrackerProps {
-//   history: {
-//     listen: (...args) => any;
-//   };
-//   match: IMatchProps
-//   router: {
-//     location: ILocationProps;
-//     action: string;
-//   };
-// }
+//   const mapStateToProps = (state: RootState): object => ({
+//     router: state.router,
+//   });
 //
-// const mapStateToProps = (state: RootState): object => ({
-//   router: state.router,
-// });
-//
-// function withPageTracker(WrappedComponent) {
 //   const getDescriptionText = (type, slugName) => {
 //     const id = Slugs.findOne({ name: slugName })._id;
 //     switch (type) {
@@ -83,28 +249,14 @@
 //     // Students may not necessarily start reading the description as soon as they enter the page.
 //     // If it is a longer description, they might spend some time skimming the page first before reading. (in milliseconds)
 //     const initiationTime = isLongDescriptionText ? 1500 : 1000;
+//
 //     return expectedReadingTime + initiationTime;
 //   };
 //
-//   const PageTracker = (props: IPageTrackerProps) => {
-//     useEffect(() => {
-//       let timeoutHandle;
-//
-//       // componentWillUnmount
-//       return () => {
-//         Meteor.clearTimeout(timeoutHandle);
-//       };
-//     }, [props.router.location]);
-//
-//     return (
-//       <WrappedComponent {...props} />
-//     );
-//   };
-//
-//   class HistoryListen extends React.Component<IPageTrackerProps> {
+//   class HistoryListen extends React.Component<IHistoryListenProps> {
 //     private timeoutHandle;
 //
-//     componentDidUpdate(prevProps: Readonly<IPageTrackerProps>, prevState: Readonly<{}>, snapshot?: any): void {
+//     componentDidUpdate(prevProps: Readonly<IHistoryListenProps>, prevState: Readonly<{}>, snapshot?: any): void {
 //       const { match, router } = this.props;
 //       if (prevProps.router.location !== router.location) {
 //         // Defining User Interactions
@@ -175,7 +327,5 @@
 //     }
 //   }
 //
-//   return withRouter(connect(mapStateToProps)(PageTracker));
+//   return withRouter(connect(mapStateToProps)(HistoryListen));
 // }
-//
-// export default withPageTracker;
