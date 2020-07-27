684bf73bf9fb67985259beec059f2265dc2df18c
hannaparkUH <53065327+hannaparkUH@users.noreply.github.com>
2020-07-27 09:43:49 -1000

    update the video page

--- app/imports/ui/components/shared/Community/CommunityRadGradVideosWidget.tsx
+++ app/imports/ui/components/shared/Community/CommunityRadGradVideosWidget.tsx
@@ -2,15 +2,20 @@ import * as React from 'react';
 import { Embed } from 'semantic-ui-react';
 import { radgradVideoIds } from '../../../../api/radgrad/radgrad-video-ids';
 
+const videoStyle = { float: left, width: 400, paddingRight: 20, paddingBottom: 30 };
+
+// @ts-ignore
 const CommunityRadGradVideosWidget = () => (
   <>
     {radgradVideoIds.map((link) => (
-      <Embed
-        active
-        autoplay={false}
-        source="youtube"
-        id={link}
-      />
+      <div style={videoStyle}>
+        <Embed
+          active
+          autoplay={false}
+          source="youtube"
+          id={link}
+        />
+      </div>
     ))}
   </>
 );
--- app/imports/ui/pages/shared/CommunityRadGradVideosPage.tsx
+++ app/imports/ui/pages/shared/CommunityRadGradVideosPage.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Header } from 'semantic-ui-react';
 import StudentPageMenuWidget from '../../components/student/StudentPageMenuWidget';
 import BackToTopButton from '../../components/shared/BackToTopButton';
 import CommunityRadGradVideosWidget from '../../components/shared/Community/CommunityRadGradVideosWidget';
@@ -8,9 +8,9 @@ const CommunityRadGradVideosPage = () => (
   <>
     <StudentPageMenuWidget />
     <Container>
+      <Header as="h1">RadGrad Videos</Header>
       <CommunityRadGradVideosWidget />
     </Container>
-
     <BackToTopButton />
   </>
 );
