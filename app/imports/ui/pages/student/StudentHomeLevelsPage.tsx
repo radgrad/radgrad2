fb9ff31e6c65c6210eeaf13e46667a28577fdde1
RashmiAR <rashmiar@hawaii.edu>
2020-12-07 11:05:45 -1000

    StudentLevelsPage

--- app/imports/ui/pages/student/StudentHomeLevelsPage.tsx
+++ app/imports/ui/pages/student/StudentHomeLevelsPage.tsx
@@ -1,20 +1,28 @@
 import { withTracker } from 'meteor/react-meteor-data';
+import { useParams } from 'react-router-dom';
 import React from 'react';
 import { Grid, Container, Card, Image } from 'semantic-ui-react';
 import { HelpMessages } from '../../../api/help/HelpMessageCollection';
-import HelpPanelWidget, { IHelpPanelWidgetProps } from '../../components/shared/HelpPanelWidget';
+import HelpPanelWidget from '../../components/shared/HelpPanelWidget';
 import StudentPageMenuWidget from '../../components/student/StudentPageMenuWidget';
 import BackToTopButton from '../../components/shared/BackToTopButton';
 import StudentLevelsWidget from '../../components/student/levels/StudentLevelsWidget';
 import StudentLevelsOthersWidget from '../../components/student/levels/StudentLevelsOthersWidget';
+import { Users } from '../../../api/user/UserCollection';
+import { IStudentProfile, IHelpMessage } from '../../../typings/radgrad';
 
-const StudentHomeLevelsPage: React.FC<IHelpPanelWidgetProps> = ({ helpMessages }) => (
+interface IStudentHomeLevelsPageProps {
+  profile: IStudentProfile;
+  helpMessages: IHelpMessage[];
+}
+
+const StudentHomeLevelsPage = (props: IStudentHomeLevelsPageProps) => (
   <div id="student-levels-page">
     <StudentPageMenuWidget />
     <Container>
       <Grid stackable>
         <Grid.Row>
-          <Grid.Column width={16}><HelpPanelWidget helpMessages={helpMessages} /></Grid.Column>
+          <Grid.Column width={16}><HelpPanelWidget helpMessages={props.helpMessages} /></Grid.Column>
         </Grid.Row>
         <Grid.Row>
           <Grid.Column width={16}>
@@ -29,8 +37,10 @@ const StudentHomeLevelsPage: React.FC<IHelpPanelWidgetProps> = ({ helpMessages }
                   <Card.Header>LEVEL 1</Card.Header>
                   <Card.Meta>GRAY</Card.Meta>
                   <Card.Description>
-                    You begin your RadGrad experience at Level 1, and you will receive this laptop sticker when you
-                    first sign up for RadGrad with your advisor. <em>&quot;A journey of a thousand miles begins with a
+                    You begin your RadGrad experience at Level 1, and you will receive this
+                    laptop sticker when you
+                    first sign up for RadGrad with your advisor. <em>&quot;A journey of a
+                      thousand miles begins with a
                       single step&quot; -- Lao Tzu</em>
                   </Card.Description>
                 </Card.Content>
@@ -45,8 +55,10 @@ const StudentHomeLevelsPage: React.FC<IHelpPanelWidgetProps> = ({ helpMessages }
                   <Card.Header>LEVEL 2</Card.Header>
                   <Card.Meta>YELLOW</Card.Meta>
                   <Card.Description>
-                    Successfully finish your first academic term of ICS coursework. Then meet with your advisor and ask
-                    him/her to update RadGrad with your current STAR data. That should bring you to Level 2, and earn
+                    Successfully finish your first academic term of ICS coursework. Then meet
+                    with your advisor and ask
+                    him/her to update RadGrad with your current STAR data. That should bring you
+                    to Level 2, and earn
                     you the Level 2 laptop sticker.
                   </Card.Description>
                 </Card.Content>
@@ -61,9 +73,12 @@ const StudentHomeLevelsPage: React.FC<IHelpPanelWidgetProps> = ({ helpMessages }
                   <Card.Header>LEVEL 3</Card.Header>
                   <Card.Meta>GREEN</Card.Meta>
                   <Card.Description>
-                    With any luck, you&apos;ll achieve Level 3 after you complete your second academic term of ICS
-                    coursework, as long as your grades are good. As before, meet with your Advisor to update RadGrad
-                    with your current STAR data, and if the system shows you&apos;ve gotten to Level 3, you&apos;ll get
+                    With any luck, you&apos;ll achieve Level 3 after you complete your second
+                    academic term of ICS
+                    coursework, as long as your grades are good. As before, meet with your
+                    Advisor to update RadGrad
+                    with your current STAR data, and if the system shows you&apos;ve gotten to
+                    Level 3, you&apos;ll get
                     your Green laptop sticker.
                   </Card.Description>
                 </Card.Content>
@@ -78,9 +93,12 @@ const StudentHomeLevelsPage: React.FC<IHelpPanelWidgetProps> = ({ helpMessages }
                   <Card.Header>LEVEL 4</Card.Header>
                   <Card.Meta>BLUE</Card.Meta>
                   <Card.Description>
-                    ICS has a &quot;core curriculum&quot;, and Level 4 students have not only finished it, but they have
-                    also thought beyond mere competency. Once your current STAR data is in RadGrad, and you&apos;ve
-                    achieved some verified opportunities, you might just find yourself at Level 4! Meet with your
+                    ICS has a &quot;core curriculum&quot;, and Level 4 students have not only
+                    finished it, but they have
+                    also thought beyond mere competency. Once your current STAR data is in
+                    RadGrad, and you&apos;ve
+                    achieved some verified opportunities, you might just find yourself at Level
+                    4! Meet with your
                     advisor to pick up your sticker, and bask in the glory it will bring to you!
                   </Card.Description>
                 </Card.Content>
@@ -95,11 +113,16 @@ const StudentHomeLevelsPage: React.FC<IHelpPanelWidgetProps> = ({ helpMessages }
                   <Card.Header>LEVEL 5</Card.Header>
                   <Card.Meta>BROWN</Card.Meta>
                   <Card.Description>
-                    Level 5 students are far along in their degree program, and they&apos;ve made significant progress
-                    toward 100 verified points in each of the three ICE categories. You will probably be at least a
-                    Junior before Level 5 becomes a realistic option for you. Keep your STAR data current in RadGrad,
-                    make sure your opportunities are verified, and good luck! Some students might graduate before
-                    reaching Level 5, so try to be one of the few that make it all the way to here!
+                    Level 5 students are far along in their degree program, and they&apos;ve
+                    made significant progress
+                    toward 100 verified points in each of the three ICE categories. You will
+                    probably be at least a
+                    Junior before Level 5 becomes a realistic option for you. Keep your STAR
+                    data current in RadGrad,
+                    make sure your opportunities are verified, and good luck! Some students
+                    might graduate before
+                    reaching Level 5, so try to be one of the few that make it all the way to
+                    here!
                   </Card.Description>
                 </Card.Content>
               </Card>
@@ -113,10 +136,14 @@ const StudentHomeLevelsPage: React.FC<IHelpPanelWidgetProps> = ({ helpMessages }
                   <Card.Header>LEVEL 6</Card.Header>
                   <Card.Meta>BLACK</Card.Meta>
                   <Card.Description>
-                    If you achieve Level 6, you are truly one of the elite ICS students, and you will have demonstrated
-                    excellent preparation for entering the workforce, or going on to Graduate School, whichever you
-                    prefer. Congratulations! Note that in addition to fulfilling the ICE requirements, you&apos;ll also need
-                    to &quot;pay it forward&quot; to the RadGrad community in order to obtain your Black RadGrad laptop
+                    If you achieve Level 6, you are truly one of the elite ICS students, and you
+                    will have demonstrated
+                    excellent preparation for entering the workforce, or going on to Graduate
+                    School, whichever you
+                    prefer. Congratulations! Note that in addition to fulfilling the ICE
+                    requirements, you&apos;ll also need
+                    to &quot;pay it forward&quot; to the RadGrad community in order to obtain
+                    your Black RadGrad laptop
                     sticker.
                   </Card.Description>
                 </Card.Content>
@@ -128,7 +155,7 @@ const StudentHomeLevelsPage: React.FC<IHelpPanelWidgetProps> = ({ helpMessages }
           <Grid.Column width={16}>
             <Grid container stackable columns="equal">
               <Grid.Column stretched>
-                <StudentLevelsWidget />
+                <StudentLevelsWidget profile={props.profile} />
               </Grid.Column>
               <Grid.Column stretched>
                 <StudentLevelsOthersWidget />
@@ -136,9 +163,7 @@ const StudentHomeLevelsPage: React.FC<IHelpPanelWidgetProps> = ({ helpMessages }
             </Grid>
           </Grid.Column>
         </Grid.Row>
-
       </Grid>
-
       <BackToTopButton />
     </Container>
   </div>
@@ -146,8 +171,11 @@ const StudentHomeLevelsPage: React.FC<IHelpPanelWidgetProps> = ({ helpMessages }
 
 const StudentHomeLevelsPageContainer = withTracker(() => {
   const helpMessages = HelpMessages.findNonRetired({});
+  const { username } = useParams();
+  const profile = Users.getProfile(username) as IStudentProfile;
   return {
     helpMessages,
+    profile,
   };
 })(StudentHomeLevelsPage);
 
