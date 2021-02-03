0cb096d7ee3d43cad187315e3d599320149898a1
Cam Moore <cammymoore07@gmail.com>
2021-02-03 09:41:52 -1000

    Update download-bulk-star-json.ts
    STAR download is now working.

--- scripts/ts/download-bulk-star-json.ts
+++ scripts/ts/download-bulk-star-json.ts
@@ -24,7 +24,10 @@ function splitEmails(emails, key, emailsPerFile) {
   retVal.keys = [];
   while (end < numEmails + emailsPerFile) {
     retVal.keys.push(`${key}-${count}`);
-    fs.writeFileSync(`emails${key}-${count}.txt`, emailArr.slice(start, end).join('\n'));
+    fs.writeFileSync(
+      `emails${key}-${count}.txt`,
+      emailArr.slice(start, end).join('\n'),
+    );
     count++;
     start += emailsPerFile;
     end += emailsPerFile;
@@ -37,7 +40,9 @@ function getStarArrivalTime(emails) {
   const numEmails = emailData.toString().split('\n').length;
   const arrivalTime = moment().add(numEmails * 10, 's');
   // eslint-disable-next-line
-  return `Obtaining STAR data for ${numEmails} users. Results expected at ${arrivalTime.format('h:mm A')} (${arrivalTime.fromNow()})`;
+  return `Obtaining STAR data for ${numEmails} users. Results expected at ${arrivalTime.format(
+    'h:mm A',
+  )} (${arrivalTime.fromNow()})`;
 }
 
 /**
@@ -55,12 +60,15 @@ function getCourseData(username, password, emails) {
   };
 
   return new Promise((res, rej) => {
-    // request.post('https://www.star.hawaii.edu:10011/api/radgrad', params, function (err, httpRes, body) {
     // eslint-disable-next-line no-unused-vars
-    request.post('https://www.star.hawaii.edu/api/radgrad', params, function (err, httpRes, body) {
-      if (err) return rej(err);
-      return res(httpRes);
-    });
+    request.post(
+      'https://www.star.hawaii.edu/api/radgrad',
+      params,
+      function (err, httpRes, body) {
+        if (err) return rej(err);
+        return res(httpRes);
+      },
+    );
   });
 }
 
@@ -118,27 +126,27 @@ async function downloadStarData() {
       name: 'username',
       type: 'input',
       message: 'Enter your UH username:',
-      validate: value => (value.length ? true : 'Please enter your UH username'),
+      validate: (value) => (value.length ? true : 'Please enter your UH username'),
     },
     {
       name: 'password',
       type: 'password',
       message: 'Enter your password:',
-      validate: value => (value.length ? true : 'Please enter your password'),
+      validate: (value) => (value.length ? true : 'Please enter your password'),
     },
     {
       name: 'key',
       type: 'input',
       message: 'Enter the emails list key: (e.g. emails{key}.txt',
-      validate: value => (value.length ? true : 'Please enter the email list file name'),
+      validate: (value) => (value.length ? true : 'Please enter the email list file name'),
     },
     {
       name: 'emailsPerFile',
       type: 'input',
       message: 'Enter the number of emails per file: ',
-      validate: value => {
+      validate: (value) => {
         const num = parseInt(value, 10);
-        return (num > 0 ? true : 'Please enter a positive number');
+        return num > 0 ? true : 'Please enter a positive number';
       },
     },
   ];
@@ -153,8 +161,8 @@ async function downloadStarData() {
     const sFileName = `star${k}.json`;
     console.log(getStarArrivalTime(eFileName));
     getCourseData(userParams.username, userParams.password, eFileName)
-        .then((res: any) => fs.writeFileSync(sFileName, filterAlumni(res.body, k)))
-        .catch(err => console.log(err));
+      .then((res: any) => fs.writeFileSync(sFileName, filterAlumni(res.body, k)))
+      .catch((err) => console.log(err));
   });
   // const starFilenName = `star${userParams.key}.json`;
   // console.log(getStarArrivalTime(emailFileName));
@@ -169,7 +177,9 @@ async function downloadStarData() {
 // downloadStarData();
 
 program
-  .description('Request user, password, and email info, then download STAR data. emails.txt must have student email addresses, one per line.')
+  .description(
+    'Request user, password, and email info, then download STAR data. emails.txt must have student email addresses, one per line.',
+  )
   .action(() => downloadStarData());
 
 program.parse(process.argv);
