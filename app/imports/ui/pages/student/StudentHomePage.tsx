4e0be22af382d872663fb17b8ee2b7d9aedb500d
Philip Johnson <johnson@hawaii.edu>
2021-02-08 11:28:21 -1000

    Minor edits

--- app/imports/ui/pages/student/StudentHomePage.tsx
+++ app/imports/ui/pages/student/StudentHomePage.tsx
@@ -5,11 +5,13 @@ import HeaderPane from '../../components/shared/HeaderPane';
 
 const headerPaneTitle = 'Make the most of RadGrad';
 const headerPaneBody = `
-This page contains a personalized set of recommendations to help RadGrad help you!
+This page contains a personalized set of recommendations to help RadGrad help you! It's divided into three sections. Not all of them might be present at any particular time.
 
-  * Please act on suggestions in the <span style="color:red">red section</span> right away. They really help RadGrad help you. 
-  * Suggestions in the <span style="color:yellow">yellow section</span> are requests for you to review your settings or areas of the site that might have changed recently. 
-  * The <span style="color:green">green section</span> lists everything that looks good for now!
+<span style="color:red">The red section:</span> Please act on these right away. They really help RadGrad help you. 
+
+<span style="color:yellow">The yellow section:</span> Requests for you to review your settings or areas of the site that might have changed recently. 
+
+<span style="color:green">The green section:</span>  Looks good for now!
 `;
 
 const StudentHomePage: React.FC = () => (
