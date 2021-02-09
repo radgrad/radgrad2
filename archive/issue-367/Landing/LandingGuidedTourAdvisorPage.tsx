75d0ea7361e146c5f906471b3959a5ccb3c0a969
RashmiAR <rashmiar@hawaii.edu>
2021-02-10 03:38:31 +0530

    removed unimported files

--- archive/issue-367/Landing/LandingGuidedTourAdvisorPage.tsx
+++ archive/issue-367/Landing/LandingGuidedTourAdvisorPage.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Container, List, Segment } from 'semantic-ui-react';
 import Slider from 'react-slick';
-import styles from '../../../app/imports/ui/pages/landing/utilities/guidedtour-style';
+import styles from '../guidedtour-style';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import LandingFooter from '../../../app/imports/ui/components/landing/LandingFooter';
-import WhyRadGrad from '../../../app/imports/ui/components/landing/guided-tour/advisor/why-radgrad';
-import Welcome from '../../../app/imports/ui/components/landing/guided-tour/advisor/welcome';
-import DegreePlan from '../../../app/imports/ui/components/landing/guided-tour/advisor/degree-plan';
-import Opportunities from '../../../app/imports/ui/components/landing/guided-tour/advisor/opportunities';
-import ReviewModeration from '../../../app/imports/ui/components/landing/guided-tour/advisor/review-moderation';
-import WhatsNext from '../../../app/imports/ui/components/landing/guided-tour/advisor/whats-next';
+import WhyRadGrad from '../guided-tour/advisor/why-radgrad';
+import Welcome from '../guided-tour/advisor/welcome';
+import DegreePlan from '../guided-tour/advisor/degree-plan';
+import Opportunities from '../guided-tour/advisor/opportunities';
+import ReviewModeration from '../guided-tour/advisor/review-moderation';
+import WhatsNext from '../guided-tour/advisor/whats-next';
 
 const LandingGuidedTourAdvisorPage: React.FC = () => {
   const settings = {
