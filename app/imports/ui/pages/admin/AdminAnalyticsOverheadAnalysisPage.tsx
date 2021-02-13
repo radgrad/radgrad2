c847d48b63a0ad6610abb865eb6bdf363c6c6d7c
Philip Johnson <johnson@hawaii.edu>
2021-02-13 08:32:32 -1000

    check

--- app/imports/ui/pages/admin/AdminAnalyticsOverheadAnalysisPage.tsx
+++ app/imports/ui/pages/admin/AdminAnalyticsOverheadAnalysisPage.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { withTracker } from 'meteor/react-meteor-data';
-import { HelpMessages } from '../../../api/help/HelpMessageCollection';
 import AdminAnalyticsOverheadAnalysisWidget from '../../components/admin/analytics/overhead-analysis/AdminAnalyticsOverheadAnalysisWidget';
-import { HelpPanelWidgetProps } from '../../components/shared/HelpPanelWidget';
 import PageLayout from '../PageLayout';
 
-const AdminAnalyticsOverheadAnalysisPage: React.FC<HelpPanelWidgetProps> = ({ helpMessages }) => (
+const AdminAnalyticsOverheadAnalysisPage: React.FC = () => (
   <PageLayout id="analytics-overhead-analysis-page" headerPaneTitle="Overhead Analysis">
     <AdminAnalyticsOverheadAnalysisWidget/>
   </PageLayout>
 );
 
-export default withTracker(() => {
-  const helpMessages = HelpMessages.findNonRetired({});
-  return {
-    helpMessages,
-  };
-})(AdminAnalyticsOverheadAnalysisPage);
+export default AdminAnalyticsOverheadAnalysisPage;
--- app/imports/ui/pages/admin/AdminAnalyticsUserInteractionsPage.tsx
+++ app/imports/ui/pages/admin/AdminAnalyticsUserInteractionsPage.tsx
@@ -1,33 +1,14 @@
 import { withTracker } from 'meteor/react-meteor-data';
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
 import { StudentProfiles } from '../../../api/user/StudentProfileCollection';
-import AdminPageMenu from '../../components/admin/AdminPageMenu';
-import AdminAnalyticsMenuWidget from '../../components/admin/analytics/AdminAnalyticsMenuWidget';
 import AdminAnalyticsUserInteractionsWidget, { AdminAnalyticsUserInteractionsWidgetProps } from '../../components/admin/analytics/user-interactions/AdminAnalyticsUserInteractionsWidget';
+import PageLayout from '../PageLayout';
 
-const AdminAnalyticsUserInteractionsPage: React.FC<AdminAnalyticsUserInteractionsWidgetProps> = ({ students }) => {
-  const paddedStyle = {
-    paddingTop: 20,
-  };
-  return (
-    <div id="analytics-user-interactions-page">
-      <AdminPageMenu />
-      <Grid container stackable style={paddedStyle} columns={1}>
-        <Grid.Column>
-          <Grid>
-            <Grid.Column width={3}>
-              <AdminAnalyticsMenuWidget />
-            </Grid.Column>
-            <Grid.Column width={13}>
-              <AdminAnalyticsUserInteractionsWidget students={students} />
-            </Grid.Column>
-          </Grid>
-        </Grid.Column>
-      </Grid>
-    </div>
-  );
-};
+const AdminAnalyticsUserInteractionsPage: React.FC<AdminAnalyticsUserInteractionsWidgetProps> = ({students}) => (
+  <PageLayout id="analytics-user-interactions-page" headerPaneTitle="User Interactions">
+    <AdminAnalyticsUserInteractionsWidget students={students}/>
+  </PageLayout>
+);
 
 const con = withTracker(() => {
   const students = StudentProfiles.find({ isAlumni: false }).fetch();
@@ -35,4 +16,5 @@ const con = withTracker(() => {
     students,
   };
 })(AdminAnalyticsUserInteractionsPage);
+
 export default con;
