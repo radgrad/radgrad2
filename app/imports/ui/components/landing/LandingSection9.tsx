e71b1464aa563bc8ab7ace7f87bbec7a593c1490
Cam Moore <cammymoore07@gmail.com>
2021-02-13 10:33:04 -1000

    Update LandingSection9.tsx

--- app/imports/ui/components/landing/LandingSection9.tsx
+++ app/imports/ui/components/landing/LandingSection9.tsx
@@ -10,7 +10,7 @@ interface LandingSection9Props {
 }
 const LandingSection9: React.FC<LandingSection9Props> = ({ instanceName, userGuideURL }) => (
   <div id="landing-section-9" style={styles['section-9']}>
-    <Grid doubled stackable container columns={2}>
+    <Grid doubling stackable container columns={2}>
       <Grid.Column>
         <div style={styles['ready-to-get-started']}>
           <Grid>
