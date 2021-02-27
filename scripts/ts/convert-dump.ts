4ebdd95173664d3f7bf9038f6632833dcbba559d
Cam Moore <cammymoore07@gmail.com>
2021-02-27 13:02:24 -1000

    Added conversion of career goal interests to interests

--- scripts/ts/convert-dump.ts
+++ scripts/ts/convert-dump.ts
@@ -1,8 +1,9 @@
 import * as fs from 'fs';
 // import * as inquirer from 'inquirer';
 import * as _ from 'lodash';
-import * as moment from 'moment';
+import moment from 'moment';
 import { program } from 'commander';
+import { getCollectionData, getCollectionDocFromSlug } from './data-dump-utils';
 
 interface ICollection {
   name: string;
@@ -16,92 +17,94 @@ interface IDataDump {
 
 const databaseFileDateFormat = 'YYYY-MM-DD-HH-mm-ss';
 
-const convertHelpRouteName = (name: string): string => {
-  switch (name) {
-    case 'Advisor_Student_Configuration_Page':
-      return '/advisor/:username/home';
-    case 'Advisor_Verification_Requests_Pending_Page':
-      return '/advisor/:username/verification-requests';
-    case 'Advisor_Event_Verification_Page':
-      return '/advisor/:username/event-verification';
-    case 'Advisor_Completed_Verifications_Page':
-      return '/advisor/:username/completed-verifications';
-    case 'Advisor_Academic_Plan_Page':
-      return '/advisor/:username/academic-plan';
-    case 'Advisor_Moderation_Page':
-      return '/advisor/:username/moderation';
-    case 'Faculty_Home_Page':
-      return '/faculty/:username/home';
-    case 'Faculty_Manage_Opportunities_Page':
-      return '/faculty/:username/manage-opportunities';
-    case 'Faculty_Explorer_Page':
-      return '/faculty/:username/explorer';
-    case 'Faculty_Explorer_CareerGoals_Page':
-      return '/faculty/:username/explorer/career-goals';
-    case 'Faculty_Explorer_Courses_Page':
-      return '/faculty/:username/explorer/courses';
-    case 'Faculty_Explorer_Plans_Page':
-      return '/faculty/:username/explorer/academic-plans';
-    case 'Faculty_Explorer_Interests_Page':
-      return '/faculty/:username/explorer/interests';
-    case 'Faculty_Explorer_Opportunities_Page':
-      return '/faculty/:username/explorer/opportunities';
-    case 'Faculty_Explorer_Users_Page':
-      return '/faculty/:username/explorer/users';
-    case 'Faculty_Verification_Page':
-      return '/faculty/:username/verification-requests';
-    case 'Mentor_Home_Page':
-      return '/mentor/:username/home';
-    case 'Mentor_Explorer_Page':
-      return '/mentor/:username/explorer';
-    case 'Mentor_Explorer_CareerGoals_Page':
-      return '/mentor/:username/explorer/career-goals';
-    case 'Mentor_Explorer_Courses_Page':
-      return '/mentor/:username/explorer/courses';
-    case 'Mentor_Explorer_Plans_Page':
-      return '/mentor/:username/explorer/academic-plans';
-    case 'Mentor_Explorer_Interests_Page':
-      return '/mentor/:username/explorer/interests';
-    case 'Mentor_Explorer_Opportunities_Page':
-      return '/mentor/:username/explorer/opportunities';
-    case 'Mentor_Explorer_Users_Page':
-      return '/mentor/:username/explorer/users';
-    case 'Mentor_MentorSpace_Page':
-      return '/mentor/:username/mentor-space';
-    case 'Student_Home_Page':
-      return '/student/:username/home';
-    case 'Student_Degree_Planner_Page':
-      return '/student/:username/degree-planner';
-    case 'Student_Home_AboutMe_Page':
-      return '/student/:username/home/about-me';
-    case 'Student_Home_Ice_Page':
-      return '/student/:username/home/ice';
-    case 'Student_Home_Levels_Page':
-      return '/student/:username/home/levels';
-    case 'Student_Ice':
-      return '/student/:username/ice';
-    case 'Student_Explorer_Page':
-      return '/student/:username/explorer';
-    case 'Student_MentorSpace_Page':
-      return '/student/:username/mentor-space';
-    case 'Student_Home_Log_Page':
-      return '/student/:username/home/log';
-    case 'Student_Explorer_CareerGoals_Page':
-      return '/student/:username/explorer/career-goals';
-    case 'Student_Explorer_Courses_Page':
-      return '/student/:username/explorer/courses';
-    case 'Student_Explorer_Plans_Page':
-      return '/student/:username/explorer/academic-plans';
-    case 'Student_Explorer_Interests_Page':
-      return '/student/:username/explorer/interests';
-    case 'Student_Explorer_Opportunities_Page':
-      return '/student/:username/explorer/opportunities';
-    case 'Student_Explorer_Users_Page':
-      return '/student/:username/explorer/users';
-    default:
-      return name;
-  }
-};
+let radGrad1DataDump: IDataDump;
+
+// const convertHelpRouteName = (name: string): string => {
+//   switch (name) {
+//     case 'Advisor_Student_Configuration_Page':
+//       return '/advisor/:username/home';
+//     case 'Advisor_Verification_Requests_Pending_Page':
+//       return '/advisor/:username/verification-requests';
+//     case 'Advisor_Event_Verification_Page':
+//       return '/advisor/:username/event-verification';
+//     case 'Advisor_Completed_Verifications_Page':
+//       return '/advisor/:username/completed-verifications';
+//     case 'Advisor_Academic_Plan_Page':
+//       return '/advisor/:username/academic-plan';
+//     case 'Advisor_Moderation_Page':
+//       return '/advisor/:username/moderation';
+//     case 'Faculty_Home_Page':
+//       return '/faculty/:username/home';
+//     case 'Faculty_Manage_Opportunities_Page':
+//       return '/faculty/:username/manage-opportunities';
+//     case 'Faculty_Explorer_Page':
+//       return '/faculty/:username/explorer';
+//     case 'Faculty_Explorer_CareerGoals_Page':
+//       return '/faculty/:username/explorer/career-goals';
+//     case 'Faculty_Explorer_Courses_Page':
+//       return '/faculty/:username/explorer/courses';
+//     case 'Faculty_Explorer_Plans_Page':
+//       return '/faculty/:username/explorer/academic-plans';
+//     case 'Faculty_Explorer_Interests_Page':
+//       return '/faculty/:username/explorer/interests';
+//     case 'Faculty_Explorer_Opportunities_Page':
+//       return '/faculty/:username/explorer/opportunities';
+//     case 'Faculty_Explorer_Users_Page':
+//       return '/faculty/:username/explorer/users';
+//     case 'Faculty_Verification_Page':
+//       return '/faculty/:username/verification-requests';
+//     case 'Mentor_Home_Page':
+//       return '/mentor/:username/home';
+//     case 'Mentor_Explorer_Page':
+//       return '/mentor/:username/explorer';
+//     case 'Mentor_Explorer_CareerGoals_Page':
+//       return '/mentor/:username/explorer/career-goals';
+//     case 'Mentor_Explorer_Courses_Page':
+//       return '/mentor/:username/explorer/courses';
+//     case 'Mentor_Explorer_Plans_Page':
+//       return '/mentor/:username/explorer/academic-plans';
+//     case 'Mentor_Explorer_Interests_Page':
+//       return '/mentor/:username/explorer/interests';
+//     case 'Mentor_Explorer_Opportunities_Page':
+//       return '/mentor/:username/explorer/opportunities';
+//     case 'Mentor_Explorer_Users_Page':
+//       return '/mentor/:username/explorer/users';
+//     case 'Mentor_MentorSpace_Page':
+//       return '/mentor/:username/mentor-space';
+//     case 'Student_Home_Page':
+//       return '/student/:username/home';
+//     case 'Student_Degree_Planner_Page':
+//       return '/student/:username/degree-planner';
+//     case 'Student_Home_AboutMe_Page':
+//       return '/student/:username/home/about-me';
+//     case 'Student_Home_Ice_Page':
+//       return '/student/:username/home/ice';
+//     case 'Student_Home_Levels_Page':
+//       return '/student/:username/home/levels';
+//     case 'Student_Ice':
+//       return '/student/:username/ice';
+//     case 'Student_Explorer_Page':
+//       return '/student/:username/explorer';
+//     case 'Student_MentorSpace_Page':
+//       return '/student/:username/mentor-space';
+//     case 'Student_Home_Log_Page':
+//       return '/student/:username/home/log';
+//     case 'Student_Explorer_CareerGoals_Page':
+//       return '/student/:username/explorer/career-goals';
+//     case 'Student_Explorer_Courses_Page':
+//       return '/student/:username/explorer/courses';
+//     case 'Student_Explorer_Plans_Page':
+//       return '/student/:username/explorer/academic-plans';
+//     case 'Student_Explorer_Interests_Page':
+//       return '/student/:username/explorer/interests';
+//     case 'Student_Explorer_Opportunities_Page':
+//       return '/student/:username/explorer/opportunities';
+//     case 'Student_Explorer_Users_Page':
+//       return '/student/:username/explorer/users';
+//     default:
+//       return name;
+//   }
+// };
 
 const convertObject = (item) => {
   const result: any = {};
@@ -119,9 +122,6 @@ const convertObject = (item) => {
         break;
       case 'number':
         result.num = item[key];
-        break;
-      case 'routeName':
-        result[key] = convertHelpRouteName(item[key]);
         break;
       case 'semester':
         result.academicTerm = item[key];
@@ -163,11 +163,22 @@ const convertCareerGoal = (goal) => {
   return result;
 };
 
-const convertPictureUrl = (profile) => {
+const convertProfile = (profile, data) => {
   const result: any = {};
   for (const key in profile) { // eslint-disable-line no-restricted-syntax
     if (key === 'picture') {
       result.picture = profile.picture.replace(/http:\/\/res.cloudinary.com/g, 'https://res.cloudinary.com');
+    } else if (key === 'interests') {
+      let interests = profile.interests;
+      const careerGoalCollection = getCollectionData(data, 'CareerGoalCollection');
+      const favoriteCareerGoalCollection = getCollectionData(data, 'FavoriteCareerGoalCollection');
+      const profileFavoriteCareerGoals = _.filter(favoriteCareerGoalCollection, (entry) => entry.username === profile.username);
+      const profileCareerGoalSlugs = profileFavoriteCareerGoals.map((fav) => fav.careerGoal);
+      const profileCareerGoals = profileCareerGoalSlugs.map((slug) => getCollectionDocFromSlug(careerGoalCollection, slug));
+      profileCareerGoals.forEach((careerGoal) => {
+        interests = _.union(interests, careerGoal.interests);
+      });
+      result[key] = _.uniq(interests);
     } else {
       result[key] = profile[key];
     }
@@ -175,7 +186,7 @@ const convertPictureUrl = (profile) => {
   return result;
 };
 
-function processRadGradCollection(collection: ICollection) {
+const processRadGradCollection = (collection: ICollection) => {
   const result: any = {};
   // console.log(collection.name, collection.contents);
   if (collection.name === 'SemesterCollection') {
@@ -187,14 +198,14 @@ function processRadGradCollection(collection: ICollection) {
     result.contents = _.map(collection.contents, convertCareerGoal);
   } else if (collection.name === 'StudentProfileCollection' || collection.name === 'AdvisorProfileCollection' || collection.name === 'FacultyProfileCollection') {
     result.name = collection.name;
-    result.contents = _.map(collection.contents, convertPictureUrl);
+    result.contents = _.map(collection.contents, (profile) => convertProfile(profile, radGrad1DataDump));
   } else {
     result.name = collection.name;
     result.contents = _.map(collection.contents, convertObject);
   }
   // console.log(result.name, result.contents);
   return result;
-}
+};
 
 const reinitializedCollections = ['FavoriteCareerGoalCollection',
   'FavoriteInterestCollection'];
@@ -215,7 +226,7 @@ const addMissingCollections = (result) => {
 const deletedCollections = ['DesiredDegreeCollection', 'MentorAnswerCollection', 'MentorProfileCollection', 'MentorQuestionCollection', 'UserInteractionCollection', 'AcademicPlanCollection',
   'FavoriteAcademicPlanCollection', 'PlanChoiceCollection', 'AdvisorLogCollection', 'HelpMessageCollection'].concat(reinitializedCollections);
 
-function processRadGradCollections(data: IDataDump) {
+const processRadGradCollections = (data: IDataDump) => {
   const result: any = {};
   result.timestamp = moment().format(databaseFileDateFormat);
   result.collections = [];
@@ -226,7 +237,7 @@ function processRadGradCollections(data: IDataDump) {
   });
   addMissingCollections(result);
   return result;
-}
+};
 
 const compareEntries = (entry1, entry2) => {
   if (entry1.name < entry2.name) {
@@ -240,8 +251,8 @@ const compareEntries = (entry1, entry2) => {
 
 async function convertDump(radgrad1DumpFile, outFileName) {
   const data = fs.readFileSync(radgrad1DumpFile);
-  const radgrad1: IDataDump = JSON.parse(data.toString());
-  const radgrad2 = processRadGradCollections(radgrad1);
+  radGrad1DataDump = JSON.parse(data.toString());
+  const radgrad2 = processRadGradCollections(radGrad1DataDump);
   const data2 = JSON.stringify(radgrad2, null, 2);
   radgrad2.collections.sort((entry1, entry2) => compareEntries(entry1, entry2));
   radgrad2.collections.forEach(entry => console.log(entry.name, entry.contents.length));
