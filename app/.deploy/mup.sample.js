486090afcd7a4d299d666088db88db951fc506d9
Philip Johnson <johnson@hawaii.edu>
2021-03-05 11:58:20 -1000

    checkpoint

--- app/.deploy/mup.sample.js
+++ app/.deploy/mup.sample.js
@@ -1,55 +1,23 @@
 module.exports = {
-  servers: {
-    one: {
-      host: 'changeme.edu',
-      username: 'root',
-      password: 'changeme'
-    }
-  },
-  hooks: {
-    'pre.deploy': {
-      localCommand: 'npm run update-build-version'
-    }
-  },
+  servers: { one: { host: 'CHANGEME.EDU', username: 'root', password: 'CHANGEME' }},
+  hooks: { 'pre.deploy': { localCommand: 'npm run update-build-version' }},
   app: {
     name: 'radgrad',
     path: '../',
-
-    servers: {
-      one: {},
-    },
-
-    buildOptions: {
-      serverOnly: true,
-      debug: true,
-    },
-
+    servers: { one: {}},
+    buildOptions: { serverOnly: true, debug: true },
     env: {
-      ROOT_URL: 'https://changeme.edu',
+      ROOT_URL: 'https://CHANGME.EDU',
       MONGO_URL: 'mongodb://mongodb/meteor',
       MONGO_OPLOG_URL: 'mongodb://mongodb/local',
     },
-
-    docker: {
-      image: 'abernix/meteord:node-12-base',
-    },
-
+    docker: { image: 'abernix/meteord:node-12-base' },
     enableUploadProgressBar: true,
     deployCheckWaitTime: 900
   },
-
-  mongo: {
-    version: '3.4.1',
-    servers: {
-      one: {}
-    }
-  },
-
+  mongo: { version: '3.4.1', servers: { one: {} } },
   proxy: {
-    domains: 'changme.edu',
-    ssl: {
-      letsEncryptEmail: 'changeme@hawaii.edu',
-      forceSSL: true
-    }
+    domains: 'CHANGEME.EDU',
+    ssl: { letsEncryptEmail: 'CHANGEME@HAWAII.EDU', forceSSL: true }
   }
 };
