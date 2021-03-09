df9a01dc612976e5d86a242028c6b79060a94033
RashmiAR <rashmiar@hawaii.edu>
2021-03-09 13:32:25 -1000

    checkpoint

--- app/imports/ui/components/shared/explorer/browser-view/InterestBrowserView.tsx
+++ app/imports/ui/components/shared/explorer/browser-view/InterestBrowserView.tsx
@@ -8,6 +8,7 @@ import { EXPLORER_TYPE } from '../../../../layouts/utilities/route-constants';
 import ProfileCard from './ProfileCard';
 import WidgetHeaderNumber from '../WidgetHeaderNumber';
 import {RadGradProperties} from '../../../../../api/radgrad/RadGradProperties';
+import InterestSortWidget, {interestSortKeys} from './InterestSortWidget';
 
 interface InterestBrowserViewProps {
   interests: Interest[];
@@ -15,10 +16,12 @@ interface InterestBrowserViewProps {
   // Saving Scroll Position
   interestsScrollPosition: number;
   setInterestsScrollPosition: (scrollPosition: number) => any;
+  sortValue: string;
 }
 
 const mapStateToProps = (state: RootState) => ({
   interestsScrollPosition: state.shared.scrollPosition.explorer.interests,
+  sortValue: state.shared.cardExplorer.interests.sortValue,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -27,8 +30,14 @@ const mapDispatchToProps = (dispatch) => ({
 
 const adminEmail = RadGradProperties.getAdminEmail();
 
-const InterestBrowserView: React.FC<InterestBrowserViewProps> = ({ interests, inProfile, interestsScrollPosition, setInterestsScrollPosition }) => {
+const InterestBrowserView: React.FC<InterestBrowserViewProps> = ({ interests, inProfile, interestsScrollPosition, setInterestsScrollPosition, sortValue }) => {
   const cardGroupElement: HTMLElement = document.getElementById('interestsCardGroup');
+  switch (sortValue) {
+    case interestSortKeys.mostRecent:
+      break;
+    default:
+      // this.props.interests = _.sortBy(interests, (item) => item.name);
+  }
   useEffect(() => {
     const savedScrollPosition = interestsScrollPosition;
     if (savedScrollPosition && cardGroupElement) {
@@ -50,7 +59,7 @@ const InterestBrowserView: React.FC<InterestBrowserViewProps> = ({ interests, in
             ? <p color='grey'><Icon name='heart' color='grey' size='large'/>
               INTERESTS IN MY PROFILE <WidgetHeaderNumber inputValue={interests.length}/>
                { interests.length < 3 ?
-                  <span><Icon name='exclamation triangle' color='red'/> Please add atleast <b>three interests</b> to your profile</span> : '' }
+                  <span style={{ float: 'right' }}><Icon name='exclamation triangle' color='red'/> Please add atleast <b>three interests</b> to your profile</span> : '' }
               </p>
             : <p color='grey'>INTERESTS NOT IN MY PROFILE <WidgetHeaderNumber inputValue={interests.length}/>
               <Button floated='right' basic color='teal' href={`mailto:${adminEmail}?subject=New Interest Suggestion`}>
@@ -61,6 +70,7 @@ const InterestBrowserView: React.FC<InterestBrowserViewProps> = ({ interests, in
               }
           </header>
           <Divider />
+          {!inProfile ? <InterestSortWidget /> :''}
         <Card.Group itemsPerRow={4} stackable id="interestsCardGroup">
           {interests.map((interest) => (
             <ProfileCard key={interest._id} item={interest} type={EXPLORER_TYPE.INTERESTS} cardLinkName={inProfile? 'See Details / Remove from Profile':'See Details / Add to Profile'}/>
