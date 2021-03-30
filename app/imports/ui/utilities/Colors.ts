1f373aab78ca0b227b2b7b2031914049f96d0dcd
Philip Johnson <johnson@hawaii.edu>
2021-03-30 07:54:13 -1000

    Update color theme.

---
+++ app/imports/ui/utilities/Colors.ts
@@ -0,0 +1,22 @@
+/*
+ * Provide programmatic access to the colors defined in our Semantic UI Theme.
+ * These definitions should be in sync with those found in:
+ * https://github.com/radgrad/fomantic-ui/blob/main/semantic/src/site/globals/site.variables
+ */
+export const enum COLORS {
+  BLUE =          '#53A78E',
+  GREEN =         '#6FBE44',
+  GREENDARKEN =   '#38840F',
+  GREEN2 =        '#86CAB5',
+  GREEN2DARKEN =  '#53A78E',
+  GREEN2DARKEN1 = '#1B7F61',
+  GREY =          '#696969',
+  GREY2 =         '#F7F7F7',
+  OLIVE =         '#38840F',
+  ORANGE =        '#DA7F4E',
+  PINK =          '#DDA1C3',
+  PURPLE =        '#952263',
+  RED =           '#CA4864',
+  WHITE =         '#FFFFFF',
+  YELLOW =        '#CEB23F',
+}
