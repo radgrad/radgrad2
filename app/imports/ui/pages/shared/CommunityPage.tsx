960da084d99dcda3b4f06437f6ad7600e0c461c2
Philip Johnson <johnson@hawaii.edu>
2021-04-08 12:48:48 -1000

    checkpoint

--- app/imports/ui/pages/shared/CommunityPage.tsx
+++ app/imports/ui/pages/shared/CommunityPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Grid, Header, Icon, Segment } from 'semantic-ui-react';
 import PageLayout from '../PageLayout';
 
 const headerPaneTitle = 'What\'s happening in RadGrad?';
@@ -8,10 +8,36 @@ Here are the latest updates in RadGrad, plus overviews of the RadGrad community.
 `;
 const headerPaneImage = 'header-community.png';
 
-
 const CommunityPage: React.FC = () => (
   <PageLayout id="community-page" headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody} headerPaneImage={headerPaneImage}>
-    <Header>Community Page Placeholder</Header>
+    <Grid stackable>
+      <Grid.Row>
+        <Grid.Column width={16}>
+          <Segment>
+            <Header dividing><Icon name="graduation cap" /> RADGRAD STUDENTS</Header>
+          </Segment>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Column width={10}>
+        <Segment>
+          <Header dividing><Icon name="calendar alternate outline" /> UPCOMING EVENTS</Header>
+        </Segment>
+        <Segment>
+          <Header dividing><Icon name="user" />TOP LEVEL STUDENTS</Header>
+        </Segment>
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <Segment>
+          <Header dividing><Icon className='calendar alternate outline' /> COMMUNITY ACTIVITY</Header>
+        </Segment>
+        <Segment>
+          <Header dividing><Icon name="lightbulb outline" /> TOP 5 INTERESTS</Header>
+        </Segment>
+        <Segment>
+          <Header dividing><Icon name="briefcase" />TOP 5 CAREER GOALS</Header>
+        </Segment>
+      </Grid.Column>
+    </Grid>
   </PageLayout>
 );
 
