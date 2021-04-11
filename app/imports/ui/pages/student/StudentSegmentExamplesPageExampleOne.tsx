3c0961b00c81a6496a9a56312cdcd179eaccb826
Philip Johnson <johnson@hawaii.edu>
2021-04-11 11:35:51 -1000

    Fix typo.

--- app/imports/ui/pages/student/StudentSegmentExamplesPageExampleOne.tsx
+++ app/imports/ui/pages/student/StudentSegmentExamplesPageExampleOne.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import RadGradHeader from '../../components/shared/RadGradHeader';
-import RadGradSegment2 from '../../components/shared/RadGradSegment';
+import RadGradSegment from '../../components/shared/RadGradSegment';
 
 const StudentSegmentExamplesPageExampleOne: React.FC = () => {
   const header = <RadGradHeader title='Example One' count={3} icon='graduation cap' />;
   return (
-    <RadGradSegment2 header={header}>
+    <RadGradSegment header={header}>
       This is an example of a RadGrad segment with a simple header and no right side component.
-    </RadGradSegment2>
+    </RadGradSegment>
   );
 };
 
