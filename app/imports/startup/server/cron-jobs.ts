5ee312a0f027b6d408a4f17d53fe04bdce758f69
Philip Johnson <johnson@hawaii.edu>
2021-04-23 10:32:11 -1000

    checkpoint

--- app/imports/startup/server/cron-jobs.ts
+++ app/imports/startup/server/cron-jobs.ts
@@ -3,6 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { updateIceSnapshot } from '../../api/ice/IceSnapshot';
 import { updateFactoids } from '../../api/factoid/factoids';
 import { PublicStats } from '../../api/public-stats/PublicStatsCollection';
+import { RadGradForecasts } from '../both/RadGradForecasts';
 
 SyncedCron.add({
   name: 'Update ICE Snapshots',
@@ -20,6 +21,12 @@ SyncedCron.add({
   name: 'Update public stats',
   schedule: (parser) => parser.text('every 24 hours'),
   job: () => PublicStats.generateStats(),
+});
+
+SyncedCron.add({
+  name: 'Update Forecasts',
+  schedule: (parser) => parser.text('every 24 hours'),
+  job: () => RadGradForecasts.updateForecasts(),
 });
 
 Meteor.startup(() => {
