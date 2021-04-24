0785270bf64cba61d23bed327f77d70ab8858535
Philip Johnson <johnson@hawaii.edu>
2021-04-24 09:05:52 -1000

    checkpoint

---
+++ app/imports/ui/pages/sandbox/SandboxStickyState2.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Checkbox } from 'semantic-ui-react';
+import RadGradHeader from '../../components/shared/RadGradHeader';
+import RadGradSegment from '../../components/shared/RadGradSegment';
+import { useStickyState } from '../../utilities/StickyState';
+
+const SandboxStickyState2: React.FC = () => {
+  const [checkState, setCheckState] = useStickyState('SandboxStickyState', true);
+  const handleChange = (e, { checked }) => setCheckState(checked);
+  const checkStateValue = checkState ? 'Checked' : 'Not Checked';
+
+  const rightside = <Checkbox onChange={handleChange} checked={checkState} label='Show all'/>;
+  const header = <RadGradHeader title='useStickyState() is sticky'  icon='user' rightside={rightside} />;
+  return (
+    <RadGradSegment header={header}>
+      <p>This is an example of a RadGrad segment with useStickyState() to declare a sticky state variable to manage the value of a checkbox. A call to useStickyState() looks almost identical to useState():</p>
+
+      <code>const [checkState, setCheckState] = useStickyState('SandboxStickyState', true);</code>
+
+      <p style={{ marginTop: '10px' }}>The difference is that useStickyState requires a string as its first argument to uniquely identify the state variable of interest. The second argument is the initial value.</p>
+
+      <p>Just like useState(), useStickyState() creates a state variable whose value is available within the segment component and can be used to control the data that appears in the segment. For example, the value of the checkbox is: {checkStateValue}.</p>
+
+      <p>But with useStickyState(), the value of the state variable <b>is</b> sticky. To see why, unclick it from its default value, then go to a different page in RadGrad, then press the back arrow to return to this one. You will see that the checkbox retains its changed value, indicating that the state variable value was preserved even though the user navigated away from the page.</p>
+
+      <p>Note that <b>refreshing</b> the page returns the checkbox to its default value. This is unavoidable, and is actually what we want to have happen when a page is refreshed.</p>
+    </RadGradSegment>
+  );
+};
+
+export default SandboxStickyState2;
---
+++ app/imports/ui/pages/sandbox/SandboxStickyState3.tsx
@@ -0,0 +1,27 @@
+import React  from 'react';
+import RadGradHeader from '../../components/shared/RadGradHeader';
+import RadGradSegment from '../../components/shared/RadGradSegment';
+import { useStickyState } from '../../utilities/StickyState';
+
+const SandboxStickyState3: React.FC = () => {
+  const [checkState] = useStickyState('SandboxStickyState', true);
+  const checkStateValue = checkState ? 'Checked' : 'Not Checked';
+
+  const header = <RadGradHeader title='useStickyState() is available in other components'  icon='user'  />;
+  return (
+    <RadGradSegment header={header}>
+      <p>Another cool thing about useStickyState() is that it defines a state variable that can be accessed by any component in the application.</p>
+
+      <p>To access and manipulate a shared sticky (global) value, just use the same name when invoking useStickyState(). So, we use the same name in this component when declaring the state variable as we do in the above component:</p>
+
+      <code>const [checkState, setCheckState] = useStickyState('SandboxStickyState', true);</code>
+
+      <p style={{ marginTop: '10px' }}>Because we used the same name (SandboxStickyState), both this component and the othe one are accessing the same state variable. To see this, the value of the checkbox in the above component is: {checkStateValue}.</p>
+
+      <p>If we wanted to, we could change the state variable in this component and it would be reflected in the other component.</p>
+
+    </RadGradSegment>
+  );
+};
+
+export default SandboxStickyState3;
