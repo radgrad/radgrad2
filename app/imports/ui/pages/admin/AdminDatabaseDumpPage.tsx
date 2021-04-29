ae733e620ac13e75d3deee3d384a3930da74794d
Philip Johnson <johnson@hawaii.edu>
2021-04-29 13:56:43 -1000

    Annotated the code.

--- app/imports/ui/pages/admin/AdminDatabaseDumpPage.tsx
+++ app/imports/ui/pages/admin/AdminDatabaseDumpPage.tsx
@@ -12,12 +12,15 @@ import { RootState } from '../../../redux/types';
 import UploadFixture from '../../components/admin/datamodel/UploadFixture';
 import { PAGEIDS } from '../../utilities/PageIDs';
 import PageLayout from '../PageLayout';
+/** Annotated version of the Redux-based AdminDatabaseDump page. */
 
+/** This is the type of the accordion that lists the contents of all collections after dump finishes. */
 interface Collection {
   name?: string;
   contents?: string[];
 }
 
+/** Page Props. All for Redux. The connect() function at the bottom re-renders the page on changes. */
 interface AdminDatabaseDumpPageProps {
   startDumpDatabase: () => any;
   dumpDatabaseDone: () => any;
@@ -29,11 +32,13 @@ interface AdminDatabaseDumpPageProps {
 
 export const databaseFileDateFormat = 'YYYY-MM-DD-HH-mm-ss';
 
+/** The Redux convention for figure out which of the global state becomes available here. */
 const mapStateToProps = (state: RootState) => ({
   dumpDatabaseWorking: state.admin.database.dumpDatabase,
   getStudentEmailsWorking: state.admin.analytics.newsletter.getStudentEmails,
 });
 
+/** The Redux dispatchers to set the global state when things happen on this page. */
 const mapDispatchToProps = (dispatch) => ({
   startDumpDatabase: () => dispatch(databaseActions.startDumpDatabase()),
   dumpDatabaseDone: () => dispatch(databaseActions.dumpDatabaseDone()),
@@ -42,26 +47,34 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const AdminDatabaseDumpPage: React.FC<AdminDatabaseDumpPageProps> = ({ startDumpDatabase, dumpDatabaseDone, dumpDatabaseWorking, getStudentEmailsDone, getStudentEmailsWorking, startGetStudentEmails }) => {
+  /** Local state for this page: did we get an error from either dumpDatabase or getEmails? */
   const [isErrorState, setIsError] = useState(false);
+  /** Local state: do we have any results to display? */
   const [resultsState, setResults] = useState([]);
 
   const clickDump = () => {
+    // Signal Redux that a dump database operation has started.
     startDumpDatabase();
+    // Call the method. We should convert this to callPromise to make it easier to read.
     dumpDatabaseMethod.call(null, (error, result) => {
       if (error) {
         setIsError(true);
       }
+      // Update our local state with the collection data returned from the method call.
       setResults(result.collections);
+      // Now package it up and download it to the client's disk.
       const zip = new ZipZap();
       const dir = 'radgrad-db';
       const fileName = `${dir}/${moment(result.timestamp).format(databaseFileDateFormat)}.json`;
       zip.file(fileName, JSON.stringify(result, null, 2));
       zip.saveAs(`${dir}.zip`);
+      // Signal Redux that the dump database operation is over.
       dumpDatabaseDone();
     });
   };
 
   const clickEmails = () => {
+    // Signal Redux that we've started the emails.
     startGetStudentEmails();
     generateStudentEmailsMethod.call(null, (error, result) => {
       if (error) {
@@ -78,14 +91,24 @@ const AdminDatabaseDumpPage: React.FC<AdminDatabaseDumpPageProps> = ({ startDump
       const fileName = `${dir}/Students.txt`;
       zip.file(fileName, result.students.join('\n'));
       zip.saveAs(`${dir}.zip`);
+      // Signal Redux that we've finished.
       getStudentEmailsDone();
     });
   };
 
+  // We don't need the following line. :-)
   const errorCondition = isErrorState;
+  // ResultsState is either a list of collections (from the database dump) or a list of emails (from the email sending)
   const showMessage = resultsState.length > 0;
+  // Why these next two lines are here is beyond me.
   const dumpWorking = dumpDatabaseWorking;
   const getWorking = getStudentEmailsWorking;
+
+  /** Now the actual rendering code. Note the following:
+   *    * Both buttons display loading if the *Working variable is true.
+   *    * If there are results (i.e. resultsState.length > 0, then display a simple accordion containing them.
+   *    * Then there's a lonely UploadFixture component at the bottom who doesn't participate in any of this.
+   */
   return (
     <PageLayout id={PAGEIDS.DATABASE_DUMP_DATABASE} headerPaneTitle="Dump Database">
       <Form>
@@ -112,5 +135,6 @@ const AdminDatabaseDumpPage: React.FC<AdminDatabaseDumpPageProps> = ({ startDump
   );
 };
 
+/** Here's where we hook up Redux to this page and get it to re-render upon state changes. */
 const AdminDatabaseDumpPageContainer = connect(mapStateToProps, mapDispatchToProps)(AdminDatabaseDumpPage);
 export default AdminDatabaseDumpPageContainer;
