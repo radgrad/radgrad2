a906d53b99419fae9c761ef890ab7a9201b7b204
Philip Johnson <johnson@hawaii.edu>
2021-04-30 09:04:20 -1000

    checkpoint

---
+++ app/imports/ui/components/admin/database/DumpStudentEmails.tsx
@@ -0,0 +1,62 @@
+import { ZipZap } from 'meteor/udondan:zipzap';
+import moment from 'moment';
+import React, { useState } from 'react';
+import { Button, Form, Grid, Message } from 'semantic-ui-react';
+import { generateStudentEmailsMethod } from '../../../../api/user/UserCollection.methods';
+import { databaseFileDateFormat } from '../../../pages/admin/AdminDatabaseDumpPage';
+import { useStickyState } from '../../../utilities/StickyState';
+import RadGradHeader from '../../shared/RadGradHeader';
+import RadGradSegment from '../../shared/RadGradSegment';
+import AdminDatabaseAccordion from './AdminDatabaseAccordion';
+
+interface Collection {
+  name?: string;
+  contents?: string[];
+}
+const header = <RadGradHeader title='Dump Student Emails' icon='cloud download alternate' />;
+
+const DumpStudentEmails: React.FC = () => {
+  const [error, setError] = useState(false);
+  const [results, setResults] = useState([]);
+  const [inProgress, setInProgress] = useStickyState('DumpStudentEmails', false);
+
+  const onClick = () => {
+    setInProgress(true);
+    generateStudentEmailsMethod.callPromise(null)
+      .catch(err => setError(true))
+      .then(result => {
+        const data: Collection = { name: 'Students', contents: result.students};
+        setResults([data]);
+        const zip = new ZipZap();
+        const now = moment().format(databaseFileDateFormat);
+        const dir = `radgrad-students-${now}`;
+        const fileName = `${dir}/Students.txt`;
+        zip.file(fileName, result.students.join('\n'));
+        zip.saveAs(`${dir}.zip`);
+      })
+      .finally(() => setInProgress(false));
+  };
+
+  return (
+    <RadGradSegment header={header}>
+      <Form>
+        <Button color="green" loading={inProgress} basic type="submit" onClick={onClick}>
+          Dump Student Emails
+        </Button>
+        {results.length > 0 ? (
+          <Grid stackable style={{ paddingTop: '20px' }}>
+            <Message positive={!error} error={error}>
+              {results.map((item, index) => (
+                <AdminDatabaseAccordion key={item.name} index={index} name={item.name} contents={item.contents}/>
+              ))}
+            </Message>
+          </Grid>
+        ) : (
+          ''
+        )}
+      </Form>
+    </RadGradSegment>
+  );
+};
+
+export default DumpStudentEmails;
--- app/imports/ui/pages/admin/AdminDatabaseDumpPage.tsx
+++ app/imports/ui/pages/admin/AdminDatabaseDumpPage.tsx
@@ -1,90 +1,21 @@
-import React, { useState } from 'react';
-import { Button, Form, Grid, Message } from 'semantic-ui-react';
-import moment from 'moment';
-import { ZipZap } from 'meteor/udondan:zipzap';
-import { dumpDatabaseMethod } from '../../../api/base/BaseCollection.methods';
-import { generateStudentEmailsMethod } from '../../../api/user/UserCollection.methods';
-import AdminDatabaseAccordion from '../../components/admin/database/AdminDatabaseAccordion';
-import UploadFixture from '../../components/admin/datamodel/UploadFixture';
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import DumpFixture from '../../components/admin/database/DumpFixture';
+import DumpStudentEmails from '../../components/admin/database/DumpStudentEmails';
+import UploadFixture from '../../components/admin/database/UploadFixture';
 import { PAGEIDS } from '../../utilities/PageIDs';
-import { useStickyState } from '../../utilities/StickyState';
 import PageLayout from '../PageLayout';
-
-interface Collection {
-  name?: string;
-  contents?: string[];
-}
 
 export const databaseFileDateFormat = 'YYYY-MM-DD-HH-mm-ss';
 
 const AdminDatabaseDumpPage: React.FC = () => {
-  const [isErrorState, setIsError] = useState(false);
-  const [resultsState, setResults] = useState([]);
-  const [dumpInProgress, setDumpInProgress] = useStickyState('AdminDatabaseDumpPage.dump', false);
-  const [emailsInProgress, setEmailsInProgress] = useStickyState('AdminDatabaseDumpPage.email', false);
-
-  const clickDump = () => {
-    setDumpInProgress(true);
-    dumpDatabaseMethod.call(null, (error, result) => {
-      if (error) {
-        setIsError(true);
-      }
-      setResults(result.collections);
-      const zip = new ZipZap();
-      const dir = 'radgrad-db';
-      const fileName = `${dir}/${moment(result.timestamp).format(databaseFileDateFormat)}.json`;
-      zip.file(fileName, JSON.stringify(result, null, 2));
-      zip.saveAs(`${dir}.zip`);
-      setDumpInProgress(false);
-    });
-  };
-
-  const clickEmails = () => {
-    setEmailsInProgress(true);
-    generateStudentEmailsMethod.call(null, (error, result) => {
-      if (error) {
-        setIsError(true);
-      }
-      // console.log(error, result);
-      const data: Collection = {};
-      data.name = 'Students';
-      data.contents = result.students;
-      setResults([data]);
-      const zip = new ZipZap();
-      const now = moment().format(databaseFileDateFormat);
-      const dir = `radgrad-students${now}`;
-      const fileName = `${dir}/Students.txt`;
-      zip.file(fileName, result.students.join('\n'));
-      zip.saveAs(`${dir}.zip`);
-      setEmailsInProgress(false);
-    });
-  };
-
-  const errorCondition = isErrorState;
-  const showMessage = resultsState.length > 0;
- 
   return (
-    <PageLayout id={PAGEIDS.DATABASE_DUMP_DATABASE} headerPaneTitle="Dump Database">
-      <Form>
-        <Button color="green" loading={dumpInProgress} basic type="submit" onClick={clickDump}>
-          Dump Database
-        </Button>
-        <Button color="green" loading={emailsInProgress} basic type="submit" onClick={clickEmails}>
-          Get Student Emails
-        </Button>
-      </Form>
-      {showMessage ? (
-        <Grid stackable style={{ paddingTop: '20px' }}>
-          <Message positive={!errorCondition} error={errorCondition}>
-            {resultsState.map((item, index) => (
-              <AdminDatabaseAccordion key={item.name} index={index} name={item.name} contents={item.contents}/>
-            ))}
-          </Message>
-        </Grid>
-      ) : (
-        ''
-      )}
-      <UploadFixture />
+    <PageLayout id={PAGEIDS.DATABASE_DUMP_DATABASE} headerPaneTitle="Database Management">
+      <Grid stackable columns='three'>
+        <Grid.Column><UploadFixture /></Grid.Column>
+        <Grid.Column><DumpFixture /></Grid.Column>
+        <Grid.Column><DumpStudentEmails /></Grid.Column>
+      </Grid>
     </PageLayout>
   );
 };
