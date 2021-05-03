8534bcee509e26f3bd181e507ceced72053b6a8b
Philip Johnson <johnson@hawaii.edu>
2021-05-03 13:52:07 -1000

    checkpoint

--- app/imports/ui/pages/admin/AdminAnalyticsLoggedInUsersPage.tsx
+++ app/imports/ui/pages/admin/AdminAnalyticsLoggedInUsersPage.tsx
@@ -1,20 +1,41 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
-import AdminAnalyticsLoggedInUsersWidget, { AdminAnalyticsLoggedInUsersWidgetProps } from '../../components/admin/analytics/AdminAnalyticsLoggedInUsersWidget';
+import { Grid, Label } from 'semantic-ui-react';
+import { Users } from '../../../api/user/UserCollection';
+import RadGradHeader from '../../components/shared/RadGradHeader';
+import RadGradSegment from '../../components/shared/RadGradSegment';
+import { COLORS } from '../../utilities/Colors';
 import { PAGEIDS } from '../../utilities/PageIDs';
 import PageLayout from '../PageLayout';
 
-/**
- * AdminAnalyticsLoggedInUsersPage.
- * @param {object[]} loggedInUsers
- * @return {JSX.Element}
- * @memberOf ui/pages/admin
- * @constructor
- */
-const AdminAnalyticsLoggedInUsersPage: React.FC<AdminAnalyticsLoggedInUsersWidgetProps> = ({ loggedInUsers }) => (
+const header = <RadGradHeader title='Logged in Users'/>;
+
+export interface AdminAnalyticsLoggedInUsersProps {
+  loggedInUsers: { status: { idle: boolean }; _id: string }[];
+}
+
+const AdminAnalyticsLoggedInUsersPage: React.FC<AdminAnalyticsLoggedInUsersProps> = ({ loggedInUsers }) => (
   <PageLayout id={PAGEIDS.ANALYTICS_LOGGED_IN_USERS} headerPaneTitle="Logged In Users">
-    <AdminAnalyticsLoggedInUsersWidget loggedInUsers={loggedInUsers}/>
+    <RadGradSegment header={header}>
+      <Grid padded stackable>
+        <Label.Group>
+          {loggedInUsers.map((user) => {
+            let color;
+            if (user.status.idle) {
+              color = COLORS.GREY;
+            } else {
+              color = COLORS.GREEN;
+            }
+            return (
+              <Label key={user._id} basic color={color}>
+                {Users.getFullName(user._id)}
+              </Label>
+            );
+          })}
+        </Label.Group>
+      </Grid>
+    </RadGradSegment>
   </PageLayout>
 );
 
