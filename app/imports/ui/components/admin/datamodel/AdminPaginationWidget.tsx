978f5c839bf1634cfb2ba1cf0cd894551df4bf7f
Philip Johnson <johnson@hawaii.edu>
2021-05-05 14:21:45 -1000

    checkpoint

--- app/imports/ui/components/admin/datamodel/AdminPaginationWidget.tsx
+++ app/imports/ui/components/admin/datamodel/AdminPaginationWidget.tsx
@@ -1,95 +1,51 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Button, Grid, Icon, Message } from 'semantic-ui-react';
 import BaseCollection from '../../../../api/base/BaseCollection';
-import { RootState } from '../../../../redux/types';
+import { useStickyState } from '../../../utilities/StickyState';
 
 interface AdminPaginationWidgetProps {
   collection: BaseCollection;
-  dispatch: any;
-  pagination: any;
-  setShowIndex: (collectionName: string, index: number) => any;
-  setShowCount: (collectionName: string, count: number) => any;
 }
 
-const mapStateToProps = (state: RootState) => ({
-  pagination: state.admin.dataModel.pagination,
-});
-
-const handleFirstClick = (dispatch, setShowIndex: (collectionName: string, index: number) => any, collection: BaseCollection) => (event) => {
-  event.preventDefault();
-  // console.log('handleFirstClick(%o) props=%o', event, props);
-  dispatch(setShowIndex(collection.getCollectionName(), 0));
-};
-
-const handlePrevClick = (pagination, collection, dispatch, setShowIndex) => (event) => {
-  event.preventDefault();
-  // console.log('handlePrevClick(%o) props=%o', event, props);
-  const showIndex = pagination[collection.getCollectionName()].showIndex;
-  const showCount = pagination[collection.getCollectionName()].showCount;
-  dispatch(setShowIndex(collection.getCollectionName(), showIndex - showCount));
-};
-
-const handleNextClick = (pagination, collection, dispatch, setShowIndex) => (event) => {
-  event.preventDefault();
-  // console.log('handleNextClick(%o) props=%o', event, props);
-  const showIndex = pagination[collection.getCollectionName()].showIndex;
-  const showCount = pagination[collection.getCollectionName()].showCount;
-  dispatch(setShowIndex(collection.getCollectionName(), showIndex + showCount));
-};
-
-const handleLastClick = (collection, pagination, dispatch, setShowIndex) => (event) => {
-  event.preventDefault();
-  // console.log('handleLastClick(%o) props=%o', event, props);
-  const count = collection.count();
-  const showCount = pagination[collection.getCollectionName()].showCount;
-  dispatch(setShowIndex(collection.getCollectionName(), count - showCount));
-};
-
-const handleCountChange = (dispatch, setShowCount, collection) => (event) => {
-  event.preventDefault();
-  // console.log('handleCountChange count=%o', event.target.value);
-  const count = parseInt(event.target.value, 10);
-  dispatch(setShowCount(collection.getCollectionName(), count));
-};
-
-const AdminPaginationWidget: React.FC<AdminPaginationWidgetProps> = ({ pagination, dispatch, collection, setShowCount, setShowIndex }) => {
+const AdminPaginationWidget: React.FC<AdminPaginationWidgetProps> = ({ collection }) => {
   // console.log('AdminPaginationWidget.render props=%o', props);
-  const heightStyle = {
-    height: 48,
-  };
-  const messageStyle = {
-    height: 48,
-    marginTop: 0,
-    marginRight: '0.25em',
-  };
-  const showCount = pagination[collection.getCollectionName()].showCount;
+  const heightStyle = { height: 48 };
+  const messageStyle = { height: 48, marginTop: 0, marginRight: '0.25em' };
+  const collectionName = collection.getCollectionName();
+  const [startIndex, setStartIndex] = useStickyState(`Pagination.${collectionName}.index`, 0);
+  const [showCount, setShowCount] = useStickyState(`Pagination.${collectionName}.count`, 25);
   const count = collection.count();
-  const startIndex = pagination[collection.getCollectionName()].showIndex;
-  let endIndex = startIndex + pagination[collection.getCollectionName()].showCount;
+  let endIndex = startIndex + showCount;
   if (endIndex > count) {
     endIndex = count;
   }
   const label = count <= showCount ? 'Showing all' : `${startIndex + 1} - ${endIndex} of ${count}`;
   const firstDisabled = startIndex < showCount;
   const lastDisabled = count - startIndex <= showCount;
+
+  const handleFirstClick = (event) => { event.preventDefault(); setStartIndex(0); };
+  const handlePrevClick = (event) => { event.preventDefault(); setStartIndex(startIndex - showCount); };
+  const handleNextClick = (event) => { event.preventDefault(); setStartIndex(startIndex + showCount); };
+  const handleLastClick = (event) => { event.preventDefault(); setStartIndex(count - showCount); };
+  const handleCountChange = (event) => {event.preventDefault(); setShowCount(parseInt(event.target.value, 10)); };
+
   return (
     <Grid.Row centered>
-      <Button basic color="green" onClick={handleFirstClick(dispatch, setShowIndex, collection)} style={heightStyle}>
+      <Button basic color="green" onClick={handleFirstClick} style={heightStyle}>
         <Icon name="fast backward" /> First
       </Button>
-      <Button basic color="green" disabled={firstDisabled} onClick={handlePrevClick(pagination, collection, dispatch, setShowIndex)} style={heightStyle}>
+      <Button basic color="green" disabled={firstDisabled} onClick={handlePrevClick} style={heightStyle}>
         <Icon name="step backward" /> Prev
       </Button>
       <Message style={messageStyle}>{label}</Message>
-      <Button basic color="green" disabled={lastDisabled} onClick={handleNextClick(pagination, collection, dispatch, setShowIndex)} style={heightStyle}>
+      <Button basic color="green" disabled={lastDisabled} onClick={handleNextClick} style={heightStyle}>
         <Icon name="step forward" /> Next
       </Button>
-      <Button basic color="green" onClick={handleLastClick(collection, pagination, dispatch, setShowIndex)} style={heightStyle}>
+      <Button basic color="green" onClick={handleLastClick} style={heightStyle}>
         <Icon name="fast forward" /> Last
       </Button>
       {/* <Dropdown selection={true} options={options} className="jsNum"/> */}
-      <select className="ui dropdown jsNum" style={heightStyle} value={showCount} onChange={handleCountChange(dispatch, setShowCount, collection)}>
+      <select className="ui dropdown jsNum" style={heightStyle} value={showCount} onChange={handleCountChange}>
         <option value={100}>100</option>
         <option value={50}>50</option>
         <option value={25}>25</option>
@@ -100,4 +56,4 @@ const AdminPaginationWidget: React.FC<AdminPaginationWidgetProps> = ({ paginatio
   );
 };
 
-export default connect(mapStateToProps)(AdminPaginationWidget);
+export default AdminPaginationWidget;
--- app/imports/ui/components/admin/datamodel/ListCollectionWidget.tsx
+++ app/imports/ui/components/admin/datamodel/ListCollectionWidget.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Grid, Header, Segment } from 'semantic-ui-react';
 import _ from 'lodash';
 import BaseCollection from '../../../../api/base/BaseCollection';
 import { DescriptionPair } from '../../../../typings/radgrad';
+import { useStickyState } from '../../../utilities/StickyState';
 import AdminCollectionAccordion from './AdminCollectionAccordion';
 import AdminPaginationWidget from './AdminPaginationWidget';
-import { dataModelActions } from '../../../../redux/admin/data-model';
-import { RootState } from '../../../../redux/types';
 
 interface ListCollectionWidgetProps {
   collection: BaseCollection;
@@ -17,27 +15,23 @@ interface ListCollectionWidgetProps {
   handleDelete: (evt: any, id: any) => any;
   items: any[];
   itemTitle: (item) => React.ReactNode;
-  pagination: any;
 }
 
-const mapStateToProps = (state: RootState) => ({
-  pagination: state.admin.dataModel.pagination,
-});
+const ListCollectionWidget: React.FC<ListCollectionWidgetProps> = ({ collection, items, findOptions, descriptionPairs, handleDelete, handleOpenUpdate, itemTitle }) => {
 
-const ListCollectionWidget: React.FC<ListCollectionWidgetProps> = ({ collection, pagination, items, findOptions, descriptionPairs, handleDelete, handleOpenUpdate, itemTitle }) => {
   const count = collection.count();
-  const startIndex = pagination[collection.getCollectionName()].showIndex;
-  const showCount = pagination[collection.getCollectionName()].showCount;
+  const collectionName = collection.getCollectionName();
+  const [startIndex] = useStickyState(`Pagination.${collectionName}.index`, 0);
+  const [showCount] = useStickyState(`Pagination.${collectionName}.count`, 25);
   const endIndex = startIndex + showCount;
   const itemsToShow = _.slice(items, startIndex, endIndex);
-  // console.log('startIndex=%o endIndex=%o items=%o', startIndex, endIndex, items);
   return (
     <Segment padded>
       <Header dividing>
         {collection.getCollectionName()}({count})
       </Header>
       <Grid>
-        <AdminPaginationWidget collection={collection} setShowIndex={dataModelActions.setCollectionShowIndex} setShowCount={dataModelActions.setCollectionShowCount} />
+        <AdminPaginationWidget collection={collection}  />
         {itemsToShow.map((item) => (
           <AdminCollectionAccordion
             key={item._id}
@@ -55,6 +49,4 @@ const ListCollectionWidget: React.FC<ListCollectionWidgetProps> = ({ collection,
   );
 };
 
-const ListCollectionWidgetCon = connect(mapStateToProps)(ListCollectionWidget);
-
-export default ListCollectionWidgetCon;
+export default ListCollectionWidget;
