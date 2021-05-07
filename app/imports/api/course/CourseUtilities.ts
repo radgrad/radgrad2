422227008c5e4aedf2b2002c402c12577f03893f
Cam Moore <cammymoore07@gmail.com>
2021-05-07 12:44:58 -1000

    Checkpoint

--- app/imports/api/course/CourseUtilities.ts
+++ app/imports/api/course/CourseUtilities.ts
@@ -44,6 +44,10 @@ export const clearPlannedCourseInstances = (studentID: string) => {
   });
 };
 
+export const courseInstanceIsRepeatable = (ci: CourseInstance): boolean => Meteor.settings.public.repeatableCourseNums.includes(ci.note);
+
+export const courseIsRepeatable = (course: Course): boolean => Meteor.settings.public.repeatableCourseNums.includes(course.num);
+
 export const get300LevelDocs = (): Course[] => Courses.find({ num: /3\d\d/ }).fetch();
 
 export const getStudent300LevelDocs = (studentID: string, coursesTakenSlugs: string[]) => {
@@ -166,12 +170,4 @@ export const getDepartment = (courseSlug: string): string => courseSlug.split('_
  */
 export const getCourseNumber = (courseSlug: string): string => courseSlug.split('_')[1];
 
-export const passedCourse = (ci: CourseInstance): boolean => {
-  const courseDoc = CourseInstances.getCourseDoc(ci._id);
-  const courseSlug = Slugs.getNameFromID(courseDoc.slugID);
-  // TODO: We need another way of representing 'passing'. This is going to change.
-  if (courseSlug.includes('111') || courseSlug.includes('141') || courseSlug.includes('211') || courseSlug.includes('241')) {
-    return _.includes(['B', 'B+', 'A-', 'A', 'A+'], ci.grade);
-  }
-  return _.includes(['C', 'C+', 'B-', 'B', 'B+', 'A-', 'A', 'A+'], ci.grade);
-};
+export const passedCourse = (ci: CourseInstance): boolean => _.includes(['C', 'C+', 'B-', 'B', 'B+', 'A-', 'A', 'A+'], ci.grade) && ci.verified;
