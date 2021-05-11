ad005d7fbf6e2eb39ef6a3c9120eb9ee9346f92e
Philip Johnson <johnson@hawaii.edu>
2021-05-11 10:22:57 -1000

    checkpoint

--- app/imports/ui/pages/onboard/philip/Task5.tsx
+++ app/imports/ui/pages/onboard/philip/Task5.tsx
@@ -1,11 +1,12 @@
 import { withTracker } from 'meteor/react-meteor-data';
-import React from 'react';
+import React, { useState } from 'react';
+import Markdown from 'react-markdown';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
+import { Header } from 'semantic-ui-react';
 import { AutoForm, SubmitField, SelectField } from 'uniforms-semantic';
 import { Interests } from '../../../../api/interest/InterestCollection';
 import { Interest } from '../../../../typings/radgrad';
-import MultiSelectField from '../../../components/form-fields/MultiSelectField';
 import RadGradHeader from '../../../components/shared/RadGradHeader';
 import RadGradSegment from '../../../components/shared/RadGradSegment';
 
@@ -14,22 +15,27 @@ interface Task5Props {
 }
 
 const Task5: React.FC<Task5Props> = ({ interests }) => {
-  const header = <RadGradHeader title="Task 5: A simple form" icon='file alternate outline' />;
+  const header = <RadGradHeader title="Task 5: Show me the description" icon='file alternate outline' />;
   const interestNames = interests.map(interest => interest.name);
   const schema = new SimpleSchema({
     interest: { type: String, allowedValues: interestNames },
   });
   const formSchema = new SimpleSchema2Bridge(schema);
-  const onSubmit = ({ interest }) => {
-
-  }
+  const [description, setDescription] = useState('');
+  const onSubmit = ({ interest }) => { setDescription(Interests.findDoc(interest).description); };
 
   return (
     <RadGradSegment header={header}>
       <AutoForm schema={formSchema} onSubmit={onSubmit} >
         <SelectField name="interest" placeholder="(Select interest)" showInlineError/>
-        <SubmitField className="mini basic green" value="Select Interest" />
+        <SubmitField className="mini basic green" value="Display Description" />
       </AutoForm>
+      {description &&
+        <div style={{ paddingTop: '20px' }}>
+          <Header>Description</Header>
+          <Markdown source={description}/>
+        </div>
+      }
     </RadGradSegment>
   );
 };
