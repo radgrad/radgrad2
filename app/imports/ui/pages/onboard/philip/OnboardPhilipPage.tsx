9d4db45fd9b7264e012cf0b4c844e5ce52bb1389
Philip Johnson <johnson@hawaii.edu>
2021-05-11 09:32:07 -1000

    checkpoint

--- app/imports/ui/pages/onboard/philip/OnboardPhilipPage.tsx
+++ app/imports/ui/pages/onboard/philip/OnboardPhilipPage.tsx
@@ -5,6 +5,7 @@ import Task1 from './Task1';
 import Task2 from './Task2';
 import Task3 from './Task3';
 import Task4 from './Task4';
+import Task5 from './Task5';
 
 const headerPaneTitle = "Philip's Onboarding Sandbox";
 const headerPaneBody = 'Page for display of onboarding component development practice';
@@ -13,6 +14,9 @@ const headerPaneImage = 'header-onboarding.png';
 const OnboardPhilipPage: React.FC = () => (
   <PageLayout id="sandbox-onboard-philip" headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody} headerPaneImage={headerPaneImage}>
     <Grid columns='equal'>
+      <Grid.Row>
+        <Grid.Column><Task5 /></Grid.Column>
+      </Grid.Row>
       <Grid.Row>
         <Grid.Column><Task4 /></Grid.Column>
       </Grid.Row>
