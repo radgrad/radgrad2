cb711a48f4e9cd19b97f6c7ded958659428cbc9f
Philip Johnson <johnson@hawaii.edu>
2021-05-10 14:54:43 -1000

    checkpoint

--- app/imports/ui/pages/onboard/philip/Task4.tsx
+++ app/imports/ui/pages/onboard/philip/Task4.tsx
@@ -1,7 +1,7 @@
 import { withTracker } from 'meteor/react-meteor-data';
 import React from 'react';
 import { useParams } from 'react-router';
-import { Header, Label } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 import { CareerGoals } from '../../../../api/career/CareerGoalCollection';
 import { Courses } from '../../../../api/course/CourseCollection';
 import { Interests } from '../../../../api/interest/InterestCollection';
@@ -23,43 +23,41 @@ interface Task4Props {
   courses: Course[];
   opportunities: Opportunity[];
   students: StudentProfile[];
-  userID: string;
 }
 
-const Task4: React.FC<Task4Props> = ({ careerGoals, interests, courses, opportunities, students, userID }) => {
+const Task4: React.FC<Task4Props> = ({ careerGoals, interests, courses, opportunities, students }) => {
+  const { username } = useParams();
+  const profile = Users.getProfile(username) as BaseProfile;
+  const userID = profile.userID;
   const header = <RadGradHeader title="Task 4: Labels" icon='tags' />;
   const careerGoalSlugs = careerGoals.map(careerGoal => CareerGoals.findSlugByID(careerGoal._id));
   const courseSlugs = courses.map(course => Courses.findSlugByID(course._id));
   const interestSlugs = interests.map(course => Interests.findSlugByID(course._id));
   const opportunitySlugs = opportunities.map(course => Opportunities.findSlugByID(course._id));
-  const studentUsernames = students.map(profile => profile.username);
+  const studentUsernames = students.map(student => student.username);
   return (
     <RadGradSegment header={header}>
       <Header>Career Goals</Header>
-      {careerGoalSlugs.map(slug => <CareerGoalLabel slug={slug} userID={userID}  size='tiny'/>)}
+      {careerGoalSlugs.map(slug => <CareerGoalLabel key={slug} slug={slug} userID={userID}  size='tiny'/>)}
       <Header>Courses</Header>
-      {courseSlugs.map(slug => <CourseLabel slug={slug} userID={userID}  size='tiny'/>)}
+      {courseSlugs.map(slug => <CourseLabel key={slug} slug={slug} userID={userID}  size='tiny'/>)}
       <Header>Interests</Header>
-      {interestSlugs.map(slug => <InterestLabel slug={slug} userID={userID}  size='tiny'/>)}
+      {interestSlugs.map(slug => <InterestLabel key={slug} slug={slug} userID={userID}  size='tiny'/>)}
       <Header>Opportunities</Header>
-      {opportunitySlugs.map(slug => <OpportunityLabel slug={slug} userID={userID}  size='tiny'/>)}
+      {opportunitySlugs.map(slug => <OpportunityLabel key={slug} slug={slug} userID={userID}  size='tiny'/>)}
       <Header>Students</Header>
-      {studentUsernames.map(username => <UserLabel username={username} size='tiny'/>)}
-
+      {studentUsernames.map(user => <UserLabel key={user} username={user} size='tiny'/>)}
     </RadGradSegment>
   );
 };
 
 const Task4Container = withTracker(() => {
-  const { username } = useParams();
-  const profile = Users.getProfile(username) as BaseProfile;
-  const userID = profile.userID;
   const careerGoals = CareerGoals.findNonRetired();
   const interests = Interests.findNonRetired();
   const courses = Courses.findNonRetired();
   const opportunities = Opportunities.findNonRetired();
   const students = StudentProfiles.findNonRetired();
-  return { careerGoals, interests, courses, opportunities, students, userID };
+  return { careerGoals, interests, courses, opportunities, students };
 })(Task4);
 
 export default Task4Container;
