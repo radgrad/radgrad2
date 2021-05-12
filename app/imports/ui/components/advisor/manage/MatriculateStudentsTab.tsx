e9229da1198915baf381626638911ceace53f11b
Cam Moore <cammymoore07@gmail.com>
2021-05-12 13:04:24 -1000

    Update MatriculateStudentsTab.tsx
    Trying to build a form.

--- app/imports/ui/components/advisor/manage/MatriculateStudentsTab.tsx
+++ app/imports/ui/components/advisor/manage/MatriculateStudentsTab.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Tab } from 'semantic-ui-react';
+import SimpleSchema from 'simpl-schema';
+import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
+import { AutoFields, AutoForm, ErrorsField, SubmitField } from 'uniforms-semantic';
 import { StudentProfile } from '../../../../typings/radgrad';
+import RadGradHeader from '../../shared/RadGradHeader';
 
 interface MatriculateStudentsTabProps {
   students: StudentProfile[];
@@ -11,9 +15,21 @@ const MatriculateStudentsTab: React.FC<MatriculateStudentsTabProps> = ({ student
   const retiredStudents = students.filter((s) => s.retired);
   const retiredAlumni = alumni.filter((a) => a.retired);
   const retired = retiredAlumni.concat(retiredStudents);
+  const tempSchema = {};
+  retired.forEach((r) => {
+    tempSchema[r._id] = { type: Boolean, label: r.username, optional: true };
+  });
+  console.log(tempSchema);
+  const schema = new SimpleSchema(tempSchema);
+  const formSchema = new SimpleSchema2Bridge(schema);
   return (
     <Tab.Pane>
-      {retired.map((r) => (<p>{r.username}</p>))}
+      <AutoForm schema={formSchema} onSubmit={console.log} >
+        <RadGradHeader title='Matriculate Retired Students' />
+        <AutoFields />
+        <ErrorsField />
+        <SubmitField />
+      </AutoForm>
     </Tab.Pane>
   );
 };
