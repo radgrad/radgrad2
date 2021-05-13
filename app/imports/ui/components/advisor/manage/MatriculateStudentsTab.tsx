39f91e90e962159ca88bc75aa2d09615874cdeb0
Cam Moore <cammymoore07@gmail.com>
2021-05-12 14:30:03 -1000

    Added confirm to Matriculate students.

--- app/imports/ui/components/advisor/manage/MatriculateStudentsTab.tsx
+++ app/imports/ui/components/advisor/manage/MatriculateStudentsTab.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Tab } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Confirm, Message, Tab } from 'semantic-ui-react';
 import SimpleSchema from 'simpl-schema';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
 import { AutoFields, AutoForm, ErrorsField, SubmitField } from 'uniforms-semantic';
+import { matriculateStudentMethod } from '../../../../api/user/StudentProfileCollection.methods';
 import { StudentProfile } from '../../../../typings/radgrad';
 import RadGradHeader from '../../shared/RadGradHeader';
 
@@ -17,19 +18,33 @@ const MatriculateStudentsTab: React.FC<MatriculateStudentsTabProps> = ({ student
   const retired = retiredAlumni.concat(retiredStudents);
   const tempSchema = {};
   retired.forEach((r) => {
-    tempSchema[r._id] = { type: Boolean, label: r.username, optional: true };
+    tempSchema[r.userID] = { type: Boolean, label: r.username, optional: true };
   });
-  console.log(tempSchema);
+  const [data, setData] = useState({});
+  const [openConfirm, setOpenConfirm] = useState(false);
   const schema = new SimpleSchema(tempSchema);
   const formSchema = new SimpleSchema2Bridge(schema);
+  const handleSubmit = () => {
+    setOpenConfirm(false);
+    for (const [studentID] of Object.entries(data)) {
+      matriculateStudentMethod.callPromise(studentID)
+        .then(() => console.log(`Removed student ${studentID}`))
+        .catch((error) => console.error(error));
+    }
+  };
   return (
     <Tab.Pane>
-      <AutoForm schema={formSchema} onSubmit={console.log} >
+      <Message info>You should dump the database before matriculating the students. This will delete their records.</Message>
+      <AutoForm schema={formSchema} onSubmit={(model) => {
+        setData(model);
+        setOpenConfirm(true);
+      }} >
         <RadGradHeader title='Matriculate Retired Students' />
         <AutoFields />
         <ErrorsField />
         <SubmitField />
       </AutoForm>
+      <Confirm onConfirm={handleSubmit} onCancel={() => setOpenConfirm(false)} open={openConfirm} content='Are you sure you want to matriculate these students?'/>
     </Tab.Pane>
   );
 };
