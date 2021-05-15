f7aa9f99b6cd18cea67e4b124974875972e590f6
Cam Moore <cammymoore07@gmail.com>
2021-05-15 08:06:18 -1000

    Update EditCareerGoalButton.tsx
    handleSubmit working.

--- app/imports/ui/components/shared/manage/career-goal/EditCareerGoalButton.tsx
+++ app/imports/ui/components/shared/manage/career-goal/EditCareerGoalButton.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { Button, Modal } from 'semantic-ui-react';
 import SimpleSchema from 'simpl-schema';
+import Swal from 'sweetalert2';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { AutoForm, BoolField, ErrorsField, LongTextField, SubmitField, TextField } from 'uniforms-semantic';
+import { updateMethod } from '../../../../../api/base/BaseCollection.methods';
+import { CareerGoals } from '../../../../../api/career/CareerGoalCollection';
 import { Interests } from '../../../../../api/interest/InterestCollection';
 import { CareerGoal, CareerGoalUpdate, Interest } from '../../../../../typings/radgrad';
 import MultiSelectField from '../../../form-fields/MultiSelectField';
@@ -17,10 +20,34 @@ const EditCareerGoalButton: React.FC<EditCareerGoalButtonProps> = ({ careerGoal,
   const interestNames = interests.map((interest) => interest.name);
 
   const model: CareerGoalUpdate = careerGoal;
+  // convert the interestIDs to names for the form.
   model.interests = careerGoal.interestIDs.map((id) => Interests.findDoc(id).name);
 
   const handleSubmit = (doc) => {
-    console.log('handleSubmit', doc);
+    // console.log('handleSubmit', doc);
+    const collectionName = CareerGoals.getCollectionName();
+    const updateData: CareerGoalUpdate = doc;
+    updateData.id = doc._id;
+    // convert the interest names to IDs for the update
+    updateData.interests = doc.interests.map((name) => Interests.findDoc(name)._id);
+    // console.log(collectionName, updateData);
+    updateMethod.callPromise({ collectionName, updateData })
+      .then((result) => Swal.fire({
+        title: 'Interest Updated',
+        icon: 'success',
+        text: 'Successfully updated interest.',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false,
+        showConfirmButton: false,
+        timer: 1500,
+      }))
+      .catch((error) => Swal.fire({
+        title: 'Update Failed',
+        text: error.message,
+        icon: 'error',
+        // timer: 1500,
+      }));
   };
 
   const updateSchema = new SimpleSchema({
