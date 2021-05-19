ed65dfe064f69648cface79aa20a89742f4f1129
Cam Moore <cammymoore07@gmail.com>
2021-05-19 08:19:07 -1000

    Update demo-fixture-generator.ts

--- scripts/ts/demo-fixture-generator.ts
+++ scripts/ts/demo-fixture-generator.ts
@@ -1,16 +1,64 @@
 import { program } from 'commander';
 import * as fs from 'fs';
+import moment from 'moment';
+import _ from 'lodash';
 import { getCollectionData } from './data-dump-utils';
+
+const isQuarterSystem = (academicTerms) => {
+  const termNames = [];
+  academicTerms.map((t) => termNames.push(t.term));
+  const uniq = _.uniq(termNames);
+  return uniq.length % 4 === 0;
+};
+
+const getCurrentTerm = (academicTerms) => {
+  const isQuarter = isQuarterSystem(academicTerms);
+  let fallStart;
+  let springStart;
+  let summerStart;
+  if (isQuarter) {
+    fallStart = parseInt(moment('09-26-2015', 'MM-DD-YYYY').format('DDD'), 10);
+    springStart = parseInt(moment('04-01-2015', 'MM-DD-YYYY').format('DDD'), 10);
+    summerStart = parseInt(moment('06-20-2015', 'MM-DD-YYYY').format('DDD'), 10);
+  } else {
+    fallStart = parseInt(moment('08-15-2015', 'MM-DD-YYYY').format('DDD'), 10);
+    springStart = parseInt(moment('01-01-2015', 'MM-DD-YYYY').format('DDD'), 10);
+    summerStart = parseInt(moment('05-15-2015', 'MM-DD-YYYY').format('DDD'), 10);
+  }
+  const year = moment().year();
+  const day = moment().dayOfYear();
+  let term = '';
+  if (isQuarter) {
+    if (day >= fallStart) {
+      term = 'FALL';
+    } else if (day >= summerStart) {
+      term = 'SUMMER';
+    } else if (day >= springStart) {
+      term = 'SPRING';
+    } else {
+      term = 'WINTER';
+    }
+  } else if (day >= fallStart) {
+    term = 'FALL';
+  } else if (day >= summerStart) {
+    term = 'SUMMER';
+  } else {
+    term = 'SPRING';
+  }
+  return `${term} ${year}`;
+};
 
 async function generateDemoFixture(radgradDumpFile, userConfigFile) {
   console.log('generateDemoFixture', radgradDumpFile, userConfigFile);
   const data = fs.readFileSync(radgradDumpFile);
   const radgradDump = JSON.parse(data.toString());
+  const academicTerms = getCollectionData(radgradDump, 'AcademicTermCollection');
   const careerGoals = getCollectionData(radgradDump, 'CareerGoalCollection');
   const courses = getCollectionData(radgradDump, 'CourseCollection');
   const interests = getCollectionData(radgradDump, 'InterestCollection');
   const oppotunities = getCollectionData(radgradDump, 'OpportunityCollection');
   console.log(careerGoals.length, courses.length, interests.length, oppotunities.length);
+  console.log(getCurrentTerm(academicTerms));
 }
 
 program
