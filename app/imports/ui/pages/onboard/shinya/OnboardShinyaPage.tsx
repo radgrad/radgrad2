cb496f405979978b7e86230841b1501adcaa9ab9
Shun <47809066+saitoshi@users.noreply.github.com>
2021-05-21 03:52:29 +0900

    Update OnboardShinyaPage.tsx

--- app/imports/ui/pages/onboard/shinya/OnboardShinyaPage.tsx
+++ app/imports/ui/pages/onboard/shinya/OnboardShinyaPage.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { PAGEIDS } from '../../../utilities/PageIDs';
 import PageLayout from '../../PageLayout';
+import RadGradSegment from '../../../components/shared/RadGradSegment';
+import RadGradHeader from '../../../components/shared/RadGradHeader';
+
 
 const headerPaneTitle = "Shinya's Onboarding Sandbox";
 const headerPaneBody = 'Page for display of onboarding component development practice';
 const headerPaneImage = 'header-onboarding.png';
+const task1Header = <RadGradHeader title="TASK 1: HELLO WORLD" icon = "globe america"/>;
 
 const OnboardShinyaPage: React.FC = () => (
   <PageLayout id={PAGEIDS.ONBOARD_SHINYA} headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody} headerPaneImage={headerPaneImage}>
-    Components go here.
+    <RadGradSegment header= {task1Header}>
+        Hello World
+    </RadGradSegment>
   </PageLayout>
 );
 
