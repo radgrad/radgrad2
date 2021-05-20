c3b9a550285a4d296f4d5bd71e68ac1981ec2fa0
Shun <47809066+saitoshi@users.noreply.github.com>
2021-05-21 08:39:18 +0900

    task2

--- app/imports/ui/pages/onboard/shinya/OnboardShinyaPage.tsx
+++ app/imports/ui/pages/onboard/shinya/OnboardShinyaPage.tsx
@@ -1,21 +1,41 @@
 import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Grid } from 'semantic-ui-react';
+import { useParams } from 'react-router';
 import { PAGEIDS } from '../../../utilities/PageIDs';
 import PageLayout from '../../PageLayout';
 import RadGradSegment from '../../../components/shared/RadGradSegment';
 import RadGradHeader from '../../../components/shared/RadGradHeader';
-
+import Task2Segment, { Task2SegmentProps } from './Task2Segment';
+import { Users } from '../../../../api/user/UserCollection';
 
 const headerPaneTitle = "Shinya's Onboarding Sandbox";
 const headerPaneBody = 'Page for display of onboarding component development practice';
 const headerPaneImage = 'header-onboarding.png';
 const task1Header = <RadGradHeader title="TASK 1: HELLO WORLD" icon = "globe america"/>;
 
-const OnboardShinyaPage: React.FC = () => (
+const OnboardShinyaPage: React.FC<Task2SegmentProps> = ({ loggedInUser, urlUser }) => (
   <PageLayout id={PAGEIDS.ONBOARD_SHINYA} headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody} headerPaneImage={headerPaneImage}>
-    <RadGradSegment header= {task1Header}>
-        Hello World
-    </RadGradSegment>
+    <Grid columns={2}>
+        <Grid.Column>
+            <RadGradSegment header= {task1Header}>
+                Hello World
+            </RadGradSegment>
+        </Grid.Column>
+        <Grid.Column>
+            <Task2Segment loggedInUser={loggedInUser} urlUser={urlUser}/>
+        </Grid.Column>
+    </Grid>
   </PageLayout>
 );
 
-export default OnboardShinyaPage;
+export default withTracker(() => {
+  const loggedInUser = Meteor.user() ? Meteor.user().username : '';
+  const { username } = useParams();
+  const urlUser = Users.getProfile(username).username;
+  return {
+    loggedInUser,
+    urlUser,
+  };
+})(OnboardShinyaPage);
+// export default OnboardShinyaPage;
---
+++ app/imports/ui/pages/onboard/shinya/Task2Segment.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import RadGradHeader from '../../../components/shared/RadGradHeader';
+import RadGradSegment from '../../../components/shared/RadGradSegment';
+
+export interface Task2SegmentProps {
+  loggedInUser: string;
+  urlUser: string;
+}
+
+const Task2Segment: React.FC<Task2SegmentProps> = ({ loggedInUser, urlUser }) => {
+  const task2Header = <RadGradHeader title="TASK 2:  WHO IS THE USER?" icon = "user graduate" dividing/>;
+  return (
+      <RadGradSegment header={task2Header}>
+          <p>The currently logged in user is {`${loggedInUser}`}.</p>
+          <p>The user in the URL is {`${urlUser}`}.</p>
+      </RadGradSegment>
+  );
+};
+
+export default Task2Segment;
