afd83242e30d638a9e24a879473144543be9a881
Cam Moore <cammymoore07@gmail.com>
2021-05-20 09:20:30 -1000

    Academic term functions

---
+++ scripts/ts/fixture-generator/academic-term-utilities.ts
@@ -0,0 +1,130 @@
+import _ from 'lodash';
+import moment from 'moment';
+
+enum AcademicTerms {
+  FALL = 'FALL',
+  WINTER = 'WINTER',
+  SPRING = 'SPRING',
+  SUMMER = 'SUMMER',
+}
+
+export const isQuarterSystem = (academicTerms) => {
+  const termNames = [];
+  academicTerms.map((t) => termNames.push(t.term));
+  const uniq = _.uniq(termNames);
+  return uniq.length % 4 === 0;
+};
+
+export const getTerm = (academicTerm: string): string => academicTerm.split(' ')[0];
+
+export const getYear = (academicTerm: string): number => parseInt(academicTerm.split(' ')[1], 10);
+
+export const getCurrentTerm = (academicTerms) => {
+  const isQuarter = isQuarterSystem(academicTerms);
+  let fallStart;
+  let springStart;
+  let summerStart;
+  if (isQuarter) {
+    fallStart = parseInt(moment('09-26-2015', 'MM-DD-YYYY').format('DDD'), 10);
+    springStart = parseInt(moment('04-01-2015', 'MM-DD-YYYY').format('DDD'), 10);
+    summerStart = parseInt(moment('06-20-2015', 'MM-DD-YYYY').format('DDD'), 10);
+  } else {
+    fallStart = parseInt(moment('08-15-2015', 'MM-DD-YYYY').format('DDD'), 10);
+    springStart = parseInt(moment('01-01-2015', 'MM-DD-YYYY').format('DDD'), 10);
+    summerStart = parseInt(moment('05-15-2015', 'MM-DD-YYYY').format('DDD'), 10);
+  }
+  const year = moment().year();
+  const day = moment().dayOfYear();
+  let term = '';
+  if (isQuarter) {
+    if (day >= fallStart) {
+      term = AcademicTerms.FALL;
+    } else if (day >= summerStart) {
+      term = AcademicTerms.SUMMER;
+    } else if (day >= springStart) {
+      term = AcademicTerms.SPRING;
+    } else {
+      term = AcademicTerms.WINTER;
+    }
+  } else if (day >= fallStart) {
+    term = AcademicTerms.FALL;
+  } else if (day >= summerStart) {
+    term = AcademicTerms.SUMMER;
+  } else {
+    term = AcademicTerms.SPRING;
+  }
+  return `${term} ${year}`;
+};
+
+export const nextAcademicTerm = (academicTerm: string, quarters: boolean): string => {
+  const currentTerm = getTerm(academicTerm);
+  const currentYear = getYear(academicTerm);
+  let term;
+  let year = currentYear;
+  switch (currentTerm) {
+    case AcademicTerms.FALL: {
+      if (quarters) {
+        term = AcademicTerms.WINTER;
+      } else {
+        term = AcademicTerms.SPRING;
+        year += 1;
+      }
+      break;
+    }
+    case AcademicTerms.WINTER: {
+      term = AcademicTerms.SPRING;
+      year += 1;
+      break;
+    }
+    case AcademicTerms.SPRING: {
+      term = AcademicTerms.SUMMER;
+      break;
+    }
+    default:
+      term = AcademicTerms.FALL;
+  }
+  return `${term} ${year}`;
+};
+
+export const nextNonSummerTerm = (academicTerm: string, quarters: boolean): string => {
+  const next = nextAcademicTerm(academicTerm, quarters);
+  if (getTerm(next) === AcademicTerms.SUMMER) {
+    return nextAcademicTerm(next, quarters);
+  }
+  return next;
+};
+
+export const prevAcademicTerm = (academicTerm: string, quaters: boolean): string => {
+  const currentTerm = getTerm(academicTerm);
+  const currentYear = getYear(academicTerm);
+  let term;
+  let year = currentYear;
+  switch (currentTerm) {
+    case AcademicTerms.SPRING: {
+      if (quaters) {
+        term = AcademicTerms.WINTER;
+      } else {
+        term = AcademicTerms.FALL;
+      }
+      year -= 1;
+      break;
+    }
+    case AcademicTerms.WINTER:
+      term = AcademicTerms.FALL;
+      break;
+    case AcademicTerms.FALL:
+      term = AcademicTerms.SUMMER;
+      break;
+    default:
+      term = AcademicTerms.SPRING;
+  }
+  return `${term} ${year}`;
+};
+
+export const prevNonSummerTerm = (academicTerm: string, quarters: boolean): string => {
+  const prev = prevAcademicTerm(academicTerm, quarters);
+  if (getTerm(prev) === AcademicTerms.SUMMER) {
+    return prevAcademicTerm(prev, quarters);
+  }
+  return prev;
+};
---
+++ scripts/ts/fixture-generator/demo-fixture-generator.ts
@@ -0,0 +1,31 @@
+import { program } from 'commander';
+import * as fs from 'fs';
+import { getCollectionData } from '../data-dump-utils';
+import { getCurrentTerm, isQuarterSystem, nextAcademicTerm, prevAcademicTerm, prevNonSummerTerm } from './academic-term-utilities';
+
+async function generateDemoFixture(radgradDumpFile, userConfigFile) {
+  console.log('generateDemoFixture', radgradDumpFile, userConfigFile);
+  const data = fs.readFileSync(radgradDumpFile);
+  const radgradDump = JSON.parse(data.toString());
+  const academicTerms = getCollectionData(radgradDump, 'AcademicTermCollection');
+  const careerGoals = getCollectionData(radgradDump, 'CareerGoalCollection');
+  const courses = getCollectionData(radgradDump, 'CourseCollection');
+  const interests = getCollectionData(radgradDump, 'InterestCollection');
+  const opportunities = getCollectionData(radgradDump, 'OpportunityCollection');
+  console.log(careerGoals.length, courses.length, interests.length, opportunities.length);
+  const quarters = isQuarterSystem(academicTerms);
+  const currentTerm = getCurrentTerm(academicTerms);
+  const nextTerm = nextAcademicTerm(currentTerm, quarters);
+  console.log(currentTerm, nextTerm, nextAcademicTerm(nextTerm, quarters));
+  console.log(nextTerm, prevAcademicTerm(nextTerm, quarters), prevNonSummerTerm(nextTerm, quarters));
+}
+
+program
+  .arguments('<radgradDumpFile> <userConfigFile>')
+  .description('Creates a demo fixture file based upon the RadGrad dump file and a user config file.', {
+    radgradDumpFile: 'A pre-existing RadGrad2 database dump file',
+    userConfigFile: 'A pre-existing user configuration file',
+  })
+  .action((radgradDumpFile, userConfigFile) => generateDemoFixture(radgradDumpFile, userConfigFile));
+
+program.parse(process.argv);
