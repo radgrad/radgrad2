340cef02a4021d49e4487c7bd978f324a30101b5
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2021-05-21 09:01:37 -1000

    Update OnboardCalianaPage.tsx

--- app/imports/ui/pages/onboard/caliana/OnboardCalianaPage.tsx
+++ app/imports/ui/pages/onboard/caliana/OnboardCalianaPage.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { PAGEIDS } from '../../../utilities/PageIDs';
 import PageLayout from '../../PageLayout';
+import RadGradHeader from '../../../components/shared/RadGradHeader';
+import RadGradSegment from '../../../components/shared/RadGradSegment';
 
 const headerPaneTitle = "Caliana's Onboarding Sandbox";
 const headerPaneBody = 'Page for display of onboarding component development practice';
 const headerPaneImage = 'header-onboarding.png';
+const headerForTask1 = <RadGradHeader title='TASK 1: HELLO WORLD' icon='globe americas icon' />;
 
 const OnboardCalianaPage: React.FC = () => (
+    
   <PageLayout id={PAGEIDS.ONBOARD_CALIANA} headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody} headerPaneImage={headerPaneImage}>
-    Components go here.
+      <RadGradSegment header={headerForTask1}>
+          Hello World
+      </RadGradSegment>
   </PageLayout>
 );
 
