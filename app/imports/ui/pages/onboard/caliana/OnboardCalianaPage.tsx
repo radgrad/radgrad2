fe0bd9cfc66ea120849108825ac316bb34dca8d9
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2021-05-21 09:29:36 -1000

    Update OnboardCalianaPage.tsx
    Fixed some lint errors and name formats

--- app/imports/ui/pages/onboard/caliana/OnboardCalianaPage.tsx
+++ app/imports/ui/pages/onboard/caliana/OnboardCalianaPage.tsx
@@ -7,15 +7,17 @@ import RadGradSegment from '../../../components/shared/RadGradSegment';
 const headerPaneTitle = "Caliana's Onboarding Sandbox";
 const headerPaneBody = 'Page for display of onboarding component development practice';
 const headerPaneImage = 'header-onboarding.png';
-const headerForTask1 = <RadGradHeader title='TASK 1: HELLO WORLD' icon='globe americas icon' />;
+
+const headerTask1 = <RadGradHeader title='TASK 1: HELLO WORLD' icon='globe americas icon'/>;
 
 const OnboardCalianaPage: React.FC = () => (
-    
-  <PageLayout id={PAGEIDS.ONBOARD_CALIANA} headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody} headerPaneImage={headerPaneImage}>
-      <RadGradSegment header={headerForTask1}>
-          Hello World
-      </RadGradSegment>
-  </PageLayout>
+
+    <PageLayout id={PAGEIDS.ONBOARD_CALIANA} headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody}
+                headerPaneImage={headerPaneImage}>
+        <RadGradSegment header={headerTask1}>
+            Hello World
+        </RadGradSegment>
+    </PageLayout>
 );
 
 export default OnboardCalianaPage;
