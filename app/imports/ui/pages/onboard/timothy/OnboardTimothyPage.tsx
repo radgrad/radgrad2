120d390c6708350c48e16a4243c54997a8fbe8ff
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-05-21 08:41:53 -1000

    Task 1 Completed

--- app/imports/ui/pages/onboard/timothy/OnboardTimothyPage.tsx
+++ app/imports/ui/pages/onboard/timothy/OnboardTimothyPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { PAGEIDS } from '../../../utilities/PageIDs';
 import PageLayout from '../../PageLayout';
+import RadGradSegment from '../../../components/shared/RadGradSegment';
+import RadGradHeader from '../../../components/shared/RadGradHeader';
 
 const headerPaneTitle = "Timothy's Onboarding Sandbox";
 const headerPaneBody = 'Page for display of onboarding component development practice';
@@ -8,7 +10,9 @@ const headerPaneImage = 'header-onboarding.png';
 
 const OnboardTimothyPage: React.FC = () => (
   <PageLayout id={PAGEIDS.ONBOARD_SHINYA} headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody} headerPaneImage={headerPaneImage}>
-    Components go here.
+   <RadGradSegment header={<RadGradHeader title='TASK 1: HELLO WORLD' icon='globe americas'/>}>
+   Hello World
+   </RadGradSegment>
   </PageLayout>
 );
 
