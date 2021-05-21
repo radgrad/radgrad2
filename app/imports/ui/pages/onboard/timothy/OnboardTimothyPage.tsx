b4f6eb5c4dc0d31e423f4acd64b913e8759dd7f1
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-05-21 11:29:28 -1000

    Task2 Completed.

--- app/imports/ui/pages/onboard/timothy/OnboardTimothyPage.tsx
+++ app/imports/ui/pages/onboard/timothy/OnboardTimothyPage.tsx
@@ -1,5 +1,9 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { withTracker } from 'meteor/react-meteor-data';
 import { PAGEIDS } from '../../../utilities/PageIDs';
+import { Users } from '../../../../api/user/UserCollection';
 import PageLayout from '../../PageLayout';
 import RadGradSegment from '../../../components/shared/RadGradSegment';
 import RadGradHeader from '../../../components/shared/RadGradHeader';
@@ -8,12 +12,41 @@ const headerPaneTitle = "Timothy's Onboarding Sandbox";
 const headerPaneBody = 'Page for display of onboarding component development practice';
 const headerPaneImage = 'header-onboarding.png';
 
-const OnboardTimothyPage: React.FC = () => (
-  <PageLayout id={PAGEIDS.ONBOARD_SHINYA} headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody} headerPaneImage={headerPaneImage}>
-   <RadGradSegment header={<RadGradHeader title='TASK 1: HELLO WORLD' icon='globe americas'/>}>
-   Hello World
-   </RadGradSegment>
-  </PageLayout>
+interface OnBoardVar {
+  user: string;
+  urlName: string;
+}
+
+const OnboardTimothyPage: React.FC<OnBoardVar> = ({ user, urlName }) => (
+    <PageLayout id={PAGEIDS.ONBOARD_SHINYA} headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody}
+                headerPaneImage={headerPaneImage}>
+        <div className='ui two column grid'>
+            <div className='column'>
+                <RadGradSegment header={<RadGradHeader title='TASK 1: HELLO WORLD' icon='globe americas'/>}>
+                    Hello World
+                </RadGradSegment>
+            </div>
+            <div className='column'>
+                <RadGradSegment header={<RadGradHeader title='TASK 2: WHO IS THE USER?' icon='user graduate'/>}>
+                    The current logged in user is: {user}
+                    <br/>
+                    <br/>
+                    The username appearing in the URL is: {urlName}
+                    <br/>
+                    <br/>
+                    In RadGrad, these are not necessarily the same!
+                </RadGradSegment>
+            </div>
+        </div>
+    </PageLayout>
 );
 
-export default OnboardTimothyPage;
+export default withTracker(() => {
+  const user = Meteor.user() ? Meteor.user().username : '';
+  const { username } = useParams();
+  const urlName = Users.getProfile(username).username;
+  return {
+    user,
+    urlName,
+  };
+})(OnboardTimothyPage);
