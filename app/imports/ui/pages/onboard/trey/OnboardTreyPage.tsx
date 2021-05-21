227c0bb6dc1fc20e8e5037684df01eb95b9534fc
trey-sumida <tasumida@hawaii.edu>
2021-05-21 09:39:56 -1000

    refactored files to turn each task into a separate component

--- app/imports/ui/pages/onboard/trey/OnboardTreyPage.tsx
+++ app/imports/ui/pages/onboard/trey/OnboardTreyPage.tsx
@@ -1,34 +1,28 @@
-import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import { Grid } from 'semantic-ui-react';
-import RadGradSegment from '../../../components/shared/RadGradSegment';
-import RadGradHeader from '../../../components/shared/RadGradHeader';
 import { PAGEIDS } from '../../../utilities/PageIDs';
 import PageLayout from '../../PageLayout';
+import Task1Component from './Task1Component';
+import Task2Component from './Task2Component';
 
 const headerPaneTitle = "Trey's Onboarding Sandbox";
 const headerPaneBody = 'Page for display of onboarding component development practice';
 const headerPaneImage = 'header-onboarding.png';
 
-const OnboardTreyPage: React.FC = () => {
-  const currentUser = Meteor.user() ? Meteor.user().username : '';
-  const { username } = useParams();
-  return (
+const OnboardTreyPage: React.FC = () => (
     <PageLayout id={PAGEIDS.ONBOARD_TREY} headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody} headerPaneImage={headerPaneImage}>
 
       <Grid>
         <Grid.Row columns={2}>
           <Grid.Column>
-            <RadGradSegment header={<RadGradHeader title='TASK 1: HELLO WORLD' icon='globe americas' dividing />}>Hello World</RadGradSegment>
+            <Task1Component/>
           </Grid.Column>
           <Grid.Column>
-            <RadGradSegment header={<RadGradHeader title='TASK 2: WHO IS THE USER?' icon='user graduate' dividing />}><p>The currently logged in user is: {currentUser}.</p><p>The username appearing in the URL is: {username}.</p><p>In RadGrad, these are not necessarily the same!</p></RadGradSegment>
+            <Task2Component/>
           </Grid.Column>
         </Grid.Row>
       </Grid>
     </PageLayout>
-  );
-};
+);
 
 export default OnboardTreyPage;
---
+++ app/imports/ui/pages/onboard/trey/Task2Component.tsx
@@ -0,0 +1,22 @@
+import { Meteor } from 'meteor/meteor';
+import { useParams } from 'react-router-dom';
+import React from 'react';
+import RadGradSegment from '../../../components/shared/RadGradSegment';
+import RadGradHeader from '../../../components/shared/RadGradHeader';
+
+const Task2Component: React.FC = () => {
+
+  const currentUser = Meteor.user() ? Meteor.user().username : '';
+  const { username } = useParams();
+
+  return (
+    <RadGradSegment header={<RadGradHeader title='TASK 2: WHO IS THE USER?' icon='user graduate' dividing />}>
+      <p>The currently logged in user is: {currentUser}.</p>
+      <p>The username appearing in the URL is: {username}.</p>
+      <p>In RadGrad, these are not necessarily the same!</p>
+    </RadGradSegment>
+  );
+};
+
+
+export default Task2Component;
