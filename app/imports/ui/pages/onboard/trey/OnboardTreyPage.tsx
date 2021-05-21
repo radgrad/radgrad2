f0b4f39e332a9e8bd2215ea7f783e7095d956bff
trey-sumida <tasumida@hawaii.edu>
2021-05-21 08:56:07 -1000

    Task 2 completed

--- app/imports/ui/pages/onboard/trey/OnboardTreyPage.tsx
+++ app/imports/ui/pages/onboard/trey/OnboardTreyPage.tsx
@@ -1,4 +1,7 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
 import RadGradSegment from '../../../components/shared/RadGradSegment';
 import RadGradHeader from '../../../components/shared/RadGradHeader';
 import { PAGEIDS } from '../../../utilities/PageIDs';
@@ -8,10 +11,24 @@ const headerPaneTitle = "Trey's Onboarding Sandbox";
 const headerPaneBody = 'Page for display of onboarding component development practice';
 const headerPaneImage = 'header-onboarding.png';
 
-const OnboardTreyPage: React.FC = () => (
-  <PageLayout id={PAGEIDS.ONBOARD_TREY} headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody} headerPaneImage={headerPaneImage}>
-    <RadGradSegment header={<RadGradHeader title='TASK 1: HELLO WORLD' icon='globe americas' dividing/>}>Hello World</RadGradSegment>
-  </PageLayout>
-);
+const OnboardTreyPage: React.FC = () => {
+  const currentUser = Meteor.user() ? Meteor.user().username : '';
+  const { username } = useParams();
+  return (
+    <PageLayout id={PAGEIDS.ONBOARD_TREY} headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody} headerPaneImage={headerPaneImage}>
+
+      <Grid>
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            <RadGradSegment header={<RadGradHeader title='TASK 1: HELLO WORLD' icon='globe americas' dividing />}>Hello World</RadGradSegment>
+          </Grid.Column>
+          <Grid.Column>
+            <RadGradSegment header={<RadGradHeader title='TASK 2: WHO IS THE USER?' icon='user graduate' dividing />}><p>The currently logged in user is: {currentUser}.</p><p>The username appearing in the URL is: {username}.</p><p>In RadGrad, these are not necessarily the same!</p></RadGradSegment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </PageLayout>
+  );
+};
 
 export default OnboardTreyPage;
