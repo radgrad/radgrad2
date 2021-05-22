4f948f569a1fedbb766cd7e2098c4a760e7d7d05
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-05-22 13:36:51 -1000

    Completed Task3.

--- app/imports/ui/pages/onboard/timothy/OnboardTimothyPage.tsx
+++ app/imports/ui/pages/onboard/timothy/OnboardTimothyPage.tsx
@@ -3,16 +3,13 @@ import React from 'react';
 import _ from 'lodash';
 import { useParams } from 'react-router-dom';
 import Markdown from 'react-markdown';
-import { useRouteMatch } from 'react-router';
 import { withTracker } from 'meteor/react-meteor-data';
 import { PAGEIDS } from '../../../utilities/PageIDs';
 import { Users } from '../../../../api/user/UserCollection';
-import { ProfileCareerGoals } from '../../../../api/user/profile-entries/ProfileCareerGoalCollection';
 import { CareerGoals } from '../../../../api/career/CareerGoalCollection';
 import PageLayout from '../../PageLayout';
 import RadGradSegment from '../../../components/shared/RadGradSegment';
 import RadGradHeader from '../../../components/shared/RadGradHeader';
-import * as Router from '../../../components/shared/utilities/router';
 
 const headerPaneTitle = "Timothy's Onboarding Sandbox";
 const headerPaneBody = 'Page for display of onboarding component development practice';
@@ -21,23 +18,21 @@ const headerPaneImage = 'header-onboarding.png';
 interface OnBoardVar {
   user: string;
   urlName: string;
-  MyCareerGoals: string;
+  TotalCareerGoals: string;
+  randomName: string;
+  randomDescription: string;
 }
 
-const OnboardTimothyPage: React.FC<OnBoardVar> = ({ user, urlName, MyCareerGoals }) => {
-  const profileItems = MyCareerGoals.length;
-  const random = _.sample(MyCareerGoals).careerGoalID;
-  const randomCareer = CareerGoals.findNonRetired({ _id: random });
-  const randomCareerName = randomCareer[0].name;
-  const randomCareerDesc = randomCareer[0].description;
+const OnboardTimothyPage: React.FC<OnBoardVar> = ({ user, urlName, TotalCareerGoals, randomName, randomDescription }) => {
+  const profileItems = TotalCareerGoals.length;
   return (
         <PageLayout id={PAGEIDS.ONBOARD_SHINYA} headerPaneTitle={headerPaneTitle} headerPaneBody={headerPaneBody}
                     headerPaneImage={headerPaneImage}>
             <div>
                 <RadGradSegment header={<RadGradHeader title='TASK 3: A RANDOM CAREER GOAL (REFRESH FOR A NEW ONE)' icon='database'/>}>
                     <div className='ui vertical segment'>
-                        <h3 className='ui header' style={{ marginBottom: '1em' }}> {randomCareerName} </h3>
-                        <Markdown escapeHtml source={randomCareerDesc} />
+                        <h3 className='ui header' style={{ marginBottom: '1em' }}> {randomName} </h3>
+                        <Markdown escapeHtml source={randomDescription} />
                     </div>
                     <div className='ui vertical segment'>
                         Note: The total number of career goals is: {profileItems}
@@ -71,13 +66,16 @@ const OnboardTimothyPage: React.FC<OnBoardVar> = ({ user, urlName, MyCareerGoals
 export default withTracker(() => {
   const user = Meteor.user() ? Meteor.user().username : '';
   const { username } = useParams();
-  const match = useRouteMatch();
-  const userID = Router.getUserIdFromRoute(match);
-  const MyCareerGoals = ProfileCareerGoals.findNonRetired({ userID: userID });
+  const TotalCareerGoals = CareerGoals.find({}).fetch();
+  const random = _.sample(TotalCareerGoals);
+  const randomName  = random.name;
+  const randomDescription = random.description;
   const urlName = Users.getProfile(username).username;
   return {
     user,
     urlName,
-    MyCareerGoals,
+    TotalCareerGoals,
+    randomName,
+    randomDescription,
   };
 })(OnboardTimothyPage);
