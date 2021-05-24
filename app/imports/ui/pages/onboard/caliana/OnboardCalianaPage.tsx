4c0004dbed6a9777336cb93dde110a3c5e645eea
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2021-05-24 06:01:13 -1000

    Finished task 3

--- app/imports/ui/pages/onboard/caliana/OnboardCalianaPage.tsx
+++ app/imports/ui/pages/onboard/caliana/OnboardCalianaPage.tsx
@@ -4,12 +4,13 @@ import PageLayout from '../../PageLayout';
 import RadGradHeader from '../../../components/shared/RadGradHeader';
 import RadGradSegment from '../../../components/shared/RadGradSegment';
 import Task2 from './Task2';
+import Task3 from './Task3';
 
 const headerPaneTitle = "Caliana's Onboarding Sandbox";
 const headerPaneBody = 'Page for display of onboarding component development practice';
 const headerPaneImage = 'header-onboarding.png';
 
-const headerTask1 = <RadGradHeader title='TASK 1: HELLO WORLD' icon='globe americas icon'/>;
+const headerTask1 = <RadGradHeader title='TASK 1: HELLO WORLD' icon='globe americas icon' dividing/>;
 
 const OnboardCalianaPage: React.FC = () => (
 
@@ -19,6 +20,7 @@ const OnboardCalianaPage: React.FC = () => (
             Hello World
         </RadGradSegment>
         <Task2/>
+        <Task3/>
     </PageLayout>
 );
 
--- app/imports/ui/pages/onboard/caliana/Task2.tsx
+++ app/imports/ui/pages/onboard/caliana/Task2.tsx
@@ -5,7 +5,7 @@ import RadGradHeader from '../../../components/shared/RadGradHeader';
 import RadGradSegment from '../../../components/shared/RadGradSegment';
 
 const Task2: React.FC = () => {
-  const headerTask2 = <RadGradHeader title='Task 2: Who is the user?' icon='user graduate icon'/>;
+  const headerTask2 = <RadGradHeader title='TASK 2: WHO IS THE USER?' icon='user graduate icon' dividing/>;
   const currentUser = Meteor.user() ? Meteor.user().username : '';
   const { username } = useParams();
 
