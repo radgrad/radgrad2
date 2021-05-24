b00ced61240e27bb65dd465902924e252f0da933
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2021-05-24 02:33:07 -1000

    Finished Task2

--- app/imports/ui/pages/onboard/caliana/OnboardCalianaPage.tsx
+++ app/imports/ui/pages/onboard/caliana/OnboardCalianaPage.tsx
@@ -3,6 +3,7 @@ import { PAGEIDS } from '../../../utilities/PageIDs';
 import PageLayout from '../../PageLayout';
 import RadGradHeader from '../../../components/shared/RadGradHeader';
 import RadGradSegment from '../../../components/shared/RadGradSegment';
+import Task2 from './Task2';
 
 const headerPaneTitle = "Caliana's Onboarding Sandbox";
 const headerPaneBody = 'Page for display of onboarding component development practice';
@@ -17,6 +18,7 @@ const OnboardCalianaPage: React.FC = () => (
         <RadGradSegment header={headerTask1}>
             Hello World
         </RadGradSegment>
+        <Task2/>
     </PageLayout>
 );
 
