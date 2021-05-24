a22b3a7ed80f1fe9868f2291915f16e4c34e7e7c
Cam Moore <cammymoore07@gmail.com>
2021-05-24 13:01:42 -1000

    Create Task4.tsx

---
+++ app/imports/ui/pages/onboard/caliana/Task4.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { CareerGoals } from '../../../../api/career/CareerGoalCollection';
+import { Courses } from '../../../../api/course/CourseCollection';
+import { Interests } from '../../../../api/interest/InterestCollection';
+import { Opportunities } from '../../../../api/opportunity/OpportunityCollection';
+import { Slugs } from '../../../../api/slug/SlugCollection';
+import { StudentProfiles } from '../../../../api/user/StudentProfileCollection';
+import { CareerGoal, Course, Interest, Opportunity, StudentProfile } from '../../../../typings/radgrad';
+import CareerGoalLabel from '../../../components/shared/label/CareerGoalLabel';
+import RadGradHeader from '../../../components/shared/RadGradHeader';
+import RadGradSegment from '../../../components/shared/RadGradSegment';
+
+interface Task4Props {
+  careerGoals: CareerGoal[];
+  courses: Course[];
+  interests: Interest[];
+  opportunities: Opportunity[];
+  students: StudentProfile[];
+}
+
+const Task4: React.FC<Task4Props> = ({ careerGoals, courses, opportunities, interests, students }) => {
+  const header = <RadGradHeader title="TASK 4: LABELS" icon="graduate" />;
+  return (
+    <RadGradSegment header={header}>
+      <RadGradHeader title="Career Goals" />
+      {careerGoals.map((goal) => (
+        <CareerGoalLabel slug={Slugs.getNameFromID(goal.slugID)} />
+      ))}
+    </RadGradSegment>
+  );
+};
+export default withTracker(() => {
+  const careerGoals = CareerGoals.findNonRetired();
+  const courses = Courses.findNonRetired();
+  const interests = Interests.findNonRetired();
+  const opportunities = Opportunities.findNonRetired();
+  const students = StudentProfiles.findNonRetired({ isAlumni: false });
+  return { careerGoals, courses, interests, opportunities, students };
+})(Task4);
