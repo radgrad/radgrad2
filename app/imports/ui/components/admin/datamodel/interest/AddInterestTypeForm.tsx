1f2fe71f4d4819f4fcda3bb04643994a79316c8d
Shun <47809066+saitoshi@users.noreply.github.com>
2021-05-25 19:09:27 +0900

    Update AddInterestTypeForm.tsx

--- app/imports/ui/components/admin/datamodel/interest/AddInterestTypeForm.tsx
+++ app/imports/ui/components/admin/datamodel/interest/AddInterestTypeForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Form, Header, Segment } from 'semantic-ui-react';
-import { AutoForm, TextField, LongTextField, BoolField, SubmitField } from 'uniforms-semantic';
+import { AutoForm, TextField, LongTextField, BoolField, SubmitField, ErrorsField } from 'uniforms-semantic';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
 import { defineMethod } from '../../../../../api/base/BaseCollection.methods';
 import { InterestTypes } from '../../../../../api/interest/InterestTypeCollection';
@@ -29,6 +29,7 @@ const AddInterestTypeForm: React.FC = () => {
         <LongTextField name="description" />
         <BoolField name="retired" />
         <SubmitField className="mini basic green" value="Add" disabled={false} inputRef={undefined} />
+          <ErrorsField/>
       </AutoForm>
     </Segment>
   );
