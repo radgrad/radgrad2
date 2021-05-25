04de1a9f4d731c555d1344fef0e0de21f726df29
Shun <47809066+saitoshi@users.noreply.github.com>
2021-05-25 19:16:45 +0900

    Update UpdateOpportunityTypeForm.tsx

--- app/imports/ui/components/admin/datamodel/opportunity/UpdateOpportunityTypeForm.tsx
+++ app/imports/ui/components/admin/datamodel/opportunity/UpdateOpportunityTypeForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Header, Segment } from 'semantic-ui-react';
-import { AutoForm, TextField, LongTextField, BoolField, SubmitField } from 'uniforms-semantic';
+import { AutoForm, TextField, LongTextField, BoolField, SubmitField, ErrorsField } from 'uniforms-semantic';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
 import BaseCollection from '../../../../../api/base/BaseCollection';
 import { OpportunityTypes } from '../../../../../api/opportunity/OpportunityTypeCollection';
@@ -27,6 +27,7 @@ const UpdateOpportunityTypeForm: React.FC<UpdateOpportunityTypeFormProps> = ({ c
         <BoolField name="retired" />
         <SubmitField inputRef={undefined} value="Update" disabled={false} className="mini basic green" />
         <Button onClick={handleCancel} basic color="green" size="mini">Cancel</Button>
+          <ErrorsField/>
       </AutoForm>
     </Segment>
   );
