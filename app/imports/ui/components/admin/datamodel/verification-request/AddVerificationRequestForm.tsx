87ab65862f1808fa671f845ddcad42d08363015a
Shun <47809066+saitoshi@users.noreply.github.com>
2021-05-25 19:23:36 +0900

    Update AddVerificationRequestForm.tsx

--- app/imports/ui/components/admin/datamodel/verification-request/AddVerificationRequestForm.tsx
+++ app/imports/ui/components/admin/datamodel/verification-request/AddVerificationRequestForm.tsx
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import React from 'react';
 import { Header, Segment } from 'semantic-ui-react';
 import SimpleSchema from 'simpl-schema';
-import { AutoForm, SelectField, BoolField, SubmitField } from 'uniforms-semantic';
+import { AutoForm, SelectField, BoolField, SubmitField, ErrorsField } from 'uniforms-semantic';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
 import { defineMethod } from '../../../../../api/base/BaseCollection.methods';
 import {
@@ -100,6 +100,7 @@ const AddVerificationRequestForm: React.FC<AddVerificationRequestFormProps> = ({
         {/* </Form.Group> */}
         <BoolField name="retired" />
         <SubmitField className="mini basic green" value="Add" disabled={false} inputRef={undefined} />
+          <ErrorsField/>
       </AutoForm>
     </Segment>
   );
