3dcf69aa5bc641d95a0fc241d363a504486ae9eb
Shun <47809066+saitoshi@users.noreply.github.com>
2021-05-25 19:22:48 +0900

    Update UpdateTeasersForm.tsx

--- app/imports/ui/components/admin/datamodel/teaser/UpdateTeasersForm.tsx
+++ app/imports/ui/components/admin/datamodel/teaser/UpdateTeasersForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Form, Header, Segment } from 'semantic-ui-react';
-import { AutoForm, TextField, SelectField, LongTextField, BoolField, SubmitField } from 'uniforms-semantic';
+import { AutoForm, TextField, SelectField, LongTextField, BoolField, SubmitField, ErrorsField } from 'uniforms-semantic';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
 import { CareerGoal, Course, Interest, Opportunity } from '../../../../../typings/radgrad';
@@ -72,6 +72,7 @@ const UpdateTeaserForm: React.FC<UpdateTeaserFormProps> = ({ careerGoals, course
         <BoolField name="retired" />
         <SubmitField inputRef={undefined} value="Update" disabled={false} className="mini basic green" />
         <Button onClick={handleCancel} basic color="green" size="mini">Cancel</Button>
+          <ErrorsField/>
       </AutoForm>
     </Segment>
   );
