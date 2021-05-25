fa30e2a5ada7a8ee8a39325f547d3215f661e387
Shun <47809066+saitoshi@users.noreply.github.com>
2021-05-26 05:16:44 +0900

    Update AddCareerGoalForm.tsx

--- app/imports/ui/components/admin/datamodel/career-goal/AddCareerGoalForm.tsx
+++ app/imports/ui/components/admin/datamodel/career-goal/AddCareerGoalForm.tsx
@@ -45,17 +45,17 @@ const AddCareerGoalForm: React.FC<AddCareerGoalFormProps> = ({ interests }) => {
   });
   const formSchema = new SimpleSchema2Bridge(schema);
   return (
-    <Segment padded>
-      <Header dividing>Add Career Goal</Header>
-      {/* eslint-disable-next-line no-return-assign */}
-      <AutoForm schema={formSchema} onSubmit={handleAdd} ref={(ref) => formRef = ref} showInlineError>
-        <TextField name="name" placeholder="Software Engineer" />
-        <MultiSelectField name="interests" placeholder="Select interest(s)" />
-        <LongTextField name="description" placeholder="Describe the Career Goal here" />
-        <SubmitField className="mini basic green" value="Add" />
-          <ErrorsField/>
-      </AutoForm>
-    </Segment>
+        <Segment padded>
+            <Header dividing>Add Career Goal</Header>
+            {/* eslint-disable-next-line no-return-assign */}
+            <AutoForm schema={formSchema} onSubmit={handleAdd} ref={(ref) => formRef = ref} showInlineError>
+                <TextField name="name" placeholder="Software Engineer"/>
+                <MultiSelectField name="interests" placeholder="Select interest(s)"/>
+                <LongTextField name="description" placeholder="Describe the Career Goal here"/>
+                <SubmitField className="mini basic green" value="Add"/>
+                <ErrorsField/>
+            </AutoForm>
+        </Segment>
   );
 };
 
