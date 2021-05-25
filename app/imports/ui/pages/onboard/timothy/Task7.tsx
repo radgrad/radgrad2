e8dcc52ec9549b984e24ceb821cacf9206a7aad6
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-05-25 10:04:28 -1000

    Complete Task 7

--- app/imports/ui/pages/onboard/timothy/Task7.tsx
+++ app/imports/ui/pages/onboard/timothy/Task7.tsx
@@ -17,28 +17,31 @@ interface Task7Prop {
 const Task7: React.FC<Task7Prop> = ({ totalAdvisors, totalFaculty, totalStudents }) => {
 
   const [open, setOpen] = useState(false);
+
   const panes = [
     { menuItem: 'Faculty', render: () => <Tab.Pane>{totalFaculty.map((faculty) => <UserLabel username={faculty} size='small' />)}</Tab.Pane> },
     { menuItem: 'Advisors', render: () => <Tab.Pane>{totalAdvisors.map((advisor) => <UserLabel username={advisor} size='small' />)}</Tab.Pane> },
     { menuItem: 'Student', render: () => <Tab.Pane>{totalStudents.map((student) => <UserLabel username={student} size='small' />)}</Tab.Pane> },
+    { menuItem: 'Task 1', render: () => <Tab.Pane>
+        <Modal
+        onClose={() => setOpen(false)}
+        onOpen={() =>  setOpen(true)}
+        open={open}
+        trigger={<Button>Show Modal</Button>}
+        >
+          <Modal.Header icon='globe americas'>TASK 1: HELLO WORLD</Modal.Header>
+          <Modal.Content>
+            <Modal.Description>
+              <p>Hello World</p>
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
+      </Tab.Pane> },
   ];
 
   return (
   <RadGradSegment header={<RadGradHeader title='TASK 7: Tabbed and Modal components' icon='splotch'/>}>
     <Tab panes={panes} />
-    <Modal
-    onClose={() => setOpen(false)}
-    onOpen={() =>  setOpen(true)}
-    open={open}
-    trigger={<Button>Show Modal</Button>}
-    >
-      <Modal.Header icon='globe americas'>TASK 1: HELLO WORLD</Modal.Header>
-      <Modal.Content>
-        <Modal.Description>
-          <p>Hello World</p>
-        </Modal.Description>
-      </Modal.Content>
-    </Modal>
   </RadGradSegment>
   );
 };
