0f8a2f844d907f7791b85fe2973cd96b91ce3580
Shun <47809066+saitoshi@users.noreply.github.com>
2021-05-25 19:17:52 +0900

    Update AddReviewForm.tsx

--- app/imports/ui/components/admin/datamodel/review/AddReviewForm.tsx
+++ app/imports/ui/components/admin/datamodel/review/AddReviewForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Header, Segment } from 'semantic-ui-react';
-import { AutoForm, SelectField, NumField, LongTextField, BoolField, SubmitField } from 'uniforms-semantic';
+import { AutoForm, SelectField, NumField, LongTextField, BoolField, SubmitField, ErrorsField } from 'uniforms-semantic';
 import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
 import { defineMethod } from '../../../../../api/base/BaseCollection.methods';
@@ -124,6 +124,7 @@ const AddReviewForm: React.FC<AddReviewFormProps> = ({ terms, courses, opportuni
         <LongTextField name="moderatorComments" />
         <BoolField name="retired" />
         <SubmitField className="mini basic green" value="Add" disabled={false} inputRef={undefined} />
+          <ErrorsField/>
       </AutoForm>
     </Segment>
   );
