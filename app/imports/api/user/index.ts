b51c175dac6f0efc4299c785def4b273793e39f7
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-05-27 10:55:23 -1000

    Checkpoint.

--- app/imports/api/user/index.ts
+++ app/imports/api/user/index.ts
@@ -1,8 +1,6 @@
 /** @namespace api/user */
 import './BaseProfileCollection';
-import './BaseProfileCollection.methods';
 import './AdvisorProfileCollection';
 import './FacultyProfileCollection';
 import './StudentProfileCollection';
 import './UserCollection';
-import './UserCollection.methods';
--- app/imports/api/user/profile-entries/index.ts
+++ app/imports/api/user/profile-entries/index.ts
@@ -1,8 +1,5 @@
 import './ProfileCareerGoalCollection';
 import './ProfileCourseCollection';
-import './ProfileEntries.methods';
-import './ProfileAssociatedUsers.methods';
 import './ProfileInterestCollection';
 import './ProfileOpportunityCollection';
-import './ProfileOpportunityCollection.methods';
 import './ProfileEntryTypes';
