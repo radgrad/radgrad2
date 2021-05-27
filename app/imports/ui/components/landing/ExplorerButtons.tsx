ea2ed4adbb2daf1bfd798828554a11b84da5ceab
trey-sumida <tasumida@hawaii.edu>
2021-05-26 14:23:41 -1000

    Added test for landing explorer pages

--- app/imports/ui/components/landing/ExplorerButtons.tsx
+++ app/imports/ui/components/landing/ExplorerButtons.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Button, Dropdown } from 'semantic-ui-react';
 
 const ExplorerButtons: React.FC = () => (
-  <Button inverted>
+  <Button inverted id='landing-explorer-buttons'>
     <Dropdown text='EXPLORER'>
       <Dropdown.Menu pointing="top right">
-        <Dropdown.Item text='Career Goals' href="/#/explorer/career-goals" />
-        <Dropdown.Item text='Interests' href="/#/explorer/interests" />
-        <Dropdown.Item text='Courses' href="/#/explorer/courses" />
-        <Dropdown.Item text='Opportunities' href="/#/explorer/opportunities" />
+        <Dropdown.Item id='landing-career-goals-explorer' text='Career Goals' href="/#/explorer/career-goals" />
+        <Dropdown.Item id='landing-interests-explorer' text='Interests' href="/#/explorer/interests" />
+        <Dropdown.Item id='landing-courses-explorer' text='Courses' href="/#/explorer/courses" />
+        <Dropdown.Item id='landing-opportunities-explorer' text='Opportunities' href="/#/explorer/opportunities" />
       </Dropdown.Menu>
     </Dropdown>
   </Button>
