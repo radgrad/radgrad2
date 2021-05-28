cc188b72efc3b97a38985fed65467109a0b01a2f
trey-sumida <tasumida@hawaii.edu>
2021-05-28 08:19:34 -1000

    Created ComponentIDs file and added enums from landing.page.js

---
+++ app/imports/ui/utilities/ComponentIDs.ts
@@ -0,0 +1,9 @@
+/** Define IDs to identify components in the system. This is used for testing. */
+export enum COMPONENTIDS {
+  LANDING_EXPLORER_MENU = 'landing-explorer-menu',
+  LANDING_EXPLORER_BUTTONS = 'landing-explorer-buttons',
+  LANDING_CAREER_GOALS_EXPLORER = 'landing-career-goals-explorer',
+  LANDING_INTERESTS_EXPLORER = 'landing-interests-explorer',
+  LANDING_COURSES_EXPLORER = 'landing-courses-explorer',
+  LANDING_OPPORTUNITIES_EXPLORER = 'landing-opportunities-explorer',
+}
