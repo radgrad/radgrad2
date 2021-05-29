82c7b406d905f5cc87f15f6df76e1bbd4fcaee95
Shun <47809066+saitoshi@users.noreply.github.com>
2021-05-30 07:46:04 +0900

    Update main.ts

--- app/client/main.ts
+++ app/client/main.ts
@@ -20,10 +20,8 @@ import '../imports/api/test';
 import '../imports/api/user';
 import '../imports/api/verification';
 import '../imports/api/whats-new';
-
 import '../imports/startup/both';
 import '../imports/startup/client';
-
 import './style.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import '../imports/ui/pages/admin'; // for jsdoc.
