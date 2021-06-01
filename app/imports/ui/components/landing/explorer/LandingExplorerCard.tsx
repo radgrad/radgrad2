f21ec838a3f26a22ce94f0a13db16a30546d19dc
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-06-01 08:33:44 -1000

    checkpoint

--- app/imports/ui/components/landing/explorer/LandingExplorerCard.tsx
+++ app/imports/ui/components/landing/explorer/LandingExplorerCard.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Button, Card, Icon } from 'semantic-ui-react';
 import { useRouteMatch } from 'react-router-dom';
 import Markdown from 'react-markdown';
-import { getSlug, itemShortDescription } from '../utilities/helper-functions';
+import { getSlug } from '../utilities/helper-functions';
 import { EXPLORER_TYPE } from '../../../layouts/utilities/route-constants';
 import * as Router from '../../shared/utilities/router';
+import { docToShortDescription } from '../../shared/utilities/data-model';
+import LandingInterestList from '../LandingInterestList';
 import { Courses } from '../../../../api/course/CourseCollection';
 
 interface ItemProps {
   name: string;
   num?: string;
   description: string;
+  interestIDs?: string[];
   slugID: string;
 }
 
@@ -26,19 +29,20 @@ const LandingExplorerCard: React.FC<LandingExplorerCardProps> = ({ item, type })
     title = Courses.getName(getSlug(item));
   }
   const match = useRouteMatch();
+  const itemShortDescription = docToShortDescription(item);
   return (
     <Card>
       <Card.Content className="content">
         <div className="header">{title}</div>
       </Card.Content>
       <Card.Content className="content">
-        <Markdown escapeHtml source={`${itemShortDescription(item)}...`} renderers={{ link: (localProps) => Router.renderLink(localProps, match) }} />
+        <Markdown escapeHtml source={`${itemShortDescription}...`} renderers={{ link: (localProps) => Router.renderLink(localProps, match) }} />
+        { item.interestIDs ? (<LandingInterestList interestIDs={item.interestIDs} size="small" />) : ''}
       </Card.Content>
       <Button.Group attached="bottom" className="radgrad-home-buttons ui center aligned three bottom attached {{hidden}} buttons">
         <a href={routeToItem} className="ui button">
-          <Icon name="chevron circle right" />
-          <br />
-        View More
+          <Icon name="zoom in" />
+          See Details
         </a>
       </Button.Group>
     </Card>
