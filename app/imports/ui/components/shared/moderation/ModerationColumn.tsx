0d897f87b7bc93cee299c41fcab150e73d09a093
trey-sumida <tasumida@hawaii.edu>
2021-06-01 09:13:30 -1000

    Removed moderation question card and replaced segment with radgradsegment

--- app/imports/ui/components/shared/moderation/ModerationColumn.tsx
+++ app/imports/ui/components/shared/moderation/ModerationColumn.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Header, Segment, Container, Item } from 'semantic-ui-react';
+import { Container, Item } from 'semantic-ui-react';
+import RadGradHeader from '../RadGradHeader';
+import RadGradSegment from '../RadGradSegment';
 import ModerationReview from './ModerationReview';
-import ModerationQuestion from './ModerationQuestion';
 import { Review } from '../../../../typings/radgrad';
 
 interface ModerationColumnProps {
@@ -10,43 +11,26 @@ interface ModerationColumnProps {
   reviews: Review[];
   type: string;
 }
+
 // this will be the column widget that holds the individual moderation cards
 
 const ModerationColumn: React.FC<ModerationColumnProps> = ({ type, handleAccept, handleReject, reviews }) => (
-  <div>
-    <Segment>
-      <Header as="h4" textAlign="left" dividing>
-        PENDING {type} REVIEWS
-      </Header>
-      {reviews.length > 0 ? (
-        <Item.Group divided>
-          {reviews.map((review, index) => (
-            <Item key={review._id}>
-              {' '}
-              <ModerationReview item={review} handleAccept={handleAccept} handleReject={handleReject} />
-            </Item>
-          ))}
-        </Item.Group>
-      ) : (
-        <React.Fragment>
-          {reviews.length > 0 ? (
-            <Item.Group divided>
-              {reviews.map((question, index) => (
-                <Item key={question._id}>
-                  {' '}
-                  <ModerationQuestion question={question} handleAccept={handleAccept} handleReject={handleReject} />
-                </Item>
-              ))}
-            </Item.Group>
-          ) : (
-            <Container textAlign="left">
-              <i>No pending {type.toLowerCase()} reviews</i>
-            </Container>
-          )}
-        </React.Fragment>
-      )}
-    </Segment>
-  </div>
+  <RadGradSegment header={<RadGradHeader title={`PENDING ${type} REVIEWS`} dividing />}>
+    {reviews.length > 0 ? (
+      <Item.Group divided>
+        {reviews.map((review, index) => (
+          <Item key={review._id}>
+            {' '}
+            <ModerationReview item={review} handleAccept={handleAccept} handleReject={handleReject} />
+          </Item>
+        ))}
+      </Item.Group>
+    ) : (
+      <Container textAlign="left">
+        <i>No pending {type.toLowerCase()} reviews</i>
+      </Container>
+    )}
+  </RadGradSegment>
 );
 
 export default ModerationColumn;
