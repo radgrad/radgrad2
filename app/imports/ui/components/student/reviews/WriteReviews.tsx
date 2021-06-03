9ed9f74d65baf8ab509d6a71fb435666c1e1547a
trey-sumida <tasumida@hawaii.edu>
2021-06-03 09:54:50 -1000

    Fixed messaging

--- app/imports/ui/components/student/reviews/WriteReviews.tsx
+++ app/imports/ui/components/student/reviews/WriteReviews.tsx
@@ -126,7 +126,7 @@ const WriteReviews: React.FC<WriteReviewsProps> = ({ unreviewedCourses, unreview
               Select the term you participated in.
             </Grid.Column>
             <Grid.Column width={10}>
-              {termNames.length <= 1 ? <SelectField name='academicTerm' disabled /> :<SelectField name='academicTerm' disabled={disabled} />}
+              {termNames.length <= 1 ? <SelectField name='academicTerm' disabled /> : <SelectField name='academicTerm' disabled={disabled} />}
             </Grid.Column>
             <Grid.Column width={6} verticalAlign='middle'>
               Rate your overall satisfaction.
