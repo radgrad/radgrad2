c151098e0e823d1c9b83cc0a8ac4f60716b3bfcf
Shun <47809066+saitoshi@users.noreply.github.com>
2021-06-07 07:07:53 +0900

    modification for the modal to show up

---
+++ app/imports/ui/components/shared/explorer/item-view/utilities/AddCareerToProfile.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Button, Icon, Modal, SemanticFLOATS, Grid } from 'semantic-ui-react';
+import { defineMethod, removeItMethod } from '../../../../../../api/base/BaseCollection.methods';
+import RadGradAlert from '../../../../../utilities/RadGradAlert';
+import { CareerGoal, MeteorError } from '../../../../../../typings/radgrad';
+import { ProfileCareerGoals } from '../../../../../../api/user/profile-entries/ProfileCareerGoalCollection';
+import { PROFILE_ENTRY_TYPE, IProfileEntryTypes } from '../../../../../../api/user/profile-entries/ProfileEntryTypes';
+import { COMPONENTIDS } from '../../../../../utilities/ComponentIDs';
+import RelatedInterests from '../../../RelatedInterests';
+import { createDefinitionData, getCollectionName } from './profile-button';
+
+export interface AddToProfileButtonProps {
+  careerGoal: CareerGoal;
+  userID: string;
+  type: IProfileEntryTypes;
+  added: boolean;
+  inverted: boolean;
+  floated?: SemanticFLOATS;
+}
+
+const handleAdd = (userID: string, item: CareerGoal, type: IProfileEntryTypes) => () => {
+  const collectionName = getCollectionName(type);
+  const definitionData = createDefinitionData(userID, item, type);
+  defineMethod.callPromise({ collectionName, definitionData })
+    .catch((error: MeteorError) => { RadGradAlert.failure('Failed to add to profile', error.message);})
+    .then(() => { RadGradAlert.success('Added to profile');});
+};
+
+const handleRemove = (userID: string, item: CareerGoal, type: IProfileEntryTypes) => () => {
+  const collectionName = getCollectionName(type);
+  let instance;
+  switch (type) {
+    case PROFILE_ENTRY_TYPE.CAREERGOAL:
+      instance = ProfileCareerGoals.findNonRetired({
+        userID: userID,
+        careerGoalID: item._id,
+      })[0]._id;
+      break;
+    default:
+      console.error(`Bad profile entry type: ${type}`);
+      break;
+  }
+  removeItMethod.callPromise({ collectionName, instance })
+    .catch((error) => { RadGradAlert.failure('Failed to remove from profile', error.message, error);});
+};
+
+const AddToProfileButton: React.FC<AddToProfileButtonProps> = ({ userID, careerGoal, type, added, inverted, floated }) => {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <React.Fragment>
+      {added ? (
+        <Button id={COMPONENTIDS.REMOVE_FROM_PROFILE_BUTTON} onClick={handleRemove(userID, careerGoal, type)} size="small" color="teal" floated={floated || 'right'} basic inverted={inverted}>
+          <Icon name="user outline" color="grey" inverted={inverted} />
+          <Icon name="minus" />
+          REMOVE FROM PROFILE
+        </Button>
+      ) : (
+        <Modal
+          onClose={() => setOpen(false)}
+          onOpen ={() => setOpen(true)}
+          open = {open}
+          trigger = {<Button size="small" color="teal" floated={floated || 'right'} basic inverted={inverted}>
+            <Icon name="user" color="grey" inverted={inverted} />
+            <Icon name="plus" />
+            ADD TO PROFILE
+          </Button>}>
+          <Modal.Description>
+            <p> Adding this Career Goal will automatically add the following new Interests to your profile.<br/>
+              If you are OK with that, just press OK. </p>
+            <RelatedInterests item={careerGoal}/>
+          </Modal.Description>
+          <Modal.Actions>
+            <br/>
+            <Grid textAlign = 'center'>
+              <Button id={COMPONENTIDS.ADD_TO_PROFILE_BUTTON} size="small" onClick={handleAdd(userID, careerGoal, type)} color="teal" basic>
+                <Icon name="user" color="grey"/>
+                <Icon name="plus" />
+                ADD TO PROFILE
+              </Button>
+              <br/>
+            </Grid>
+          </Modal.Actions>
+        </Modal>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default AddToProfileButton;
