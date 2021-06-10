12e45f937a17b92309e5922218334ab386103b7c
trey-sumida <tasumida@hawaii.edu>
2021-06-10 12:48:50 -1000

    Prerequisite courses component only shown if there are any

--- app/imports/ui/pages/shared/item-view/CourseViewPage.tsx
+++ app/imports/ui/pages/shared/item-view/CourseViewPage.tsx
@@ -71,6 +71,8 @@ const CourseViewPage: React.FC<CourseViewPageProps> = ({
   const courseSlug = Slugs.getNameFromID(course.slugID);
   const completed = isCourseCompleted(courseSlug, profile.userID);
   const relatedCourses = getAssociationRelatedCourses(course.prerequisites.map((c) => Courses.findDocBySlug(c)), profile.userID);
+  const combinedArrays = relatedCourses.inPlan.concat(relatedCourses.notInPlan, relatedCourses.completed);
+
   return (
     <PageLayout id={PAGEIDS.COURSE} headerPaneTitle={headerPaneTitle} headerPaneImage={headerPaneImage}
       headerPaneButton={headerPaneButton}>
@@ -78,7 +80,7 @@ const CourseViewPage: React.FC<CourseViewPageProps> = ({
         <Grid.Row>
           <Grid.Column width={5}>
             <RelatedInterests item={course} />
-            <RelatedCourses relatedCourses={relatedCourses} profile={profile} title='prerequisites' />
+            {combinedArrays.length !== 0 ? <RelatedCourses relatedCourses={relatedCourses} profile={profile} title='prerequisites' /> : null}
             <RelatedCareerGoals careerGoals={relatedCareerGoals} userID={profile.userID} />
             <RelatedOpportunities relatedOpportunities={relatedOpportunities} profile={profile} />
           </Grid.Column>
