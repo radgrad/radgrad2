62f022e7e387cf669af154bfea5697516b97ccb2
trey-sumida <tasumida@hawaii.edu>
2021-06-16 10:42:35 -1000

    Added start to test for student adding a review

--- app/tests/tests.student.testcafe.ts
+++ app/tests/tests.student.testcafe.ts
@@ -5,6 +5,7 @@ import { studentHomePage } from './student.home.page';
 import { studentNavBar } from './navbar.student.component';
 import { explorerPages } from './explorer.pages';
 import { visibilityPage } from './visibility.page';
+import { reviewPage } from './review.page';
 import {
   studentDegreePlannerPage,
   studentICEPointsPage,
@@ -109,4 +110,12 @@ test('Test student visibility page', async (testController) => {
   await visibilityPage.testVisibility(testController, `#${COMPONENTIDS.SHARE_COURSES}`, `#${COMPONENTIDS.PROFILE_COURSES}`);
   await visibilityPage.testVisibility(testController, `#${COMPONENTIDS.SHARE_LEVEL}`, `#${COMPONENTIDS.PROFILE_LEVEL}`);
   await visibilityPage.testVisibility(testController, `#${COMPONENTIDS.SHARE_ICE}`, `#${COMPONENTIDS.PROFILE_ICE}`);
+});
+
+test('Test student adding a review', async (testController) => {
+  await landingNavBar.gotoStudentLogin(testController);
+  await signinPage.signin(testController, credentials.student.abi);
+
+  await studentNavBar.gotoReviewsPage(testController);
+  await reviewPage.testWriteReview(testController);
 });
