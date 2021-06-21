bdad881b4362240dca8a725927cf471342d5a4d0
Cam Moore <cammymoore07@gmail.com>
2021-06-21 08:49:57 -1000

    Removed slugs from reviews.

--- app/imports/typings/radgrad.d.ts
+++ app/imports/typings/radgrad.d.ts
@@ -788,7 +788,6 @@ export interface Review extends Document {
 }
 
 export interface ReviewDefine extends DumpOne {
-  slug?: string;
   student: string;
   reviewType: IReviewTypes;
   reviewee: string;
