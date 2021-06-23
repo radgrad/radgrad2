674fa696a2a8e33d51b12294bdc150e23b184f67
trey-sumida <tasumida@hawaii.edu>
2021-06-22 16:35:01 -1000

    created testcafe file

---
+++ app/tests/*.testcafe.details.ts
@@ -0,0 +1,22 @@
+import { landingNavBar } from './navbar.landing.component';
+import { signinPage } from './signin.page';
+import { studentNavBar } from './navbar.student.component';
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+/* global fixture:false, test:false */
+
+/** Credentials for sample user(s) defined in settings.development.json. */
+const credentials = {
+  student: {
+    abi: { userName: 'abi@hawaii.edu', password: 'foo', accountName: 'abi', fullName: 'Abigail Kealoha' },
+  },
+};
+
+fixture('Explorer details page acceptance tests').page('http://localhost:3200');
+
+test('Test the student interests details pages', async (testController) => {
+  await landingNavBar.gotoStudentLogin(testController);
+  await signinPage.signin(testController, credentials.student.abi);
+
+  await studentNavBar.gotoInterestsExplorerPage(testController);
+});
