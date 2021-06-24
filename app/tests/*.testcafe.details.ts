144c087a5c630e4180dc46481a6a48453a55b1ca
trey-sumida <tasumida@hawaii.edu>
2021-06-24 09:11:18 -1000

    Added tests for all pages

--- app/tests/*.testcafe.details.ts
+++ app/tests/*.testcafe.details.ts
@@ -23,3 +23,27 @@ test('Test the student interests details pages', async (testController) => {
   await studentNavBar.gotoInterestsExplorerPage(testController);
   await detailsPage.gotoViewPageAndVerify(testController, PAGEIDS.INTEREST, studentNavBar.gotoInterestsExplorerPage);
 });
+
+test('Test the student career goals details pages', async (testController) => {
+  await landingNavBar.gotoStudentLogin(testController);
+  await signinPage.signin(testController, credentials.student.abi);
+
+  await studentNavBar.gotoCareerGoalsExplorerPage(testController);
+  await detailsPage.gotoViewPageAndVerify(testController, PAGEIDS.CAREER_GOAL, studentNavBar.gotoCareerGoalsExplorerPage);
+});
+
+test('Test the student courses details pages', async (testController) => {
+  await landingNavBar.gotoStudentLogin(testController);
+  await signinPage.signin(testController, credentials.student.abi);
+
+  await studentNavBar.gotoCourseExplorerPage(testController);
+  await detailsPage.gotoViewPageAndVerify(testController, PAGEIDS.COURSE, studentNavBar.gotoCourseExplorerPage);
+});
+
+test('Test the student opportunities details pages', async (testController) => {
+  await landingNavBar.gotoStudentLogin(testController);
+  await signinPage.signin(testController, credentials.student.abi);
+
+  await studentNavBar.gotoOpportunitiesPage(testController);
+  await detailsPage.gotoViewPageAndVerify(testController, PAGEIDS.OPPORTUNITY, studentNavBar.gotoOpportunitiesPage);
+});
--- app/tests/details.page.js
+++ app/tests/details.page.js
@@ -4,18 +4,14 @@ import { Selector } from 'testcafe';
 class DetailsPage {
 
   async gotoViewPageAndVerify(testController, pageName, navbarFunction) {
-    const linkSelector = await Selector(() => document.querySelectorAll('div[class="ui card"] a'));
+    const linkSelector = await Selector(() => document.querySelectorAll('div[class="ui card"] a[class="ui button"]'));
     const linkCount = await linkSelector.count;
 
     for (let i = 0; i < linkCount; i++) {
-      try {
-        await testController.click(linkSelector.nth(i));
-        const pageIdSelector = await Selector(`#${pageName}`);
-        await testController.expect(pageIdSelector.exists).ok();
-        await navbarFunction(testController);
-      } catch {
-        console.log(`${linkSelector.nth(i)} failed to load.`);
-      }
+      await testController.click(linkSelector.nth(i));
+      const pageIdSelector = await Selector(`#${pageName}`);
+      await testController.expect(pageIdSelector.exists).ok();
+      await navbarFunction(testController);
     }
   }
 
