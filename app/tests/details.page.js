2e89153a6ad440a72ceaf4560e9201c37af41300
trey-sumida <tasumida@hawaii.edu>
2021-06-23 16:19:02 -1000

    Made function more dynamic

--- app/tests/details.page.js
+++ app/tests/details.page.js
@@ -1,18 +1,21 @@
 /* eslint-disable no-await-in-loop */
 import { Selector } from 'testcafe';
-import { studentNavBar } from './navbar.student.component';
 
 class DetailsPage {
 
-  async gotoViewPageAndVerify(testController, pageName) {
+  async gotoViewPageAndVerify(testController, pageName, navbarFunction) {
     const linkSelector = await Selector(() => document.querySelectorAll('div[class="ui card"] a'));
     const linkCount = await linkSelector.count;
 
     for (let i = 0; i < linkCount; i++) {
-      await testController.click(linkSelector.nth(i));
-      const pageIdSelector = await Selector(`#${pageName}`);
-      await testController.expect(pageIdSelector.exists).ok();
-      await studentNavBar.gotoInterestsExplorerPage(testController);
+      try {
+        await testController.click(linkSelector.nth(i));
+        const pageIdSelector = await Selector(`#${pageName}`);
+        await testController.expect(pageIdSelector.exists).ok();
+        await navbarFunction(testController);
+      } catch {
+        console.log(`${linkSelector.nth(i)} failed to load.`);
+      }
     }
   }
 
