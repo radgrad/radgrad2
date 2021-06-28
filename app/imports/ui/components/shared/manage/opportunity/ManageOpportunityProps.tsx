1585c0eb9cab4f812bc685ce7919fe3cf7a8f4ac
Shun <47809066+saitoshi@users.noreply.github.com>
2021-06-29 03:58:28 +0900

    Update ManageOpportunityProps.tsx

--- app/imports/ui/components/shared/manage/opportunity/ManageOpportunityProps.tsx
+++ app/imports/ui/components/shared/manage/opportunity/ManageOpportunityProps.tsx
@@ -1,6 +1,7 @@
-import { BaseProfile, Interest, Opportunity, OpportunityType } from '../../../../../typings/radgrad';
+import { AcademicTerm, BaseProfile, Interest, Opportunity, OpportunityType } from '../../../../../typings/radgrad';
 
 export interface ManageOpportunityProps {
+  terms: AcademicTerm[];
   opportunity: Opportunity;
   sponsors: BaseProfile[];
   interests: Interest[];
