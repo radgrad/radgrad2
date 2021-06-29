2baf62dedfc777689015ea7df087177a2caf6695
Timt <61216890+timothyhuo1@users.noreply.github.com>
2021-06-28 14:35:46 -1000

    Update Upcoming Event and ExplorerOpportunity Component

--- app/imports/ui/components/shared/community/UpcomingEvents.tsx
+++ app/imports/ui/components/shared/community/UpcomingEvents.tsx
@@ -49,45 +49,50 @@ export default withTracker(() => {
 
   const date = new Date();
   const currentDate = moment(date).format('MM/DD/YYYY');
-  const threeMonths = moment(currentDate).add(3, 'months');
-  const endDate = moment(threeMonths).format('MM/DD/YYYY');
+
+  // Finding yesterday's date and the date three months from now
+  const dayBefore = moment(currentDate).subtract(1, 'days').format('MM/DD/YYYY');
+  const threeMonths = moment(currentDate).add(3, 'months').format('MM/DD/YYYY');
   const eventList = [];
-  // Finding the academic term of today and 3 months from today
-  const currentTerm = AcademicTerms.getAcademicTerm(currentDate);
-  const nextTerm = AcademicTerms.getAcademicTerm(endDate);
+  // Finding the academic term of yesterday and 3 months from today
+  const currentTerm = AcademicTerms.getAcademicTerm(dayBefore);
+  const nextTerm = AcademicTerms.getAcademicTerm(threeMonths);
   const currentPlannerOpp = OpportunityInstances.findNonRetired({ studentID: userID, termID: currentTerm });
   let plannerOpportunities = currentPlannerOpp;
+  // If the current term is not the same in three months
   if (currentTerm !== nextTerm) {
     const nextPlannerOpp = OpportunityInstances.findNonRetired({ studentID: userID, termID: nextTerm });
     plannerOpportunities = [...currentPlannerOpp, ...nextPlannerOpp];
   }
 
-  // Traversing through each opportunity and find if there is an event between now and 3 months from now
+  // Traversing through each opportunity
   opportunities.forEach((opportunity) => {
     if (opportunity.eventDate1) {
       const temp = moment(opportunity.eventDate1).format('MM/DD/YYYY');
-      if (moment(temp).isBetween(currentDate, endDate)) {
+
+      // If the event date is in between yesterday's date and the date three months from now
+      if (moment(temp).isBetween(dayBefore, threeMonths)) {
         eventList.push({ id: opportunity._id, name: opportunity.name, picture: opportunity.picture, date: moment(opportunity.eventDate1).format('MM/DD/YYYY'), label: opportunity.eventDateLabel1, interestIDs: opportunity.interestIDs  });
       }
     }
 
     if (opportunity.eventDate2 ) {
       const temp = moment(opportunity.eventDate2).format('MM/DD/YYYY');
-      if (moment(temp).isBetween(currentDate, endDate)) {
+      if (moment(temp).isBetween(dayBefore, threeMonths)) {
         eventList.push({ id: opportunity._id, name: opportunity.name, picture: opportunity.picture, date: moment(opportunity.eventDate2).format('MM/DD/YYYY'), label: opportunity.eventDateLabel2, interestIDs: opportunity.interestIDs });
       }
     }
 
     if (opportunity.eventDate3) {
       const temp = moment(opportunity.eventDate3).format('MM/DD/YYYY');
-      if (moment(temp).isBetween(currentDate, endDate)) {
+      if (moment(temp).isBetween(dayBefore, threeMonths)) {
         eventList.push({ id: opportunity._id,  name: opportunity.name, picture: opportunity.picture, date: moment(opportunity.eventDate3).format('MM/DD/YYYY'), label: opportunity.eventDateLabel3, interestIDs: opportunity.interestIDs  });
       }
     }
 
     if (opportunity.eventDate4) {
       const temp = moment(opportunity.eventDate4).format('MM/DD/YYYY');
-      if (moment(temp).isBetween(currentDate, endDate)) {
+      if (moment(temp).isBetween(dayBefore, threeMonths)) {
         eventList.push({ id: opportunity._id,  name: opportunity.name, picture: opportunity.picture, date: moment(opportunity.eventDate4).format('MM/DD/YYYY'), label: opportunity.eventDateLabel4, interestIDs: opportunity.interestIDs  });
       }
     }
