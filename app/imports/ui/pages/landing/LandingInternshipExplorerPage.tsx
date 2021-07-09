42968a00f1026e025bef0e4ed66e1f5c6a5afda6
Shun <47809066+saitoshi@users.noreply.github.com>
2021-07-10 05:31:21 +0900

    Update LandingInternshipExplorerPage.tsx

--- app/imports/ui/pages/landing/LandingInternshipExplorerPage.tsx
+++ app/imports/ui/pages/landing/LandingInternshipExplorerPage.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
+import Markdown from 'react-markdown';
 import { useRouteMatch } from 'react-router';
+import { Grid } from 'semantic-ui-react';
 import { Internships } from '../../../api/internship/InternshipCollection';
 import { CareerGoal, Course, Interest, Internship, Opportunity } from '../../../typings/radgrad';
-import { Grid } from 'semantic-ui-react';
 import LandingExplorerMenuContainer from '../../components/landing/explorer/LandingExplorerMenu';
 import LandingExplorerMenuBar from '../../components/landing/explorer/LandingExplorerMenuBar';
+import RadGradHeader from '../../components/shared/RadGradHeader';
+import RadGradSegment from '../../components/shared/RadGradSegment';
+import * as Router from '../../components/shared/utilities/router';
 import { PAGEIDS } from '../../utilities/PageIDs';
 import PageLayout from '../PageLayout';
 
 interface InternshipExplorerProps {
   currentUser: string;
-  interest: Interest;
+  interest: Interest[];
   courses: Course[];
   opportunities: Opportunity[];
   careerGoals: CareerGoal[];
-  internship: Internship[];
+  internship: Internship;
 }
 
 const headerPaneTitle = 'The Internship Explorer';
 
-const LandingInternshipExplorerPage: React.FC<InternshipExplorerProps> = ({ currentUser, internship, careerGoals, courses, interest, opportunities}) => {
+const LandingInternshipExplorerPage: React.FC<InternshipExplorerProps> = ({ currentUser, internship, careerGoals, courses, interest, opportunities }) => {
   const match = useRouteMatch();
   return (
     <div>
@@ -31,7 +35,12 @@ const LandingInternshipExplorerPage: React.FC<InternshipExplorerProps> = ({ curr
               <LandingExplorerMenuContainer />
             </Grid.Column>
           </Grid.Row>
-          <Grid.Column width={13}/>
+          <Grid.Column width={13}>
+            <RadGradSegment header={<RadGradHeader title={internship.position} dividing/> }>
+              <Markdown escapeHtml source={internship.description}
+                renderers={{ link: (localProps) => Router.renderLink(localProps, match) }}/>
+            </RadGradSegment>
+          </Grid.Column>
         </Grid>
       </PageLayout>
     </div>
