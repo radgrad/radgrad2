bf39b01fb63e06e11f400ef2944645bb9f26fa3a
Cam Moore <cammymoore07@gmail.com>
2021-09-18 09:04:57 -1000

    Update InternshipBrowserView.tsx
    order the internships.

--- app/imports/ui/components/shared/explorer/browser-view/InternshipBrowserView.tsx
+++ app/imports/ui/components/shared/explorer/browser-view/InternshipBrowserView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Grid } from 'semantic-ui-react';
+import PreferredChoice from '../../../../../api/degree-plan/PreferredChoice';
 import { CareerGoal, Course, Interest, Internship, Opportunity, ProfileCareerGoal, ProfileCourse, ProfileInterest, ProfileOpportunity } from '../../../../../typings/radgrad';
 import { EXPLORER_TYPE_ICON } from '../../../../utilities/ExplorerUtils';
 import RadGradHeader from '../../RadGradHeader';
@@ -21,12 +22,15 @@ interface InternshipBrowserViewProps {
 const InternshipBrowswerView: React.FC<InternshipBrowserViewProps> = ({ careerGoals, courses, interests, internships, opportunities, profileCareerGoals, profileCourses, profileOpportunities, profileInterests }) => {
   const icon = EXPLORER_TYPE_ICON.INTERNSHIP;
   const header = <RadGradHeader title='internships' count={internships.length} icon={icon} />;
+  const interestIDs = profileInterests.map(pi => pi.interestID);
+  const preferred = new PreferredChoice(internships, interestIDs);
+  const orderedInternships = preferred.getOrderedChoices();
   return (
     <div id="internship-browser-view">
       <RadGradSegment header={header}>
         <Grid>
           <Card.Group itemsPerRow={3} stackable id="browserCardGroup" style={{ margin: '0px' }}>
-            {internships.map((internship) => (
+            {orderedInternships.map((internship) => (
               <InternshipCard internship={internship} key={internship._id} />
             ))}
           </Card.Group>
