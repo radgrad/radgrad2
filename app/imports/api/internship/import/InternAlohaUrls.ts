2ce05abcd6ed0bed83921ff6981d07b9b0f36b88
Cam Moore <cammymoore07@gmail.com>
2021-12-09 13:42:48 -1000

    Update InternAlohaUrls.ts
    Changed URLs to the v2 listings.

--- app/imports/api/internship/import/InternAlohaUrls.ts
+++ app/imports/api/internship/import/InternAlohaUrls.ts
@@ -1,18 +1,17 @@
 export enum InternAlohaUrlsEnum {
-  '80000hours' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/80000hours.canonical.data.json',
-  'angellist' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/angellist.canonical.data.json',
-  'apple' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/apple.canonical.data.json',
-  'cisco' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/cisco.canonical.data.json',
-  'glassdoor' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/glassdoor.canonical.data.json',
+  'apple' = 'https://raw.githubusercontent.com/internaloha/scrapers/main/listings/compsci/apple.json',
+  'chegg' = 'https://raw.githubusercontent.com/internaloha/scrapers/main/listings/compsci/chegg.json',
+  'cisco' = 'https://raw.githubusercontent.com/internaloha/scrapers/main/listings/compsci/cisco.json',
+  'glassdoor' = 'https://raw.githubusercontent.com/internaloha/scrapers/main/listings/compsci/glassdoor.json',
   'idealist' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/idealist.canonical.data.json',
   'indeed' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/indeed.canonical.data.json',
   'linkedin' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/linkedin.canonical.data.json',
   'monster' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/monster.canonical.data.json',
-  'nsf-reu' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/nsf-reu.canonical.data.json',
-  'simplyhired' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/simplyHired.canonical.data.json',
+  'nsf-reu' = 'https://raw.githubusercontent.com/internaloha/scrapers/main/listings/compsci/nsf.json',
+  'simplyhired' = 'https://raw.githubusercontent.com/internaloha/scrapers/main/listings/compsci/simplyHired.json',
   'soc' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/soc.canonical.data.json',
-  'stackoverflow' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/stackoverflow.canonical.data.json',
-  'ziprecruiter' = 'https://raw.githubusercontent.com/internaloha/internaloha/master/scraper/data/canonical/ziprecruiter.canonical.data.json',
+  'stackoverflow' = 'https://raw.githubusercontent.com/internaloha/scrapers/main/listings/compsci/stackoverflow.json',
+  'ziprecruiter' = 'https://raw.githubusercontent.com/internaloha/scrapers/main/listings/compsci/ziprecruiter.json',
 }
 
 export const internAlohaUrls = Object.values(InternAlohaUrlsEnum);
