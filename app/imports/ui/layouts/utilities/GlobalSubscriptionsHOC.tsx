96cc44ed3cda05820498183cd915476b1d5d2532
Cam Moore <cammymoore07@gmail.com>
2022-01-26 10:33:09 -1000

    Got rid of ClientSideInternships.
    Now using InternshipCollection.

--- app/imports/ui/layouts/utilities/GlobalSubscriptionsHOC.tsx
+++ app/imports/ui/layouts/utilities/GlobalSubscriptionsHOC.tsx
@@ -19,6 +19,7 @@ import { Courses } from '../../../api/course/CourseCollection';
 import { Interests } from '../../../api/interest/InterestCollection';
 import { Opportunities } from '../../../api/opportunity/OpportunityCollection';
 import { PublicStats } from '../../../api/public-stats/PublicStatsCollection';
+import { Internships } from '../../../api/internship/InternshipCollection';
 
 interface Loading {
   loading: boolean;
@@ -48,6 +49,7 @@ const withGlobalSubscription = (WrappedComponent) => {
       globalSubs.subscribe(Interests.getPublicationName()),
       globalSubs.subscribe(InterestKeywords.getPublicationName()),
       globalSubs.subscribe(InterestTypes.getPublicationName()),
+      globalSubs.subscribe(Internships.getPublicationName()),
       globalSubs.subscribe(Opportunities.getPublicationName()),
       globalSubs.subscribe(OpportunityTypes.getPublicationName()),
       globalSubs.subscribe(PublicStats.getPublicationName()),
--- app/imports/ui/pages/shared/browser-view/InternshipBrowserViewPage.tsx
+++ app/imports/ui/pages/shared/browser-view/InternshipBrowserViewPage.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { useRouteMatch } from 'react-router';
-import { useParams } from 'react-router-dom';
-import { getInternshipsMethod } from '../../../../api/internship/InternshipCollection.methods';
-import { Users } from '../../../../api/user/UserCollection';
 import { CareerGoal, Course, Interest, Internship, Opportunity, ProfileCareerGoal, ProfileCourse, ProfileInterest, ProfileOpportunity } from '../../../../typings/radgrad';
 import InternshipBrowserView from '../../../components/shared/explorer/browser-view/InternshipBrowserView';
 import { PAGEIDS } from '../../../utilities/PageIDs';
 import PageLayout from '../../PageLayout';
-import { ClientSideInternships } from '../../../../startup/client/collections';
 import { getEntities } from './utilities/getEntities';
+import { Internships } from '../../../../api/internship/InternshipCollection';
 
 interface InternshipBrowserViewPageProps {
   internships: Internship[];
@@ -36,21 +33,9 @@ const InternshipBrowserViewPage: React.FC<InternshipBrowserViewPageProps> = ({ i
 );
 
 export default withTracker(() => {
-  const { username } = useParams();
-  const profile = Users.getProfile(username);
-  const studentID = profile.userID;
-  getInternshipsMethod.callPromise({ studentID })
-    .then(result => {
-      // console.log(result);
-      result.forEach((internship) => {
-        if (ClientSideInternships.find({ _id: internship._id }).fetch().length === 0) { // stop duplicate inserts
-          ClientSideInternships.insert(internship);
-        }
-      });
-    });
   const match = useRouteMatch();
   const entities: any = getEntities(match);
-  const internships = ClientSideInternships.find().fetch();
+  const internships = Internships.findNonRetired();
   entities.internships = internships;
   // console.log(internships);
   return entities;
